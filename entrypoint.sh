c6bf74be67a70fdbc437f82a67f7f2a7b9c6c182
Gernot Schulz <gernot@intevation.de>
2020-02-20 10:00:17 +0100

    entrypoint: Some improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # TODO: read optional ports from env variables: DB_PORT
-wait-for-it --timeout=10 "$DB_HOST":5432
+until pg_isready -h "$DB_HOST"; do
+  echo "Waiting for Postgres server '$DB_HOST' to become available..."
+  sleep 3
+done
 
 # Create schema in postgresql
 export PGPASSWORD="$DB_PASSWORD"
-psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -f src/schema.sql
+psql -1 -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -f src/schema.sql
 
-exec $*
-
+exec "$@"
