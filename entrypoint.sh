034bf9c662ebe5a4fdb23c45c05f9bd489fff958
Gernot Schulz <gernot@intevation.de>
2020-02-21 10:39:31 +0100

    Set missing password variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ export DB_HOST="${DB_HOST:-db}"
 export DB_PORT="${DB_PORT:-5432}"
 export DB_NAME="${DB_NAME:-mediafiledata}"
 export DB_USER="${DB_USER:-openslides}"
-export PGPASSWORD="${DB_PASSWORD:-openslides}"
+export DB_PASSWORD="${DB_PASSWORD:-openslides}"
+PGPASSWORD="$DB_PASSWORD"
 
 until pg_isready -h "$DB_HOST" -p "$DB_PORT"; do
   echo "Waiting for Postgres server '$DB_HOST' to become available..."
