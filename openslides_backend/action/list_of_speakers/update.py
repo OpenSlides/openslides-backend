5a4ccec93c16b63dc69919846b7bb4fa37a22c65
Ludwig Reiter <ludwig.reiter@intevation.de>
2020-08-12 14:32:15 +0200

    Rebase the dev_list_of_speakers branch.
    Add list_of_speakers action set.
    
    Fix docstring and description of meeting_id.
    
    Add closed to list_of_speakers model.
    
    Comment model of list_of_speakers.
    
    Add create/delete tests to list_of_speakers.
    
    Update layout of the code.
    
    Update list_of_speakers: remove create and delete actions.
    
    Update reverse fields comments of list_of_speakers model.

---
+++ openslides_backend/action/list_of_speakers/update.py
@@ -0,0 +1,14 @@
+from ...models.list_of_speakers import ListOfSpeakers
+from ..action import register_action
+from ..default_schema import DefaultSchema
+from ..generics import UpdateAction
+
+
+@register_action("list_of_speakers.update")
+class ListOfSpeakersUpdateAction(UpdateAction):
+    """
+    Action to update a list of speakers.
+    """
+
+    model = ListOfSpeakers()
+    schema = DefaultSchema(ListOfSpeakers()).get_update_schema(properties=["closed"])
---
+++ tests/system/action/list_of_speakers/test_update.py
@@ -0,0 +1,41 @@
+from tests.system.action.base import BaseActionTestCase
+
+
+class ListOfSpeakersUpdateActionTest(BaseActionTestCase):
+    def test_update_correct(self) -> None:
+        self.create_model("meeting/222", {"name": "name_xQyvfmsS"})
+        self.create_model(
+            "list_of_speakers/111", {"closed": False, "meeting_id": 222},
+        )
+        response = self.client.post(
+            "/",
+            json=[
+                {
+                    "action": "list_of_speakers.update",
+                    "data": [{"id": 111, "closed": True}],
+                }
+            ],
+        )
+        self.assert_status_code(response, 200)
+
+        model = self.get_model("list_of_speakers/111")
+        assert model.get("closed") is True
+
+    def test_update_wrong_id(self) -> None:
+        self.create_model("meeting/222", {"name": "name_xQyvfmsS"})
+        self.create_model(
+            "list_of_speakers/111", {"closed": False, "meeting_id": 222},
+        )
+
+        response = self.client.post(
+            "/",
+            json=[
+                {
+                    "action": "list_of_speakers.update",
+                    "data": [{"id": 112, "closed": True}],
+                }
+            ],
+        )
+        self.assert_status_code(response, 400)
+        model = self.get_model("list_of_speakers/111")
+        assert model.get("closed") is False
