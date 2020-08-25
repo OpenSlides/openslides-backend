404983620e0c7084890d14442f4da07a3652844b
GabrielMeyer <meyergabriel@live.de>
2020-08-25 12:14:51 +0200

    Connects auth with backend

--- openslides_backend/services/authentication.py
+++ openslides_backend/services/authentication.py
@@ -1,3 +1,5 @@
+from typing import Optional, Tuple
+
 import requests
 import simplejson as json
 from simplejson.errors import JSONDecodeError
@@ -7,6 +9,8 @@ from ..shared.interfaces import Headers, LoggingModule
 
 # TODO: Something should tell this service what the correct user id of the guest is.
 GUEST_USER_ID = 0
+
+AUTHENTICATION_HEADER = "Authentication"
 
 
 class AuthenticationHTTPAdapter:
@@ -19,19 +23,22 @@ class AuthenticationHTTPAdapter:
         self.logger = logging.getLogger(__name__)
         self.headers = {"Content-Type": "application/json"}
 
-    def get_user(self, headers: Headers) -> int:
+    def get_user(self, headers: Headers) -> Tuple[int, Optional[str]]:
         """
         Fetches user id from authentication service using request headers.
+        Returns a new access token, too, if one is received from auth service.
         """
-        # TODO: remove this once the auth service is correctly implemented
-        return 1
-
         self.logger.debug(
             f"Start request to authentication service with the following data: {headers}"
         )
         request_data = json.dumps(headers.to_wsgi_list())
+        access_token = None
         try:
-            response = requests.post(self.url, data=request_data, headers=self.headers)
+            response = requests.post(
+                self.get_internal_url("/api/authenticate"),
+                data=request_data,
+                headers=headers,
+            )
         except requests.exceptions.ConnectionError as e:
             self.logger.debug(
                 f"Cannot reach the authentication service on {self.url}. Error: {e}"
@@ -49,14 +56,16 @@ class AuthenticationHTTPAdapter:
                 raise AuthenticationException(
                     "Bad response from authentication service. Body does not contain JSON."
                 )
-            self.logger.debug(f"Get repsonse: {body}")
+            self.logger.debug(f"Get response body: {body}")
+
             try:
-                user_id = body["user_id"]
+                user_id = body["userId"]
             except (TypeError, KeyError):
                 raise AuthenticationException(
                     "Empty or bad response from authentication service."
                 )
-        return user_id
+            access_token = response.headers.get(AUTHENTICATION_HEADER, None)
+        return user_id, access_token
 
     def auth_is_down(self) -> int:
         """
@@ -64,3 +73,15 @@ class AuthenticationHTTPAdapter:
         """
         self.logger.debug(f"Auth cannot be reached. Fall back to guest mode.")
         return GUEST_USER_ID
+
+    def format_url_path(self, path: str) -> str:
+        """
+        Adds a '/' to the front of the given path, if not provided.
+        """
+        return f"/{path}" if not path.startswith("/") else path
+
+    def get_internal_url(self, path: str) -> str:
+        """
+        Returns an internal url to a specified path at auth-service.
+        """
+        return f"{self.url}{self.format_url_path(path)}"
