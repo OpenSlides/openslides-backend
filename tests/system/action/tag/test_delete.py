c41fdc940d9b766da23bdd27af973977086ab444
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2020-09-16 11:39:48 +0200

    Add tests for generic m:n relation

--- tests/system/action/tag/test_delete.py
+++ tests/system/action/tag/test_delete.py
@@ -1,14 +1,14 @@
 from tests.system.action.base import BaseActionTestCase
 
 
-class TagActionTest(BaseActionTestCase):
-    # def test_delete_correct(self) -> None:
-    #     self.create_model("tag/111", {"name": "name_srtgb123"})
-    #     response = self.client.post(
-    #         "/", json=[{"action": "tag.delete", "data": [{"id": 111}]}],
-    #     )
-    #     self.assert_status_code(response, 200)
-    #     self.assert_model_not_exists("tag/112")
+class TagDeleteTest(BaseActionTestCase):
+    def test_delete_correct(self) -> None:
+        self.create_model("tag/111", {"name": "name_srtgb123"})
+        response = self.client.post(
+            "/", json=[{"action": "tag.delete", "data": [{"id": 111}]}],
+        )
+        self.assert_status_code(response, 200)
+        self.assert_model_not_exists("tag/112")
 
     def test_delete_wrong_id(self) -> None:
         self.create_model("tag/112", {"name": "name_srtgb123"})
---
+++ tests/system/action/topic/test_update.py
@@ -0,0 +1,40 @@
+from tests.system.action.base import BaseActionTestCase
+
+
+class TopicUpdateTest(BaseActionTestCase):
+    def test_update_simple(self) -> None:
+        self.create_model("meeting/1", {"name": "test"})
+        self.create_model("topic/1", {"title": "test", "meeting_id": 1})
+        response = self.client.post(
+            "/",
+            json=[
+                {
+                    "action": "topic.update",
+                    "data": [{"id": 1, "title": "test2", "text": "text"}],
+                }
+            ],
+        )
+        self.assert_status_code(response, 200)
+        topic = self.get_model("topic/1")
+        self.assertEqual(topic.get("title"), "test2")
+        self.assertEqual(topic.get("text"), "text")
+
+    def test_update_tag_ids_add(self) -> None:
+        self.create_model("meeting/1", {"name": "test"})
+        self.create_model("topic/1", {"title": "test", "meeting_id": 1})
+        self.create_model("tag/1", {"name": "tag", "meeting_id": 1})
+        response = self.client.post(
+            "/", json=[{"action": "topic.update", "data": [{"id": 1, "tag_ids": [1]}]}],
+        )
+        self.assert_status_code(response, 200)
+        topic = self.get_model("topic/1")
+        self.assertEqual(topic.get("tag_ids"), [1])
+
+    def test_update_tag_ids_remove(self) -> None:
+        self.test_update_tag_ids_add()
+        response = self.client.post(
+            "/", json=[{"action": "topic.update", "data": [{"id": 1, "tag_ids": []}]}],
+        )
+        self.assert_status_code(response, 200)
+        topic = self.get_model("topic/1")
+        self.assertEqual(topic.get("tag_ids"), [])
