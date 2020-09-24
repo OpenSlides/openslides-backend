4d6a2652d2d055a3564dbb469d269c56ffcbad26
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2020-09-24 17:04:55 +0200

    Prevent deletion of last motion_workflow

--- tests/system/action/motion_workflow/test_delete.py
+++ tests/system/action/motion_workflow/test_delete.py
@@ -9,12 +9,13 @@ class MotionWorkflowSystemTest(BaseActionTestCase):
                 "name": "name_testtest",
                 "motions_default_workflow_id": 12,
                 "motions_default_statute_amendment_workflow_id": 13,
-                "motion_workflow_ids": [111],
+                "motion_workflow_ids": [111, 2],
             },
         )
         self.create_model(
             "motion_workflow/111", {"name": "name_srtgb123", "meeting_id": 90}
         )
+        self.create_model("motion_workflow/2", {"meeting_id": 90})
         response = self.client.post(
             "/", json=[{"action": "motion_workflow.delete", "data": [{"id": 111}]}],
         )
@@ -89,3 +90,14 @@ class MotionWorkflowSystemTest(BaseActionTestCase):
         self.assert_status_code(response, 400)
         self.assert_model_exists("motion_workflow/111")
         self.assertIn("Cannot delete a default workflow.", str(response.data))
+
+    def test_delete_last_workflow(self) -> None:
+        self.create_model(
+            "meeting/1", {"motion_workflow_ids": [1]},
+        )
+        self.create_model("motion_workflow/1", {"meeting_id": 1})
+        response = self.client.post(
+            "/", json=[{"action": "motion_workflow.delete", "data": [{"id": 1}]}],
+        )
+        self.assert_status_code(response, 400)
+        self.assert_model_exists("motion_workflow/1")
