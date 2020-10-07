181c5c33c38535982d425ae2cd7bcd135ea26f5b
Ludwig Reiter <ludwig.reiter@intevation.de>
2020-10-07 10:47:08 +0200

    Add motion.reset_recommendation action.
    Add tests.
    
    Update reset action: use update schema.

---
+++ tests/system/action/motion/test_reset_recommendation.py
@@ -0,0 +1,51 @@
+from tests.system.action.base import BaseActionTestCase
+
+
+class MotionResetRecommendationActionTest(BaseActionTestCase):
+    def test_reset_recommendation_correct(self) -> None:
+        self.create_model("meeting/222", {"name": "name_SNLGsvIV"})
+        self.create_model(
+            "motion_state/77",
+            {"name": "test1", "motion_id": 22, "motion_recommendation_ids": [22]},
+        )
+        self.create_model(
+            "motion/22", {"meeting_id": 222, "title": "test1", "recommendation_id": 77}
+        )
+        response = self.client.post(
+            "/", json=[{"action": "motion.reset_recommendation", "data": [{"id": 22}]}]
+        )
+        self.assert_status_code(response, 200)
+        model = self.get_model("motion/22")
+        assert model.get("recommendation_id") is None
+
+    def test_reset_recommendation_correct_empty_recommendation(self) -> None:
+        self.create_model("meeting/222", {"name": "name_SNLGsvIV"})
+        self.create_model(
+            "motion_state/77",
+            {"name": "test1", "motion_id": 22, "motion_recommendation_ids": [22]},
+        )
+        self.create_model(
+            "motion/22",
+            {"meeting_id": 222, "title": "test1", "recommendation_id": None},
+        )
+        response = self.client.post(
+            "/", json=[{"action": "motion.reset_recommendation", "data": [{"id": 22}]}]
+        )
+        self.assert_status_code(response, 200)
+        model = self.get_model("motion/22")
+        assert model.get("recommendation_id") is None
+
+    def test_reset_recommendation_wrong(self) -> None:
+        self.create_model("meeting/222", {"name": "name_SNLGsvIV"})
+        self.create_model(
+            "motion_state/77",
+            {"name": "test1", "motion_id": 22, "motion_recommendation_ids": [22]},
+        )
+        self.create_model(
+            "motion/23", {"meeting_id": 222, "title": "test1", "recommendation_id": 77}
+        )
+        response = self.client.post(
+            "/", json=[{"action": "motion.reset_recommendation", "data": [{"id": 22}]}]
+        )
+        self.assert_status_code(response, 400)
+        self.assert_model_not_exists("motion/22")
