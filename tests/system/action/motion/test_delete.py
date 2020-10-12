83a4db27c838ab9ea88e59f1348328b7fe69c29f
Ludwig Reiter <ludwig.reiter@intevation.de>
2020-10-12 15:07:48 +0200

    Add motion ondelete cascade test.

---
+++ tests/system/action/motion/test_delete.py
@@ -0,0 +1,47 @@
+from tests.system.action.base import BaseActionTestCase
+
+
+class MotionDeleteActionTest(BaseActionTestCase):
+    def test_delete_correct(self) -> None:
+        self.create_model("motion/111", {"title": "title_srtgb123"})
+        response = self.client.post(
+            "/", json=[{"action": "motion.delete", "data": [{"id": 111}]}],
+        )
+        self.assert_status_code(response, 200)
+        self.assert_model_deleted("motion/111")
+
+    def test_delete_wrong_id(self) -> None:
+        self.create_model("motion/112", {"title": "title_srtgb123"})
+        response = self.client.post(
+            "/", json=[{"action": "motion.delete", "data": [{"id": 111}]}],
+        )
+        self.assert_status_code(response, 400)
+        self.assert_model_exists("motion/112")
+
+    def test_delete_correct_cascading(self) -> None:
+        self.create_model(
+            "motion/111",
+            {
+                "title": "title_srtgb123",
+                "list_of_speakers_id": 222,
+                "agenda_item_id": 333,
+            },
+        )
+        self.create_model(
+            "list_of_speakers/222",
+            {"closed": False, "content_object_id": "motion/111"},
+        )
+        self.create_model(
+            "agenda_item/333",
+            {
+                "comment": "test_comment_ewoirzewoirioewr",
+                "content_object_id": "motion/111",
+            },
+        )
+        response = self.client.post(
+            "/", json=[{"action": "motion.delete", "data": [{"id": 111}]}],
+        )
+        self.assert_status_code(response, 200)
+        self.assert_model_deleted("motion/111")
+        self.assert_model_deleted("agenda_item/333")
+        self.assert_model_deleted("list_of_speakers/222")
