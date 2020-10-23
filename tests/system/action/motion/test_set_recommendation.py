dc9f3e44cb08c1616779078edfc728f081de7186
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2020-10-23 10:59:40 +0200

    Fix typo

--- tests/system/action/motion/test_set_recommendation.py
+++ tests/system/action/motion/test_set_recommendation.py
@@ -2,7 +2,7 @@ from tests.system.action.base import BaseActionTestCase
 
 
 class MotionSetRecommendationActionTest(BaseActionTestCase):
-    def test_reset_recommendation_correct(self) -> None:
+    def test_set_recommendation_correct(self) -> None:
         self.create_model("workflow/34", {})
         self.create_model("motion_state/66", {"motion_ids": [22], "workflow_id": 34})
         self.create_model(
@@ -24,7 +24,7 @@ class MotionSetRecommendationActionTest(BaseActionTestCase):
         model = self.get_model("motion/22")
         assert model.get("recommendation_id") == 77
 
-    def test_reset_recommendation_missing_recommendation_label(self) -> None:
+    def test_set_recommendation_missing_recommendation_label(self) -> None:
         self.create_model("workflow/34", {})
         self.create_model("motion_state/66", {"motion_ids": [22], "workflow_id": 34})
         self.create_model(
@@ -47,7 +47,7 @@ class MotionSetRecommendationActionTest(BaseActionTestCase):
             response.data
         )
 
-    def test_reset_recommendation_not_matching_workflow_ids(self) -> None:
+    def test_set_recommendation_not_matching_workflow_ids(self) -> None:
         self.create_model("workflow/34", {})
         self.create_model("motion_state/66", {"motion_ids": [22], "workflow_id": 34})
         self.create_model(
