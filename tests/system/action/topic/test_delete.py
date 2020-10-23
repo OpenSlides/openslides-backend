cff19c03581f5af9c6f7cebad92d0247ddaf4c05
Norman Jäckel <openslides@normanjaeckel.de>
2020-10-23 11:38:12 +0200

    Re add deleted test.

--- tests/system/action/topic/test_delete.py
+++ tests/system/action/topic/test_delete.py
@@ -44,3 +44,21 @@ class TopicDeleteActionTest(BaseActionTestCase):
         self.assert_model_deleted("topic/111")
         self.assert_model_deleted("agenda_item/333")
         self.assert_model_deleted("list_of_speakers/222")
+
+    def test_create_delete(self) -> None:
+        self.create_model("meeting/1", {})
+        response = self.client.post(
+            "/",
+            json=[
+                {"action": "topic.create", "data": [{"meeting_id": 1, "title": "test"}]}
+            ],
+        )
+        self.assert_status_code(response, 200)
+        self.assert_model_exists("topic/1")
+        self.assert_model_exists("list_of_speakers/1")
+        response = self.client.post(
+            "/", json=[{"action": "topic.delete", "data": [{"id": 1}]}],
+        )
+        self.assert_status_code(response, 200)
+        self.assert_model_deleted("topic/1")
+        self.assert_model_deleted("list_of_speakers/1")
