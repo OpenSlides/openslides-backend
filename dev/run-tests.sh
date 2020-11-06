3839c3c5c7f2493afaf9d77f264f83d9dcc83877
Ludwig Reiter <ludwig.reiter@intevation.de>
2020-11-06 14:39:05 +0100

    run tests wait for auth, too.

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -5,6 +5,7 @@ DC="docker-compose -f dev/docker-compose.dev.yml"
 $DC up --build --detach
 $DC exec -T backend dev/wait.sh writer 9011
 $DC exec -T backend dev/wait.sh reader 9010
+$DC exec -T backend dev/wait.sh auth 9004
 $DC exec -T backend pytest --cov
 error=$?
 $DC down --volumes
