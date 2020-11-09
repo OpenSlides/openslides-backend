200983bd4754b00ce6302d75c964af1aaf560ed2
Ludwig Reiter <ludwig.reiter@intevation.de>
2020-11-09 11:35:35 +0100

    Add user.set_password_temporary action. Add tests.
    Update user.set_password_temporary.
    
    Rename database in datastore.
    Update tests to the use of default_password.
    
    Update set_password_temporary
    
    Move check temporary in own mixin.

---
+++ tests/system/action/user/test_set_password_temporary.py
@@ -0,0 +1,50 @@
+from tests.system.action.base import BaseActionTestCase
+
+
+class UserSetPasswordTemporaryActionTest(BaseActionTestCase):
+    def test_update_correct(self) -> None:
+        self.create_model("meeting/2", {"name": "name_meeting_2"})
+        self.update_model("user/1", {"password": "old_pw", "meeting_id": 2})
+        response = self.client.post(
+            "/",
+            json=[
+                {
+                    "action": "user.set_password_temporary",
+                    "data": [{"id": 1, "password": "test"}],
+                }
+            ],
+        )
+        self.assert_status_code(response, 200)
+        model = self.get_model("user/1")
+        assert self.auth.is_equals("test", model.get("password", ""))
+
+    def test_update_correct_default_case(self) -> None:
+        self.create_model("meeting/2", {"name": "meeting_2"})
+        self.update_model("user/1", {"password": "old_pw", "meeting_id": 2})
+        response = self.client.post(
+            "/",
+            json=[
+                {
+                    "action": "user.set_password_temporary",
+                    "data": [{"id": 1, "password": "test", "set_as_default": True}],
+                }
+            ],
+        )
+        self.assert_status_code(response, 200)
+        model = self.get_model("user/1")
+        assert self.auth.is_equals("test", model.get("password", ""))
+        assert "test" == model.get("default_password")
+
+    def test_update_not_temporary_user(self) -> None:
+        self.update_model("user/1", {"password": "old_pw"})
+        response = self.client.post(
+            "/",
+            json=[
+                {
+                    "action": "user.set_password_temporary",
+                    "data": [{"id": 1, "password": "test", "set_as_default": True}],
+                }
+            ],
+        )
+        self.assert_status_code(response, 400)
+        self.assertIn("User 1 is not temporary.", str(response.data))
