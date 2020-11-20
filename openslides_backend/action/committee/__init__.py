b914463e7d9513b98ebbd1a179faba8c37223e34
Ralf Peschke <rpeschke@peschke-it.de>
2020-11-20 09:53:32 +0100

    Implement Committee.delete including tests

--- openslides_backend/action/committee/__init__.py
+++ openslides_backend/action/committee/__init__.py
@@ -1,13 +1,8 @@
 from ..base import DummyAction
 from ..register import register_action
-from . import create  # noqa
+from . import create, delete  # noqa
 
 
 @register_action("committee.update")
 class CommitteeUpdate(DummyAction):
-    pass
-
-
-@register_action("committee.delete")
-class CommitteeDelete(DummyAction):
     pass
---
+++ tests/system/action/committee/test_delete.py
@@ -0,0 +1,75 @@
+from tests.system.action.base import BaseActionTestCase
+
+
+class CommitteeDeleteActionTest(BaseActionTestCase):
+    COMMITTEE_ID = 1
+    COMMITTEE_FQID = "committee/1"
+
+    def create_data(self) -> None:
+        self.create_model("organisation/1", {"name": "test_organisation1"})
+        self.create_model("user/20", {"username": "test_user20"})
+        self.create_model("user/21", {"username": "test_user21"})
+
+        self.client.post(
+            "/",
+            json=[
+                {
+                    "action": "committee.create",
+                    "data": [
+                        {
+                            "name": "committee_testname",
+                            "organisation_id": 1,
+                            "description": "<p>Test description</p>",
+                            "member_ids": [20],
+                            "manager_ids": [21],
+                        }
+                    ],
+                }
+            ],
+        )
+
+    def test_delete_correct(self) -> None:
+        self.create_data()
+        response = self.client.post(
+            "/",
+            json=[{"action": "committee.delete", "data": [{"id": self.COMMITTEE_ID}]}],
+        )
+
+        self.assert_status_code(response, 200)
+        self.assert_model_deleted(self.COMMITTEE_FQID)
+        self.assert_model_exists(
+            "user/20", {"member_in_meeting_ids": None, "manager_in_meeting_ids": None}
+        )
+        self.assert_model_exists(
+            "user/21", {"member_in_meeting_ids": None, "manager_in_meeting_ids": None}
+        )
+
+    def test_delete_wrong_id(self) -> None:
+        self.create_data()
+        response = self.client.post(
+            "/",
+            json=[{"action": "committee.delete", "data": [{"id": 2}]}],
+        )
+        self.assert_status_code(response, 400)
+        self.assertIn("Model \\'committee/2\\' does not exist.", str(response.data))
+        model = self.get_model(self.COMMITTEE_FQID)
+        self.assertEqual(model.get("name"), "committee_testname")
+
+    def test_delete_protected_by_meeting(self) -> None:
+        self.create_data()
+        self.create_model(
+            "meeting/22", {"name": "name_meeting_22", "committee_id": self.COMMITTEE_ID}
+        )
+        self.update_model(self.COMMITTEE_FQID, {"meeting_ids": [22]})
+
+        response = self.client.post(
+            "/",
+            json=[{"action": "committee.delete", "data": [{"id": self.COMMITTEE_ID}]}],
+        )
+
+        self.assert_status_code(response, 400)
+        self.assert_model_exists(self.COMMITTEE_FQID, {"meeting_ids": [22]})
+        self.assertIn(
+            "meeting/22",
+            str(response.data),
+        )
