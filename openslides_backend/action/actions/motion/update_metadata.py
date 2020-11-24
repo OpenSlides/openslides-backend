b487ca954c0342c665e444b47552067b770e0767
Ludwig Reiter <ludwig.reiter@intevation.de>
2020-11-24 14:28:04 +0100

    Squashed + Rebased motion.update_metadata
    Update motion.update_metadata
    
    Include a set_recommendation_extension_reference_ids method and use it.
    Also add some tests/update tests.
    
    Update motion.update_metadata action.
    
    Don't throw an exception, if the mentioned motion is not in the db.
    Update tests.
    
    Add two checks and tests.
    
    Update motion.update_metadata.
    Add a check for same meeting_ids.
    Add a check to ensure motions.
    Add some tests for these checks.
    
    Update motion.update_metadata. Remove a check, fix generic field and update tests.
    
    Update layout.
    
    Remove not needed referenced_in_motion_recommendation_extension_ids from a test.

--- openslides_backend/action/actions/motion/update_metadata.py
+++ openslides_backend/action/actions/motion/update_metadata.py
@@ -1,12 +1,17 @@
+import re
 import time
 from typing import Any, Dict
 
 from ....models.models import Motion
-from ....shared.patterns import Collection, FullQualifiedId
+from ....shared.exceptions import ActionException
+from ....shared.filters import FilterOperator
+from ....shared.patterns import KEYSEPARATOR, Collection, FullQualifiedId
 from ....shared.schema import optional_id_schema
 from ...generics.update import UpdateAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
+
+RERIDS_PATTERN = re.compile(r"\[(?P<fqid>\w+/\d+)\]")
 
 
 @register_action("motion.update_metadata")
@@ -48,4 +53,28 @@ class MotionUpdateMetadata(UpdateAction):
                 instance["state_id"] = workflow["first_state_id"]
                 instance["recommendation_id"] = None
         instance["last_modified"] = round(time.time())
+
+        if instance.get("recommendation_extension"):
+            self.set_recommendation_extension_reference_ids(instance)
         return instance
+
+    def set_recommendation_extension_reference_ids(
+        self, instance: Dict[str, Any]
+    ) -> None:
+        recommendation_extension_reference_ids = []
+        possible_rerids = RERIDS_PATTERN.findall(instance["recommendation_extension"])
+        for fqid_str in possible_rerids:
+            collection, id_ = fqid_str.split(KEYSEPARATOR)
+            if collection != "motion":
+                raise ActionException(f"Found {fqid_str} but only motion is allowed.")
+            exists_res = self.datastore.exists(
+                collection=Collection(collection),
+                filter=FilterOperator("id", "=", int(id_)),
+            )
+            if exists_res.get("exists"):
+                recommendation_extension_reference_ids.append(
+                    FullQualifiedId(Collection(collection), id_)
+                )
+        instance[
+            "recommendation_extension_reference_ids"
+        ] = recommendation_extension_reference_ids
