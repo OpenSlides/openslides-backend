4dbc79c1e84e4f744e2324254b73620d42376d7b
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-01-12 13:39:47 +0100

    Update agenda_item.delete, add a topic.delete Action, *Broken*.

--- openslides_backend/action/actions/agenda_item/delete.py
+++ openslides_backend/action/actions/agenda_item/delete.py
@@ -1,7 +1,11 @@
+from typing import Any, Dict
+
 from ....models.models import AgendaItem
+from ....shared.patterns import KEYSEPARATOR, FullQualifiedId
 from ...generics.delete import DeleteAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
+from ..topic.delete import TopicDelete
 
 
 @register_action("agenda_item.delete")
@@ -12,3 +16,15 @@ class AgendaItemDelete(DeleteAction):
 
     model = AgendaItem()
     schema = DefaultSchema(AgendaItem()).get_delete_schema()
+
+    def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
+        agenda_item = self.datastore.get(
+            FullQualifiedId(self.model.collection, instance["id"]),
+            ["content_object_id"],
+        )
+        if agenda_item.get("content_object_id") and agenda_item[
+            "content_object_id"
+        ].startswith("topic" + KEYSEPARATOR):
+            _, id_ = agenda_item["content_object_id"].split(KEYSEPARATOR)
+            self.execute_other_action(TopicDelete, [{"id": int(id_)}])
+        return instance
