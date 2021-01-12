6c5a377bbf459473d6bc4b5b918efc1047180dd8
Finn Stutzenstein <finn.stutzenstein@hotmail.de>
2021-01-12 16:10:31 +0100

    changed test data

--- tests/test_get_resource.py
+++ tests/test_get_resource.py
@@ -6,14 +6,15 @@ GET_URL = "http://media:9006/system/media/get_resource/"
 
 def test_positiv():
     # wait the service to start
-    req = requests.get(GET_URL + "2")
+    req = requests.get(GET_URL + "1")
     assert req.status_code == 200
-    assert req.content == b"a2"
-    assert "text/plain" in req.headers.get("Content-Type")
+    assert len(req.content) == 1462
+    assert type(req.content) == bytes
+    assert "image/jpeg" in req.headers.get("Content-Type")
 
 
 def test_not_found():
-    req = requests.get(GET_URL + "1")
+    req = requests.get(GET_URL + "2")
     assert req.status_code == 500
     assert "message" in req.json()
 
