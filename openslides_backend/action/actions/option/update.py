533b13351a0c0c07f29404cfc6f2c217087ab19d
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-01-14 09:24:56 +0100

    Update option.update. Add _get_poll method.

--- openslides_backend/action/actions/option/update.py
+++ openslides_backend/action/actions/option/update.py
@@ -1,6 +1,8 @@
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 
 from ....models.models import Option
+from ....shared.exceptions import ActionException
+from ....shared.patterns import Collection, FullQualifiedId
 from ...generics.update import UpdateAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
@@ -30,4 +32,25 @@ class OptionUpdateAction(UpdateAction):
             instance["no"] = instance.pop("N")
         if "A" in instance:
             instance["abstain"] = instance.pop("A")
+
+        poll_id_option, poll = self._get_poll(instance["id"])
+        print(poll_id_option, poll)
+
         return instance
+
+    def _get_poll(self, option_id: int) -> Tuple[bool, Dict[str, Any]]:
+        option = self.datastore.get(
+            FullQualifiedId(self.model.collection, option_id),
+            ["poll_id", "used_as_global_option_in_poll_id"],
+        )
+        poll_id_option = False
+        if option.get("poll_id"):
+            poll_id = option["poll_id"]
+            poll_id_option = True
+        elif option.get("used_as_global_option_in_poll_id"):
+            poll_id = option["used_as_global_option_in_poll_id"]
+        else:
+            raise ActionException("Dont find poll for option")
+        return poll_id_option, self.datastore.get(
+            FullQualifiedId(Collection("poll"), poll_id), ["type", "pollmethod"]
+        )
--- tests/system/action/option/test_update.py
+++ tests/system/action/option/test_update.py
@@ -5,12 +5,60 @@ class OptionUpdateActionTest(BaseActionTestCase):
     def test_update(self) -> None:
         self.create_model("meeting/110", {"name": "meeting_110"})
         self.create_model(
+            "poll/65",
+            {
+                "type": "analog",
+                "pollmethod": "YNA",
+                "meeting_id": 110,
+                "option_ids": [57],
+            },
+        )
+        self.create_model(
             "option/57",
             {
                 "yes": "0.000000",
                 "no": "0.000000",
                 "abstain": "0.000000",
                 "meeting_id": 110,
+                "poll_id": 65,
+            },
+        )
+        response = self.client.post(
+            "/",
+            json=[
+                {
+                    "action": "option.update",
+                    "data": [
+                        {"id": 57, "Y": "1.000000", "N": "2.000000", "A": "3.000000"}
+                    ],
+                }
+            ],
+        )
+        self.assert_status_code(response, 200)
+        option = self.get_model("option/57")
+        assert option.get("yes") == "1.000000"
+        assert option.get("no") == "2.000000"
+        assert option.get("abstain") == "3.000000"
+
+    def test_update_global_option(self) -> None:
+        self.create_model("meeting/110", {"name": "meeting_110"})
+        self.create_model(
+            "poll/65",
+            {
+                "type": "analog",
+                "pollmethod": "YNA",
+                "meeting_id": 110,
+                "global_option_id": 57,
+            },
+        )
+        self.create_model(
+            "option/57",
+            {
+                "yes": "0.000000",
+                "no": "0.000000",
+                "abstain": "0.000000",
+                "meeting_id": 110,
+                "used_as_global_option_in_poll_id": 65,
             },
         )
         response = self.client.post(
