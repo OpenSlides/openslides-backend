06f629b99d979f807584598ab5f7d310a767995b
Ralf Peschke <github@peschke-it.de>
2021-01-28 16:06:49 +0100

    issue375 Assignment_candidate: Check status of assignment (#419)
    Co-authored-by: Ralf Peschke <rpeschke@peschke-it.de>

--- openslides_backend/action/actions/assignment_candidate/delete.py
+++ openslides_backend/action/actions/assignment_candidate/delete.py
@@ -1,4 +1,8 @@
+from typing import Any, Dict
+
 from ....models.models import AssignmentCandidate
+from ....shared.exceptions import ActionException
+from ....shared.patterns import Collection, FullQualifiedId
 from ...generics.delete import DeleteAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
@@ -12,3 +16,23 @@ class AssignmentCandidateDelete(DeleteAction):
 
     model = AssignmentCandidate()
     schema = DefaultSchema(AssignmentCandidate()).get_delete_schema()
+
+    def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
+        instance = super().update_instance(instance)
+        assignment_candidate = self.datastore.get(
+            FullQualifiedId(self.model.collection, instance["id"]),
+            mapped_fields=["assignment_id"],
+            lock_result=True,
+        )
+        assignment = self.datastore.get(
+            FullQualifiedId(
+                Collection("assignment"), assignment_candidate["assignment_id"]
+            ),
+            mapped_fields=["phase"],
+            lock_result=True,
+        )
+        if assignment.get("phase") == "finished":
+            raise ActionException(
+                "It is not permitted to remove a candidate from a finished assignment!"
+            )
+        return instance
