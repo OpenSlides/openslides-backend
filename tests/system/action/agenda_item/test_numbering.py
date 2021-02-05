e5c209d689d9681f42625a6bf6385800e80d7bba
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-02-05 13:37:31 +0100

    Pass meeting settings to AgendaTree.

--- tests/system/action/agenda_item/test_numbering.py
+++ tests/system/action/agenda_item/test_numbering.py
@@ -42,6 +42,82 @@ class AgendaItemNumberingTester(BaseActionTestCase):
         agenda_item_3 = self.get_model("agenda_item/3")
         assert agenda_item_3.get("item_number") == "1.2"
 
+    def test_numbering_prefix(self) -> None:
+        self.set_models(
+            {
+                "meeting/1": {
+                    "agenda_item_ids": [1, 2, 3],
+                    "agenda_number_prefix": "P-",
+                },
+                "agenda_item/1": {
+                    "meeting_id": 1,
+                    "weight": 10,
+                    "type": AgendaItem.AGENDA_ITEM,
+                },
+                "agenda_item/2": {
+                    "meeting_id": 1,
+                    "weight": 10,
+                    "parent_id": 1,
+                    "type": AgendaItem.AGENDA_ITEM,
+                },
+                "agenda_item/3": {
+                    "meeting_id": 1,
+                    "parent_id": 1,
+                    "weight": 10,
+                    "type": AgendaItem.AGENDA_ITEM,
+                },
+            }
+        )
+        response = self.client.post(
+            "/",
+            json=[{"action": "agenda_item.numbering", "data": [{"meeting_id": 1}]}],
+        )
+        self.assert_status_code(response, 200)
+        agenda_item_1 = self.get_model("agenda_item/1")
+        assert agenda_item_1.get("item_number") == "P- 1"
+        agenda_item_2 = self.get_model("agenda_item/2")
+        assert agenda_item_2.get("item_number") == "P- 1.1"
+        agenda_item_3 = self.get_model("agenda_item/3")
+        assert agenda_item_3.get("item_number") == "P- 1.2"
+
+    def test_numbering_roman(self) -> None:
+        self.set_models(
+            {
+                "meeting/1": {
+                    "agenda_item_ids": [1, 2, 3],
+                    "agenda_numeral_system": "roman",
+                },
+                "agenda_item/1": {
+                    "meeting_id": 1,
+                    "weight": 10,
+                    "type": AgendaItem.AGENDA_ITEM,
+                },
+                "agenda_item/2": {
+                    "meeting_id": 1,
+                    "weight": 10,
+                    "parent_id": 1,
+                    "type": AgendaItem.AGENDA_ITEM,
+                },
+                "agenda_item/3": {
+                    "meeting_id": 1,
+                    "parent_id": 1,
+                    "weight": 10,
+                    "type": AgendaItem.AGENDA_ITEM,
+                },
+            }
+        )
+        response = self.client.post(
+            "/",
+            json=[{"action": "agenda_item.numbering", "data": [{"meeting_id": 1}]}],
+        )
+        self.assert_status_code(response, 200)
+        agenda_item_1 = self.get_model("agenda_item/1")
+        assert agenda_item_1.get("item_number") == "I"
+        agenda_item_2 = self.get_model("agenda_item/2")
+        assert agenda_item_2.get("item_number") == "I.1"
+        agenda_item_3 = self.get_model("agenda_item/3")
+        assert agenda_item_3.get("item_number") == "I.2"
+
     def test_numbering_without_parents(self) -> None:
         self.create_model(
             "meeting/1",
