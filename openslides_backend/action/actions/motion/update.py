c438ab778b08f0e14d795b0bbf8e49ce23ebcdba
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-02-08 13:07:26 +0100

    Update motion.update: Add attachment_ids and workflow code.
    Add this code from motion.update_metadata.
    Also update and add tests.

--- openslides_backend/action/actions/motion/update.py
+++ openslides_backend/action/actions/motion/update.py
@@ -4,6 +4,7 @@ from typing import Any, Dict
 from ....models.models import Motion
 from ....shared.exceptions import ActionException
 from ....shared.patterns import POSITIVE_NUMBER_REGEX, Collection, FullQualifiedId
+from ....shared.schema import optional_id_schema
 from ...generics.update import UpdateAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
@@ -23,10 +24,12 @@ class MotionUpdate(UpdateAction):
             "text",
             "reason",
             "modified_final_version",
+            "attachment_ids",
         ],
-        additional_optional_fields=Motion().get_property(
-            "amendment_paragraph_$", POSITIVE_NUMBER_REGEX
-        ),
+        additional_optional_fields={
+            **Motion().get_property("amendment_paragraph_$", POSITIVE_NUMBER_REGEX),
+            "workflow_id": optional_id_schema,
+        },
     )
 
     def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
@@ -58,4 +61,22 @@ class MotionUpdate(UpdateAction):
             )
             if meeting.get("motions_reason_required"):
                 raise ActionException("Reason is required to update.")
+
+        if instance.get("workflow_id"):
+            motion = self.datastore.get(
+                FullQualifiedId(self.model.collection, instance["id"]), ["state_id"]
+            )
+            state = self.datastore.get(
+                FullQualifiedId(Collection("motion_state"), motion["state_id"]),
+                ["workflow_id"],
+            )
+            if instance["workflow_id"] != state.get("workflow_id"):
+                workflow = self.datastore.get(
+                    FullQualifiedId(
+                        Collection("motion_workflow"), instance["workflow_id"]
+                    ),
+                    ["first_state_id"],
+                )
+                instance["state_id"] = workflow["first_state_id"]
+                instance["recommendation_id"] = None
         return instance
--- tests/system/action/motion/test_update.py
+++ tests/system/action/motion/test_update.py
@@ -29,6 +29,7 @@ class MotionUpdateActionTest(BaseActionTestCase):
                             "reason": "reason_ukWqADfE",
                             "modified_final_version": "mfv_ilVvBsUi",
                             "amendment_paragraph_$": {3: "<html>test</html>"},
+                            "attachment_ids": [],
                         }
                     ],
                 }
@@ -43,6 +44,7 @@ class MotionUpdateActionTest(BaseActionTestCase):
         assert model.get("modified_final_version") == "mfv_ilVvBsUi"
         assert model.get("amendment_paragraph_$3") == "&lt;html&gt;test&lt;/html&gt;"
         assert model.get("amendment_paragraph_$") == ["3"]
+        assert model.get("attachment_ids") == []
 
     def test_update_wrong_id(self) -> None:
         self.create_model(
@@ -158,3 +160,85 @@ class MotionUpdateActionTest(BaseActionTestCase):
         )
         self.assert_status_code(response, 400)
         self.assertIn("Reason is required to update.", response.json["message"])
+
+    def test_update_metadata_workflow_id(self) -> None:
+        self.create_model("meeting/2538", {"name": "name_jkPIYjFz"})
+        self.create_model(
+            "motion/111",
+            {
+                "meeting_id": 2538,
+                "state_id": 88,
+                "recommendation_id": 88,
+            },
+        )
+        self.create_model(
+            "motion_workflow/22", {"name": "name_workflow_22", "meeting_id": 2538}
+        )
+        self.create_model(
+            "motion_state/88",
+            {
+                "name": "name_blaglup",
+                "meeting_id": 2538,
+                "workflow_id": 22,
+                "motion_ids": [111],
+                "motion_recommendation_ids": [111],
+            },
+        )
+        self.create_model(
+            "motion_state/23",
+            {"name": "name_state_23", "meeting_id": 2538, "motion_ids": []},
+        )
+        self.create_model(
+            "motion_workflow/35",
+            {"name": "name_workflow_35", "first_state_id": 23, "meeting_id": 2538},
+        )
+        response = self.client.post(
+            "/",
+            json=[
+                {
+                    "action": "motion.update_metadata",
+                    "data": [{"id": 111, "workflow_id": 35}],
+                }
+            ],
+        )
+        self.assert_status_code(response, 200)
+        model = self.get_model("motion/111")
+        assert model.get("state_id") == 23
+        assert model.get("recommendation_id") is None
+
+    def test_update_metadata_workflow_id_no_change(self) -> None:
+        self.create_model("meeting/2538", {"name": "name_jkPIYjFz"})
+        self.create_model(
+            "motion/111",
+            {
+                "meeting_id": 2538,
+                "state_id": 88,
+                "recommendation_id": 88,
+            },
+        )
+        self.create_model(
+            "motion_workflow/22", {"name": "name_workflow_22", "meeting_id": 2538}
+        )
+        self.create_model(
+            "motion_state/88",
+            {
+                "name": "name_blaglup",
+                "meeting_id": 2538,
+                "workflow_id": 22,
+                "motion_ids": [111],
+                "motion_recommendation_ids": [111],
+            },
+        )
+        response = self.client.post(
+            "/",
+            json=[
+                {
+                    "action": "motion.update_metadata",
+                    "data": [{"id": 111, "workflow_id": 22}],
+                }
+            ],
+        )
+        self.assert_status_code(response, 200)
+        model = self.get_model("motion/111")
+        assert model.get("state_id") == 88
+        assert model.get("recommendation_id") == 88
