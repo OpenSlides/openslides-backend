7457d129f70976bfa6f2828a086fe7d7045f15d3
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-02-18 16:17:36 +0100

    Update poll update tests.
    Merge into one class. Remove some surplus tests.

--- tests/system/action/poll/test_update.py
+++ tests/system/action/poll/test_update.py
@@ -5,105 +5,6 @@ from openslides_backend.models.models import Poll
 from tests.system.action.base import BaseActionTestCase
 
 
-class PollUpdateActionTest(BaseActionTestCase):
-    def test_update_correct(self) -> None:
-        self.create_model("organisation/1", {"enable_electronic_voting": True})
-        self.create_model("poll/1", {"state": "started", "type": "named"})
-        response = self.request(
-            "poll.update",
-            {
-                "id": 1,
-                "title": "test",
-                "description": "test2",
-                "onehundred_percent_base": "Y",
-                "majority_method": "simple",
-            },
-        )
-        self.assert_status_code(response, 200)
-        poll = self.get_model("poll/1")
-        assert poll.get("title") == "test"
-        assert poll.get("description") == "test2"
-        assert poll.get("onehundred_percent_base") == "Y"
-        assert poll.get("majority_method") == "simple"
-
-    def test_catch_not_allowed(self) -> None:
-        self.create_model("organisation/1", {"enable_electronic_voting": True})
-        self.create_model("poll/1", {"state": "started", "type": "named"})
-        response = self.request(
-            "poll.update",
-            {
-                "id": 1,
-                "pollmethod": "Y",
-                "type": "analog",
-                "min_votes_amount": 1,
-                "max_votes_amount": 1,
-                "global_yes": False,
-                "global_no": True,
-                "global_abstain": True,
-            },
-        )
-        self.assert_status_code(response, 400)
-        assert (
-            "Following options are not allowed in this state and type: "
-            "pollmethod, type, min_votes_amount, max_votes_amount, global_yes,"
-            " global_no, global_abstain"
-        ) in response.data.decode()
-
-    def test_optional_state_created(self) -> None:
-        self.create_model("organisation/1", {"enable_electronic_voting": True})
-        self.create_model("poll/1", {"state": "created", "type": "named"})
-        response = self.request(
-            "poll.update",
-            {
-                "id": 1,
-                "pollmethod": "Y",
-                "type": "analog",
-                "min_votes_amount": 1,
-                "max_votes_amount": 1,
-                "global_yes": False,
-                "global_no": True,
-                "global_abstain": True,
-            },
-        )
-        self.assert_status_code(response, 200)
-        poll = self.get_model("poll/1")
-        assert poll.get("pollmethod") == "Y"
-        assert poll.get("type") == "analog"
-        assert poll.get("min_votes_amount") == 1
-        assert poll.get("max_votes_amount") == 1
-        assert poll.get("global_yes") is False
-        assert poll.get("global_no") is True
-        assert poll.get("global_abstain") is True
-
-    def test_not_allowed_for_analog(self) -> None:
-        self.create_model("organisation/1", {"enable_electronic_voting": True})
-        self.create_model("poll/1", {"state": "started", "type": "analog"})
-        response = self.request("poll.update", {"id": 1, "entitled_group_ids": []})
-        self.assert_status_code(response, 400)
-        assert (
-            "Following options are not allowed in this state and type: "
-            "entitled_group_ids"
-        ) in response.data.decode()
-
-    def test_not_allowed_for_non_analog(self) -> None:
-        self.create_model("organisation/1", {"enable_electronic_voting": True})
-        self.create_model("poll/1", {"state": "started", "type": "named"})
-        response = self.request(
-            "poll.update",
-            {
-                "id": 1,
-                "votesvalid": "10.000000",
-                "votesinvalid": "11.000000",
-                "votescast": "3.000000",
-            },
-        )
-        self.assert_status_code(response, 400)
-        assert (
-            "Following options are not allowed in this state and type: "
-            "votesvalid, votesinvalid, votescast"
-        ) in response.data.decode()
-
-
 class UpdatePollTestCase(BaseActionTestCase):
     def setUp(self) -> None:
         super().setUp()
@@ -142,6 +43,77 @@ class UpdatePollTestCase(BaseActionTestCase):
                 "group_$_ids": ["113"],
             },
         )
+
+    def test_catch_not_allowed(self) -> None:
+        self.update_model("poll/1", {"state": "started"})
+        response = self.request(
+            "poll.update",
+            {
+                "id": 1,
+                "pollmethod": "Y",
+                "type": "analog",
+                "min_votes_amount": 1,
+                "max_votes_amount": 1,
+                "global_yes": False,
+                "global_no": True,
+                "global_abstain": True,
+            },
+        )
+        self.assert_status_code(response, 400)
+        assert (
+            "Following options are not allowed in this state and type: "
+            "pollmethod, type, min_votes_amount, max_votes_amount, global_yes,"
+            " global_no, global_abstain"
+        ) in response.data.decode()
+
+    def test_optional_state_created(self) -> None:
+        response = self.request(
+            "poll.update",
+            {
+                "id": 1,
+                "pollmethod": "Y",
+                "type": "analog",
+                "min_votes_amount": 1,
+                "max_votes_amount": 1,
+                "global_yes": False,
+                "global_no": True,
+                "global_abstain": True,
+            },
+        )
+        self.assert_status_code(response, 200)
+        poll = self.get_model("poll/1")
+        assert poll.get("pollmethod") == "Y"
+        assert poll.get("type") == "analog"
+        assert poll.get("min_votes_amount") == 1
+        assert poll.get("max_votes_amount") == 1
+        assert poll.get("global_yes") is False
+        assert poll.get("global_no") is True
+        assert poll.get("global_abstain") is True
+
+    def test_not_allowed_for_analog(self) -> None:
+        self.update_model("poll/1", {"type": "analog"})
+        response = self.request("poll.update", {"id": 1, "entitled_group_ids": []})
+        self.assert_status_code(response, 400)
+        assert (
+            "Following options are not allowed in this state and type: "
+            "entitled_group_ids"
+        ) in response.data.decode()
+
+    def test_not_allowed_for_non_analog(self) -> None:
+        response = self.request(
+            "poll.update",
+            {
+                "id": 1,
+                "votesvalid": "10.000000",
+                "votesinvalid": "11.000000",
+                "votescast": "3.000000",
+            },
+        )
+        self.assert_status_code(response, 400)
+        assert (
+            "Following options are not allowed in this state and type: "
+            "votesvalid, votesinvalid, votescast"
+        ) in response.data.decode()
 
     def test_update_title(self) -> None:
         response = self.request(
