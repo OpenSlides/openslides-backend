02dbb3b1b5a8aab6a827decfe097dca45d51c3f5
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-02-18 15:25:58 +0100

    Remove one surplus test and merge all into one class.

--- tests/system/action/poll/test_anonymize.py
+++ tests/system/action/poll/test_anonymize.py
@@ -2,8 +2,9 @@ from openslides_backend.models.models import Poll
 from tests.system.action.base import BaseActionTestCase
 
 
-class PollAnonymizeActionTest(BaseActionTestCase):
-    def test_anonymize(self) -> None:
+class PollAnonymize(BaseActionTestCase):
+    def setUp(self) -> None:
+        super().setUp()
         self.set_models(
             {
                 "meeting/1": {},
@@ -26,6 +27,8 @@ class PollAnonymizeActionTest(BaseActionTestCase):
                 },
             }
         )
+
+    def test_anonymize(self) -> None:
         response = self.request("poll.anonymize", {"id": 1})
         self.assert_status_code(response, 200)
         poll = self.get_model("poll/1")
@@ -41,95 +44,6 @@ class PollAnonymizeActionTest(BaseActionTestCase):
         assert user.get("vote_delegated_vote_$1_ids") == []
 
 
-class AnonymizePoll(BaseActionTestCase):
-    def setUp(self) -> None:
-        super().setUp()
-        self.create_model(
-            "assignment/1",
-            {
-                "title": "test_assignment_QLydMOqkyOHG68yZFJxl",
-                "open_posts": 1,
-                "candidate_ids": [1],
-            },
-        )
-        self.create_model(
-            "poll/1",
-            {
-                "content_object_id": "assignment/1",
-                "title": "test_title_3LbUCNirKirpJhRHRxzW",
-                "pollmethod": "YNA",
-                "type": Poll.TYPE_NAMED,
-                "state": Poll.STATE_FINISHED,
-                "option_ids": [11],
-                "meeting_id": 113,
-                "voted_ids": [1, 2],
-                "votesvalid": "2.000000",
-                "votesinvalid": "0.000000",
-                "votescast": "2.000000",
-            },
-        )
-        self.create_model(
-            "option/11",
-            {
-                "meeting_id": 113,
-                "poll_id": 1,
-                "yes": "1.000000",
-                "no": "1.000000",
-                "abstain": "0.000000",
-                "vote_ids": [1, 2],
-            },
-        )
-        self.create_model("meeting/113", {"name": "my meeting"})
-
-        self.create_model(
-            "vote/1",
-            {
-                "meeting_id": 113,
-                "user_id": 1,
-                "option_id": 11,
-                "value": "Y",
-                "weight": "1.000000",
-            },
-        )
-        self.update_model(
-            "user/1",
-            {
-                "vote_$_ids": ["113"],
-                "vote_$113_ids": [1],
-            },
-        )
-        self.create_model(
-            "user/2",
-            {"username": "test_user_2", "vote_$113_ids": [2], "vote_$_ids": ["113"]},
-        )
-        self.create_model(
-            "vote/2",
-            {
-                "meeting_id": 113,
-                "user_id": 2,
-                "option_id": 11,
-                "value": "N",
-                "weight": "1.000000",
-            },
-        )
-
-    def test_anonymize_poll(self) -> None:
-        response = self.request("poll.anonymize", {"id": 1})
-        self.assert_status_code(response, 200)
-        poll = self.get_model("poll/1")
-        self.assertEqual(poll.get("votesvalid"), "2.000000")
-        self.assertEqual(poll.get("votesinvalid"), "0.000000")
-        self.assertEqual(poll.get("votescast"), "2.000000")
-        option = self.get_model("option/11")
-        self.assertEqual(option.get("yes"), "1.000000")
-        self.assertEqual(option.get("no"), "1.000000")
-        self.assertEqual(option.get("abstain"), "0.000000")
-        self.assertTrue(1 in poll.get("voted_ids", []))
-        self.assertTrue(2 in poll.get("voted_ids", []))
-        for vote_fqid in ("vote/1", "vote/2"):
-            vote = self.get_model(vote_fqid)
-            assert vote.get("user_id") is None
-
     def test_anonymize_wrong_state(self) -> None:
         self.update_model("poll/1", {"state": Poll.STATE_CREATED})
         response = self.request("poll.anonymize", {"id": 1})
@@ -137,6 +51,7 @@ class AnonymizePoll(BaseActionTestCase):
         for vote_fqid in ("vote/1", "vote/2"):
             vote = self.get_model(vote_fqid)
             assert vote.get("user_id")
+            assert vote.get("delegated_user_id")
 
     def test_anonymize_wrong_type(self) -> None:
         self.update_model("poll/1", {"type": Poll.TYPE_ANALOG})
@@ -145,3 +60,4 @@ class AnonymizePoll(BaseActionTestCase):
         for vote_fqid in ("vote/1", "vote/2"):
             vote = self.get_model(vote_fqid)
             assert vote.get("user_id")
+            assert vote.get("delegated_user_id")
