d7c6a5857df362c099710db2dd8a2b5b6f0d6e43
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-02-19 10:45:08 +0100

    Prevent to delete used_as_reference projectors.

--- tests/system/action/projector/test_delete.py
+++ tests/system/action/projector/test_delete.py
@@ -3,14 +3,33 @@ from tests.system.action.base import BaseActionTestCase
 
 class ProjectorDelete(BaseActionTestCase):
     def test_delete_correct(self) -> None:
-        self.create_model("projector/111", {"name": "name_srtgb123"})
+        self.set_models({"projector/111": {"name": "name_srtgb123"}})
         response = self.request("projector.delete", {"id": 111})
         self.assert_status_code(response, 200)
         self.assert_model_deleted("projector/111")
 
     def test_delete_wrong_id(self) -> None:
-        self.create_model("projector/112", {"name": "name_srtgb123"})
+        self.set_models({"projector/112": {"name": "name_srtgb123"}})
         response = self.request("projector.delete", {"id": 111})
         self.assert_status_code(response, 400)
         model = self.get_model("projector/112")
+        assert model.get("name") == "name_srtgb123"
+
+    def test_delete_prevent_if_used_as_reference(self) -> None:
+        self.set_models(
+            {
+                "projector/111": {
+                    "name": "name_srtgb123",
+                    "used_as_reference_projector_meeting_id": 1,
+                },
+                "meeting/1": {"reference_projector_id": 111},
+            }
+        )
+        response = self.request("projector.delete", {"id": 111})
+        self.assert_status_code(response, 400)
+        assert (
+            "A used as reference projector is not allowed to delete."
+            in response.data.decode()
+        )
+        model = self.get_model("projector/111")
         assert model.get("name") == "name_srtgb123"
