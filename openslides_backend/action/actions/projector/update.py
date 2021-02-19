e3e9b728bf91f08572ea54627f04835edcd89ceb
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-02-19 13:35:56 +0100

    Add projector.update action. Add tests.

---
+++ openslides_backend/action/actions/projector/update.py
@@ -0,0 +1,31 @@
+from ....models.models import Projector
+from ...generics.update import UpdateAction
+from ...util.default_schema import DefaultSchema
+from ...util.register import register_action
+
+
+@register_action("projector.update")
+class ProjectorUpdate(UpdateAction):
+    """
+    Action to update a projector.
+    """
+
+    model = Projector()
+    schema = DefaultSchema(Projector()).get_update_schema(
+        optional_properties=[
+            "name",
+            "width",
+            "aspect_ratio_numerator",
+            "aspect_ratio_denominator",
+            "color",
+            "background_color",
+            "header_background_color",
+            "header_font_color",
+            "header_h1_color",
+            "chyron_background_color",
+            "chyron_font_color",
+            "show_header_footer",
+            "show_title",
+            "show_logo",
+        ]
+    )
