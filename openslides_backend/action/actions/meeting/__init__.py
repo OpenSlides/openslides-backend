d36bedb6c20296e3c2268fe20a9d04c605ba3ced
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-02-23 09:25:43 +0100

    Add replace projector id action. Draft version.

--- openslides_backend/action/actions/meeting/__init__.py
+++ openslides_backend/action/actions/meeting/__init__.py
@@ -1,6 +1,7 @@
 from . import (  # noqa
     create_update_delete,
     delete_all_speakers_of_all_lists,
+    replace_projector_id,
     set_font,
     set_logo,
     unset_font,
--- openslides_backend/action/actions/projector/delete.py
+++ openslides_backend/action/actions/projector/delete.py
@@ -6,6 +6,7 @@ from ....shared.patterns import FullQualifiedId
 from ...generics.delete import DeleteAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
+from ..meeting.replace_projector_id import MeetingReplaceProjectorId
 
 
 @register_action("projector.delete")
@@ -20,10 +21,13 @@ class ProjectorDelete(DeleteAction):
     def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
         projector = self.datastore.get(
             FullQualifiedId(self.model.collection, instance["id"]),
-            ["used_as_reference_projector_meeting_id"],
+            ["used_as_reference_projector_meeting_id", "meeting_id"],
         )
         if projector.get("used_as_reference_projector_meeting_id"):
             raise ActionException(
                 "A used as reference projector is not allowed to delete."
             )
+
+        payload = [{"id": projector.get("meeting_id"), "projector_id": instance["id"]}]
+        self.execute_other_action(MeetingReplaceProjectorId, payload)
         return instance
