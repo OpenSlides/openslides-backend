a58c2455edb421232a1a073d87fc9065805f7b1f
reiterl <ludwig.reiter@intevation.de>
2021-02-25 15:22:05 +0100

    Add projection.delete action and tests. (#499)
    * Add projection.delete action and tests.
    
    * Allow delete if preview_projector_id is set.

---
+++ openslides_backend/action/actions/projection/delete.py
@@ -0,0 +1,32 @@
+from typing import Any, Dict
+
+from ....models.models import Projection
+from ....shared.exceptions import ActionException
+from ....shared.patterns import FullQualifiedId
+from ...generics.delete import DeleteAction
+from ...util.default_schema import DefaultSchema
+from ...util.register import register_action
+
+
+@register_action("projection.delete")
+class ProjectionDelete(DeleteAction):
+    """
+    Action to delete a projection.
+    """
+
+    model = Projection()
+    schema = DefaultSchema(Projection()).get_delete_schema()
+
+    def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
+        projection = self.datastore.get(
+            FullQualifiedId(self.model.collection, instance["id"]),
+            ["current_projector_id", "preview_projector_id"],
+        )
+        if not (
+            projection.get("current_projector_id")
+            or projection.get("preview_projector_id")
+        ):
+            raise ActionException(
+                "Projection must have a current_projector_id or a preview_projector_id."
+            )
+        return instance
---
+++ tests/system/action/projection/test_delete.py
@@ -0,0 +1,39 @@
+from tests.system.action.base import BaseActionTestCase
+
+
+class ProjectionDelete(BaseActionTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.set_models(
+            {
+                "meeting/112": {"all_projection_ids": [12, 13], "projector_ids": [1]},
+                "projector/1": {
+                    "current_projection_ids": [12],
+                    "meeting_id": 112,
+                    "preview_projection_ids": [13],
+                    "history_projection_ids": [14],
+                },
+                "projection/12": {"current_projector_id": 1, "meeting_id": 112},
+                "projection/13": {"preview_projector_id": 1, "meeting_id": 112},
+                "projection/14": {"history_projector_id": 1, "meeting_id": 112},
+            }
+        )
+
+    def test_delete_current_correct(self) -> None:
+        response = self.request("projection.delete", {"id": 12})
+        self.assert_status_code(response, 200)
+        self.assert_model_deleted("projection/12")
+
+    def test_delete_preview_correct(self) -> None:
+        response = self.request("projection.delete", {"id": 13})
+        self.assert_status_code(response, 200)
+        self.assert_model_deleted("projection/13")
+
+    def test_delete_history_not_allowed(self) -> None:
+        response = self.request("projection.delete", {"id": 14})
+        self.assert_status_code(response, 400)
+        assert (
+            "Projection must have a current_projector_id or a preview_projector_id."
+            in response.data.decode()
+        )
+        self.assert_model_exists("projection/13")
