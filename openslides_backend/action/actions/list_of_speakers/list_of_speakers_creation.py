8cffee139545f1a786a70b8936f2c1a68b235c4a
Ralf Peschke <rpeschke@peschke-it.de>
2021-03-04 18:03:19 +0100

    Reformat

--- openslides_backend/action/actions/list_of_speakers/list_of_speakers_creation.py
+++ openslides_backend/action/actions/list_of_speakers/list_of_speakers_creation.py
@@ -16,6 +16,8 @@ class CreateActionWithListOfSpeakersMixin(BaseAction):
     def get_dependent_action_payload_list_of_speakers(
         self, instance: Dict[str, Any], CreateActionClass: Type[Action]
     ) -> List[Dict[str, Any]]:
-        return [{
-            "content_object_id": f"{str(self.model.collection)}{KEYSEPARATOR}{instance['id']}",
-        }]
+        return [
+            {
+                "content_object_id": f"{str(self.model.collection)}{KEYSEPARATOR}{instance['id']}",
+            }
+        ]
