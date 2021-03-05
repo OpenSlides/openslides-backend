9cfb4444dfe8a7ed3ad37b1d9c96d3343007d0fd
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2021-03-05 09:09:16 +0100

    Fix tests

--- tests/system/action/tag/test_delete.py
+++ tests/system/action/tag/test_delete.py
@@ -3,13 +3,23 @@ from tests.system.action.base import BaseActionTestCase
 
 class TagDeleteTest(BaseActionTestCase):
     def test_delete_correct(self) -> None:
-        self.create_model("tag/111", {"name": "name_srtgb123"})
+        self.set_models(
+            {
+                "meeting/1": {"tag_ids": [111]},
+                "tag/111": {"name": "name_srtgb123", "meeting_id": 1},
+            }
+        )
         response = self.request("tag.delete", {"id": 111})
         self.assert_status_code(response, 200)
         self.assert_model_not_exists("tag/112")
 
     def test_delete_wrong_id(self) -> None:
-        self.create_model("tag/112", {"name": "name_srtgb123"})
+        self.set_models(
+            {
+                "meeting/1": {"tag_ids": [112]},
+                "tag/112": {"name": "name_srtgb123", "meeting_id": 1},
+            }
+        )
         response = self.request("tag.delete", {"id": 111})
         self.assert_status_code(response, 400)
         self.assert_model_exists("tag/112")
@@ -17,7 +27,12 @@ class TagDeleteTest(BaseActionTestCase):
     def test_delete_correct_2(self) -> None:
         self.set_models(
             {
-                "tag/111": {"name": "name_srtgb123", "tagged_ids": ["topic/222"]},
+                "meeting/1": {"tag_ids": [111]},
+                "tag/111": {
+                    "name": "name_srtgb123",
+                    "meeting_id": 1,
+                    "tagged_ids": ["topic/222"],
+                },
                 "topic/222": {"title": "test_title_ertgd590854398", "tag_ids": [111]},
             }
         )
