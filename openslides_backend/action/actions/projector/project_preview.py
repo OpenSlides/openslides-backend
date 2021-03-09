c99e4559f092595aebae18e7b01d8aaaf2b7ea10
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-03-09 15:37:17 +0100

    Add projector.project_preview and tests.

---
+++ openslides_backend/action/actions/projector/project_preview.py
@@ -0,0 +1,110 @@
+from typing import Any, Dict, List
+
+from ....models.models import Projection, Projector
+from ....services.datastore.commands import GetManyRequest
+from ....shared.filters import And, FilterOperator
+from ....shared.patterns import Collection, FullQualifiedId
+from ...generics.update import UpdateAction
+from ...util.default_schema import DefaultSchema
+from ...util.register import register_action
+from ...util.typing import ActionData
+from ..projection.set_weight import ProjectionSetWeight
+
+
+@register_action("projector.project_preview")
+class ProjectorProjectPreview(UpdateAction):
+    """
+    Action to get to the next projection.
+    """
+
+    model = Projector()
+    schema = DefaultSchema(Projection()).get_update_schema()
+
+    def get_updated_instances(self, payload: ActionData) -> ActionData:
+        for instance in payload:
+            projection_id = instance.pop("id")
+            projection = self.datastore.get(
+                FullQualifiedId(Collection("projection"), projection_id),
+                ["preview_projector_id"],
+            )
+            # check if projection is from a preview projector
+            if not projection.get("preview_projector_id"):
+                continue
+            projector_id = projection["preview_projector_id"]
+            projector = self.datastore.get(
+                FullQualifiedId(self.model.collection, projector_id),
+                [
+                    "current_projection_ids",
+                    "preview_projection_ids",
+                    "history_projection_ids",
+                    "meeting_id",
+                ],
+            )
+            # move current unstable projections to history
+            current_projections = []
+            if projector.get("current_projection_ids"):
+                gmr = GetManyRequest(
+                    Collection("projection"),
+                    projector["current_projection_ids"],
+                    ["id", "stable"],
+                )
+                result = self.datastore.get_many([gmr])
+                current_projections = list(
+                    result.get(Collection("projection"), {}).values()
+                )
+            new_current_projection_ids = [
+                projection["id"]
+                for projection in current_projections
+                if self.is_stable(projection)
+            ]
+            transfer_to_history_projection_ids = [
+                projection["id"]
+                for projection in current_projections
+                if not self.is_stable(projection)
+            ]
+            new_history_projection_ids = (
+                projector.get("history_projection_ids", [])
+                + transfer_to_history_projection_ids
+            )
+            self.set_weight_to_projection(
+                transfer_to_history_projection_ids,
+                projector["meeting_id"],
+                projector_id,
+            )
+            new_current_projection_ids += [projection_id]
+            new_preview_projection_ids = [
+                id_
+                for id_ in projector["preview_projection_ids"]
+                if id_ != projection_id
+            ]
+            instance["id"] = projector_id
+            instance["current_projection_ids"] = new_current_projection_ids
+            instance["preview_projection_ids"] = new_preview_projection_ids
+            instance["history_projection_ids"] = new_history_projection_ids
+            yield instance
+
+    def is_stable(self, value: Dict[str, Any]) -> bool:
+        return bool(value.get("stable"))
+
+    def get_max_projection_weight(self, meeting_id: int, projector_id: int) -> int:
+        filter_ = And(
+            FilterOperator("meeting_id", "=", meeting_id),
+            FilterOperator("history_projector_id", "=", projector_id),
+        )
+        maximum = self.datastore.max(Collection("projection"), filter_, "weight", "int")
+        if maximum is None:
+            maximum = 1
+        return maximum
+
+    def set_weight_to_projection(
+        self, projection_ids: List[int], meeting_id: int, projector_id: int
+    ) -> None:
+        max_weight = self.get_max_projection_weight(meeting_id, projector_id)
+        increment = 1
+        payload_set_weight = []
+        for projection_id in projection_ids:
+            payload_set_weight.append(
+                {"id": projection_id, "weight": max_weight + increment}
+            )
+            increment += 1
+        self.execute_other_action(ProjectionSetWeight, payload_set_weight)
---
+++ tests/system/action/projector/test_project_preview.py
@@ -0,0 +1,83 @@
+from tests.system.action.base import BaseActionTestCase
+
+
+class ProjectorProjectPreview(BaseActionTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.set_models(
+            {
+                "meeting/1": {},
+                "projector/2": {"meeting_id": 1},
+                "projector/3": {
+                    "current_projection_ids": [1, 2],
+                    "preview_projection_ids": [3, 4],
+                    "history_projection_ids": [6],
+                    "meeting_id": 1,
+                },
+                "projector/4": {
+                    "current_projection_ids": [],
+                    "preview_projection_ids": [5],
+                    "history_projection_ids": [],
+                    "meeting_id": 1,
+                },
+                "projection/1": {
+                    "current_projector_id": 3,
+                    "meeting_id": 1,
+                    "weight": 100,
+                    "stable": True,
+                },
+                "projection/2": {
+                    "current_projector_id": 3,
+                    "meeting_id": 1,
+                    "weight": 98,
+                },
+                "projection/3": {
+                    "preview_projector_id": 3,
+                    "meeting_id": 1,
+                    "weight": 99,
+                },
+                "projection/4": {
+                    "preview_projector_id": 3,
+                    "meeting_id": 1,
+                    "weight": 100,
+                },
+                "projection/5": {
+                    "preview_projector_id": 4,
+                    "meeting_id": 1,
+                    "weight": 100,
+                },
+                "projection/6": {
+                    "history_projector_id": 3,
+                    "meeting_id": 1,
+                    "weight": 50,
+                },
+            }
+        )
+
+    def test_project_preview_nothing(self) -> None:
+        response = self.request("projector.project_preview", {"id": 2})
+        self.assert_status_code(response, 200)
+        projector = self.get_model("projector/3")
+        assert projector.get("current_projection_ids") == [1, 2]
+        assert projector.get("preview_projection_ids") == [3, 4]
+        assert projector.get("history_projection_ids") == [6]
+
+    def test_project_preview_complex(self) -> None:
+        response = self.request("projector.project_preview", {"id": 3})
+        self.assert_status_code(response, 200)
+        projector = self.get_model("projector/3")
+        assert projector.get("current_projection_ids") == [1, 3]
+        assert projector.get("preview_projection_ids") == [4]
+        assert projector.get("history_projection_ids") == [6, 2]
+        projection_1 = self.get_model("projection/1")
+        assert projection_1.get("weight") == 100
+        projection_2 = self.get_model("projection/2")
+        assert projection_2.get("weight") == 51
+
+    def test_project_preview_just_preview(self) -> None:
+        response = self.request("projector.project_preview", {"id": 5})
+        self.assert_status_code(response, 200)
+        projector = self.get_model("projector/4")
+        assert projector.get("current_projection_ids") == [5]
+        assert projector.get("preview_projection_ids") == []
+        assert projector.get("history_projection_ids") == []
