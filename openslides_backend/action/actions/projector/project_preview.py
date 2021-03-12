837f186066ff24ae9e97b0ff8a3f2c10e8db10a3
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-03-12 08:51:48 +0100

    Update preview projector check, throw error if missing.

--- openslides_backend/action/actions/projector/project_preview.py
+++ openslides_backend/action/actions/projector/project_preview.py
@@ -2,12 +2,12 @@ from typing import Any, Dict, List
 
 from ....models.models import Projection, Projector
 from ....services.datastore.commands import GetManyRequest
+from ....shared.exceptions import ActionException
 from ....shared.filters import And, FilterOperator
 from ....shared.patterns import Collection, FullQualifiedId
 from ...generics.update import UpdateAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
-from ...util.typing import ActionData
 from ..projection.set_weight import ProjectionSetWeight
 
 
@@ -20,68 +20,65 @@ class ProjectorProjectPreview(UpdateAction):
     model = Projector()
     schema = DefaultSchema(Projection()).get_update_schema()
 
-    def get_updated_instances(self, payload: ActionData) -> ActionData:
-        for instance in payload:
-            projection_id = instance.pop("id")
-            projection = self.datastore.get(
-                FullQualifiedId(Collection("projection"), projection_id),
-                ["preview_projector_id"],
+    def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
+        projection_id = instance.pop("id")
+        projection = self.datastore.get(
+            FullQualifiedId(Collection("projection"), projection_id),
+            ["preview_projector_id"],
+        )
+        # check if projection is from a preview projector
+        if not projection.get("preview_projector_id"):
+            raise ActionException("Projection has not a preview_projector_id.")
+        projector_id = projection["preview_projector_id"]
+        projector = self.datastore.get(
+            FullQualifiedId(self.model.collection, projector_id),
+            [
+                "current_projection_ids",
+                "preview_projection_ids",
+                "history_projection_ids",
+                "meeting_id",
+            ],
+        )
+        # move current unstable projections to history
+        current_projections = []
+        if projector.get("current_projection_ids"):
+            gmr = GetManyRequest(
+                Collection("projection"),
+                projector["current_projection_ids"],
+                ["id", "stable"],
             )
-            # check if projection is from a preview projector
-            if not projection.get("preview_projector_id"):
-                continue
-            projector_id = projection["preview_projector_id"]
-            projector = self.datastore.get(
-                FullQualifiedId(self.model.collection, projector_id),
-                [
-                    "current_projection_ids",
-                    "preview_projection_ids",
-                    "history_projection_ids",
-                    "meeting_id",
-                ],
+            result = self.datastore.get_many([gmr])
+            current_projections = list(
+                result.get(Collection("projection"), {}).values()
             )
-            # move current unstable projections to history
-            current_projections = []
-            if projector.get("current_projection_ids"):
-                gmr = GetManyRequest(
-                    Collection("projection"),
-                    projector["current_projection_ids"],
-                    ["id", "stable"],
-                )
-                result = self.datastore.get_many([gmr])
-                current_projections = list(
-                    result.get(Collection("projection"), {}).values()
-                )
-            new_current_projection_ids = [
-                projection["id"]
-                for projection in current_projections
-                if self.is_stable(projection)
-            ]
-            transfer_to_history_projection_ids = [
-                projection["id"]
-                for projection in current_projections
-                if not self.is_stable(projection)
-            ]
-            new_history_projection_ids = (
-                projector.get("history_projection_ids", [])
-                + transfer_to_history_projection_ids
-            )
-            self.set_weight_to_projection(
-                transfer_to_history_projection_ids,
-                projector["meeting_id"],
-                projector_id,
-            )
-            new_current_projection_ids += [projection_id]
-            new_preview_projection_ids = [
-                id_
-                for id_ in projector["preview_projection_ids"]
-                if id_ != projection_id
-            ]
-            instance["id"] = projector_id
-            instance["current_projection_ids"] = new_current_projection_ids
-            instance["preview_projection_ids"] = new_preview_projection_ids
-            instance["history_projection_ids"] = new_history_projection_ids
-            yield instance
+        new_current_projection_ids = [
+            projection["id"]
+            for projection in current_projections
+            if self.is_stable(projection)
+        ]
+        transfer_to_history_projection_ids = [
+            projection["id"]
+            for projection in current_projections
+            if not self.is_stable(projection)
+        ]
+        new_history_projection_ids = (
+            projector.get("history_projection_ids", [])
+            + transfer_to_history_projection_ids
+        )
+        self.set_weight_to_projection(
+            transfer_to_history_projection_ids,
+            projector["meeting_id"],
+            projector_id,
+        )
+        new_current_projection_ids += [projection_id]
+        new_preview_projection_ids = [
+            id_ for id_ in projector["preview_projection_ids"] if id_ != projection_id
+        ]
+        instance["id"] = projector_id
+        instance["current_projection_ids"] = new_current_projection_ids
+        instance["preview_projection_ids"] = new_preview_projection_ids
+        instance["history_projection_ids"] = new_history_projection_ids
+        return instance
 
     def is_stable(self, value: Dict[str, Any]) -> bool:
         return value.get("stable", False)
