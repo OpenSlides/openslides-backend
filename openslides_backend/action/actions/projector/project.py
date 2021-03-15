41126dfc2660c824fd9e572a19fb579e9431d06f
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-03-15 13:33:20 +0100

    Add move_equal_projections_to_history. Update tests.

--- openslides_backend/action/actions/projector/project.py
+++ openslides_backend/action/actions/projector/project.py
@@ -1,11 +1,15 @@
+from typing import Any, Dict
+
 from ....models.models import Projection, Projector
-from ....shared.patterns import string_to_fqid
+from ....shared.filters import And, FilterOperator, Not
+from ....shared.patterns import Collection, string_to_fqid
 from ....shared.schema import required_id_schema
 from ...generics.update import UpdateAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
 from ...util.typing import ActionData
 from ..projection.create import ProjectionCreate
+from ..projection.update import ProjectionUpdate
 
 
 @register_action("projector.project")
@@ -30,6 +34,10 @@ class ProjectorProject(UpdateAction):
                 string_to_fqid(instance["content_object_id"]), ["meeting_id"]
             )
             meeting_id = content_object["meeting_id"]
+
+            if instance.get("stable", False) is False:
+                self.move_equal_projections_to_history(instance, meeting_id)
+
             create_data = []
             for projector_id in instance["ids"]:
                 data_dict = {
@@ -43,3 +51,31 @@ class ProjectorProject(UpdateAction):
             if create_data:
                 self.execute_other_action(ProjectionCreate, create_data)
         return []
+
+    def move_equal_projections_to_history(
+        self, instance: Dict[str, Any], meeting_id: int
+    ) -> None:
+        filter_ = And(
+            FilterOperator("meeting_id", "=", meeting_id),
+            FilterOperator("content_object_id", "=", instance["content_object_id"]),
+            FilterOperator("stable", "=", False),
+            FilterOperator("type", "=", None),
+            *[Not(FilterOperator("id", "=", id_)) for id_ in instance["ids"]],
+        )
+        result = self.datastore.filter(
+            Collection("projection"), filter_, ["id", "current_projector_id"]
+        )
+        action_data = []
+        for projection_id in result:
+            if result[projection_id]["current_projector_id"]:
+                action_data.append(
+                    {
+                        "id": int(projection_id),
+                        "current_projector_id": None,
+                        "history_projector_id": result[projection_id][
+                            "current_projector_id"
+                        ],
+                    }
+                )
+        if action_data:
+            self.execute_other_action(ProjectionUpdate, action_data)
--- tests/system/action/projector/test_project.py
+++ tests/system/action/projector/test_project.py
@@ -10,6 +10,19 @@ class ProjectorProject(BaseActionTestCase):
                 "meeting/2": {},
                 "projector/23": {"meeting_id": 1},
                 "projector/65": {"meeting_id": 1},
+                "projector/75": {"meeting_id": 1, "current_projection_ids": [110, 111]},
+                "projection/110": {
+                    "meeting_id": 1,
+                    "content_object_id": "assignment/453",
+                    "current_projector_id": 75,
+                    "stable": False
+                },
+                "projection/111": {
+                    "meeting_id": 1,
+                    "content_object_id": "assignment/453",
+                    "current_projector_id": 75,
+                    "stable": True,
+                },
                 "assignment/453": {"meeting_id": 1},
                 "assignment/567": {"meeting_id": 2},
             }
@@ -22,37 +35,41 @@ class ProjectorProject(BaseActionTestCase):
                 "ids": [23],
                 "content_object_id": "assignment/453",
                 "options": "",
-                "stable": True,
+                "stable": False,
                 "type": "test",
             },
         )
         self.assert_status_code(response, 200)
-        projection = self.get_model("projection/1")
+        projection = self.get_model("projection/112")
         assert projection.get("current_projector_id") == 23
         assert projection.get("content_object_id") == "assignment/453"
         assert projection.get("options") == ""
-        assert projection.get("stable") is True
+        assert projection.get("stable") is False
         assert projection.get("type") == "test"
+        projector_75 = self.get_model("projector/75")
+        assert projector_75.get("current_projection_ids") == [111]
+        assert projector_75.get("history_projection_ids") == [110]
 
     def test_project_2(self) -> None:
         response = self.request(
             "projector.project",
-            {"ids": [23, 65], "content_object_id": "assignment/453"},
+            {"ids": [23, 65], "content_object_id": "assignment/453", "stable": True},
         )
         self.assert_status_code(response, 200)
-        projection_1 = self.get_model("projection/1")
+        projection_1 = self.get_model("projection/112")
         assert projection_1.get("current_projector_id") == 23
         assert projection_1.get("content_object_id") == "assignment/453"
-        projection_2 = self.get_model("projection/2")
+        projection_2 = self.get_model("projection/113")
         assert projection_2.get("current_projector_id") == 65
         assert projection_2.get("content_object_id") == "assignment/453"
 
     def test_project_wrong_meeting(self) -> None:
         response = self.request(
-            "projector.project", {"ids": [23], "content_object_id": "assignment/567"}
+            "projector.project",
+            {"ids": [23], "content_object_id": "assignment/567", "stable": True},
         )
         self.assert_status_code(response, 400)
         assert (
-            "The relation current_projector_id requires the following fields to be equal:\\nprojection/1/meeting_id: 2\\nprojector/23/meeting_id: 1"
+            "The relation current_projector_id requires the following fields to be equal:\\nprojection/112/meeting_id: 2\\nprojector/23/meeting_id: 1"
             in response.data.decode()
         )
