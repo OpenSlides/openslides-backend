617c098777cbdaac6f32c928c5b7f06cf7c0bb5e
reiterl <ludwig.reiter@intevation.de>
2021-03-18 08:42:40 +0100

    Add projector.sort_preview and tests. (#553)

---
+++ openslides_backend/action/actions/projector/sort_preview.py
@@ -0,0 +1,49 @@
+from typing import Any, Dict
+
+from ....models.models import Projector
+from ....shared.exceptions import ActionException
+from ....shared.patterns import FullQualifiedId
+from ....shared.schema import required_id_schema
+from ...generics.update import UpdateAction
+from ...util.default_schema import DefaultSchema
+from ...util.register import register_action
+from ...util.typing import ActionData
+from ..projection.set_weight import ProjectionSetWeight
+
+
+@register_action("projector.sort_preview")
+class ProjectorSortPreview(UpdateAction):
+    """
+    Action to sort preview of a projector.
+    """
+
+    model = Projector()
+    schema = DefaultSchema(Projector()).get_update_schema(
+        additional_required_fields={
+            "projection_ids": {"type": "array", "items": required_id_schema}
+        },
+    )
+
+    def get_updated_instances(self, action_data: ActionData) -> ActionData:
+        for instance in action_data:
+            self.check_preview_ids(instance)
+
+            weight = 1
+            for projection_id in instance["projection_ids"]:
+                self.execute_other_action(
+                    ProjectionSetWeight, [{"id": projection_id, "weight": weight}]
+                )
+                weight += 1
+        return []
+
+    def check_preview_ids(self, instance: Dict[str, Any]) -> None:
+        projector = self.datastore.get(
+            FullQualifiedId(self.model.collection, instance["id"]),
+            ["preview_projection_ids"],
+        )
+        if set(instance["projection_ids"]) != set(
+            projector.get("preview_projection_ids", [])
+        ):
+            raise ActionException(
+                "Must give all preview projections of this projector and nothing else."
+            )
