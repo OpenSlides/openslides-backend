8a67a9fe5c68e387d5540a35c69eb10842063eae
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-03-19 14:03:59 +0100

    Couple coundown to poll.stop.

--- openslides_backend/action/actions/poll/stop.py
+++ openslides_backend/action/actions/poll/stop.py
@@ -2,10 +2,11 @@ from typing import Any, Dict
 
 from ....models.models import Poll
 from ....shared.exceptions import ActionException
-from ....shared.patterns import FullQualifiedId
+from ....shared.patterns import Collection, FullQualifiedId
 from ...generics.update import UpdateAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
+from ..projector_countdown.update import ProjectorCountdownUpdate
 
 
 @register_action("poll.stop")
@@ -19,11 +20,42 @@ class PollStopAction(UpdateAction):
 
     def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
         poll = self.datastore.get(
-            FullQualifiedId(self.model.collection, instance["id"]), ["state"]
+            FullQualifiedId(self.model.collection, instance["id"]),
+            ["state", "meeting_id"],
         )
         if poll.get("state") != Poll.STATE_STARTED:
             raise ActionException(
                 f"Cannot stop poll {instance['id']}, because it is not in state started."
             )
         instance["state"] = Poll.STATE_FINISHED
+
+        # reset countdown given by meeting
+        if poll.get("meeting_id"):
+            meeting = self.datastore.get(
+                FullQualifiedId(Collection("meeting"), poll["meeting_id"]),
+                [
+                    "poll_couple_countdown",
+                    "poll_countdown_id",
+                ],
+            )
+            if meeting.get("poll_couple_countdown") and meeting.get(
+                "poll_countdown_id"
+            ):
+                countdown = self.datastore.get(
+                    FullQualifiedId(
+                        Collection("projector_countdown"),
+                        meeting["poll_countdown_id"],
+                    ),
+                    ["default_time"],
+                )
+                self.execute_other_action(
+                    ProjectorCountdownUpdate,
+                    [
+                        {
+                            "id": meeting["poll_countdown_id"],
+                            "running": False,
+                            "countdown_time": countdown["default_time"],
+                        }
+                    ],
+                )
         return instance
