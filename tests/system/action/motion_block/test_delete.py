3fae23d16ba88c30fdccdcccfbe5df4a316a1856
reiterl <ludwig.reiter@intevation.de>
2021-03-24 11:47:04 +0100

    Add permissions to motion_block and update tests. (#599)

--- tests/system/action/motion_block/test_delete.py
+++ tests/system/action/motion_block/test_delete.py
@@ -3,13 +3,13 @@ from tests.system.action.base import BaseActionTestCase
 
 class MotionBlockActionTest(BaseActionTestCase):
     def test_delete_correct(self) -> None:
-        self.create_model("motion_block/111")
+        self.set_models({"meeting/11": {}, "motion_block/111": {"meeting_id": 11}})
         response = self.request("motion_block.delete", {"id": 111})
         self.assert_status_code(response, 200)
         self.assert_model_deleted("motion_block/111")
 
     def test_delete_wrong_id(self) -> None:
-        self.create_model("motion_block/112")
+        self.set_models({"meeting/11": {}, "motion_block/112": {"meeting_id": 11}})
         response = self.request("motion_block.delete", {"id": 111})
         self.assert_status_code(response, 400)
         self.assert_model_exists("motion_block/112")
@@ -17,17 +17,21 @@ class MotionBlockActionTest(BaseActionTestCase):
     def test_delete_correct_cascading(self) -> None:
         self.set_models(
             {
+                "meeting/12": {},
                 "motion_block/111": {
                     "list_of_speakers_id": 222,
                     "agenda_item_id": 333,
+                    "meeting_id": 12,
                 },
                 "list_of_speakers/222": {
                     "closed": False,
                     "content_object_id": "motion_block/111",
+                    "meeting_id": 12,
                 },
                 "agenda_item/333": {
                     "comment": "test_comment_ewoirzewoirioewr",
                     "content_object_id": "motion_block/111",
+                    "meeting_id": 12,
                 },
             }
         )
@@ -36,3 +40,10 @@ class MotionBlockActionTest(BaseActionTestCase):
         self.assert_model_deleted("motion_block/111")
         self.assert_model_deleted("agenda_item/333")
         self.assert_model_deleted("list_of_speakers/222")
+
+    def test_permissions(self) -> None:
+        self.base_permission_test(
+            {"meeting/11": {}, "motion_block/111": {"meeting_id": 11}},
+            "motion_block.delete",
+            {"id": 111},
+        )
