6e1ad3c74b2646ec1f41f930de2106ba36576307
Ralf Peschke <rpeschke@peschke-it.de>
2021-04-07 11:46:05 +0200

    Permissions for update_self

--- openslides_backend/action/actions/user/update_self.py
+++ openslides_backend/action/actions/user/update_self.py
@@ -2,6 +2,7 @@ from typing import Any, Dict
 
 from ....models.models import User
 from ....shared.exceptions import ActionException
+from ....shared.patterns import Collection, FullQualifiedId
 from ...generics.update import UpdateAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
@@ -16,7 +17,7 @@ class UserUpdate(UpdateAction, UserMixin):
 
     model = User()
     schema = DefaultSchema(User()).get_default_schema(
-        optional_properties=["username", "email"]
+        optional_properties=["username", "email", "about_me_$"]
     )
 
     def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
@@ -24,7 +25,26 @@ class UserUpdate(UpdateAction, UserMixin):
         Set id = user_id.
         """
         instance = super().update_instance(instance)
+        instance["id"] = self.user_id
+
+        if "about_me_$" in instance:
+            meeting_id_str = str(
+                self.datastore.get(
+                    FullQualifiedId(Collection("user"), self.user_id), ["meeting_id"]
+                ).get("meeting_id")
+            )
+            if meeting_id_str:
+                diff = [
+                    meeting
+                    for meeting in instance["about_me_$"].keys()
+                    if meeting != meeting_id_str
+                ]
+                if diff:
+                    raise ActionException(
+                        f"Temporary user may update about_me_$ only in his meeting, but tries in {diff}."
+                    )
+        return instance
+
+    def check_permissions(self, instance: Dict[str, Any]) -> None:
         if self.auth.is_anonymous(self.user_id):
             raise ActionException("Can't update for anonymous")
-        instance["id"] = self.user_id
-        return instance
