e420db62354ac4ea09bdf32ccdab144e3c893760
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-04-07 15:19:34 +0200

    Add permissions to speaker.delete and update tests.

--- openslides_backend/action/actions/speaker/create_update_delete.py
+++ openslides_backend/action/actions/speaker/create_update_delete.py
@@ -192,3 +192,12 @@ class SpeakerUpdate(UpdateAction):
 class SpeakerDeleteAction(DeleteAction):
     model = Speaker()
     schema = DefaultSchema(Speaker()).get_delete_schema()
+    permission = Permissions.ListOfSpeakers.CAN_MANAGE
+
+    def check_permissions(self, instance: Dict[str, Any]) -> None:
+        speaker = self.datastore.get(
+            FullQualifiedId(self.model.collection, instance["id"]), ["user_id"]
+        )
+        if speaker.get("user_id") == self.user_id:
+            return
+        super().check_permissions(instance)
--- tests/system/action/speaker/test_delete.py
+++ tests/system/action/speaker/test_delete.py
@@ -1,7 +1,32 @@
+from typing import Any, Dict
+
+from openslides_backend.permissions.permissions import Permissions
 from tests.system.action.base import BaseActionTestCase
+
+DEFAULT_PASSWORD = "password"
 
 
 class SpeakerDeleteActionTest(BaseActionTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.permission_test_model: Dict[str, Dict[str, Any]] = {
+            "meeting/1": {"speaker_ids": [890]},
+            "user/7": {
+                "username": "test_username1",
+                "speaker_$1_ids": [890],
+                "speaker_$_ids": ["1"],
+                "is_active": True,
+                "default_password": DEFAULT_PASSWORD,
+                "password": self.auth.hash(DEFAULT_PASSWORD),
+            },
+            "list_of_speakers/23": {"speaker_ids": [890], "meeting_id": 1},
+            "speaker/890": {
+                "user_id": 7,
+                "list_of_speakers_id": 23,
+                "meeting_id": 1,
+            },
+        }
+
     def test_delete_correct(self) -> None:
         self.set_models(
             {
@@ -46,3 +71,25 @@ class SpeakerDeleteActionTest(BaseActionTestCase):
         response = self.request("speaker.delete", {"id": 889})
         self.assert_status_code(response, 400)
         self.assert_model_exists("speaker/890")
+
+    def test_delete_no_permissions(self) -> None:
+        self.base_permission_test(
+            self.permission_test_model, "speaker.delete", {"id": 890}
+        )
+
+    def test_delete_permissions(self) -> None:
+        self.base_permission_test(
+            self.permission_test_model,
+            "speaker.delete",
+            {"id": 890},
+            Permissions.ListOfSpeakers.CAN_MANAGE,
+        )
+
+    def test_delete_self(self) -> None:
+        self.create_meeting()
+        self.user_id = 7
+        self.set_models(self.permission_test_model)
+        self.login(self.user_id)
+        self.set_user_groups(self.user_id, [3])
+        response = self.request("speaker.delete", {"id": 890})
+        self.assert_status_code(response, 200)
