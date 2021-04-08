74471872b4b3e9364ba5d3046e506dfbd062febd
reiterl <ludwig.reiter@intevation.de>
2021-04-08 08:53:20 +0200

    Couple countdown poll (#561)
    * Couple projector countdown to poll start.
    
    * Couple coundown to poll.stop.
    
    * Remove not needed if-clause.
    
    * Add mixin ControlCountdown.

--- openslides_backend/action/actions/poll/start.py
+++ openslides_backend/action/actions/poll/start.py
@@ -2,14 +2,15 @@ from typing import Any, Dict
 
 from ....models.models import Poll
 from ....shared.exceptions import ActionException
-from ....shared.patterns import FullQualifiedId
+from ....shared.patterns import Collection, FullQualifiedId
 from ...generics.update import UpdateAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
+from ..projector_countdown.mixins import CountdownControl
 
 
 @register_action("poll.start")
-class PollStartAction(UpdateAction):
+class PollStartAction(CountdownControl, UpdateAction):
     """
     Action to start a poll.
     """
@@ -19,11 +20,23 @@ class PollStartAction(UpdateAction):
 
     def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
         poll = self.datastore.get(
-            FullQualifiedId(self.model.collection, instance["id"]), ["state"]
+            FullQualifiedId(self.model.collection, instance["id"]),
+            ["state", "meeting_id"],
         )
         if poll.get("state") != Poll.STATE_CREATED:
             raise ActionException(
                 f"Cannot start poll {instance['id']}, because it is not in state created."
             )
         instance["state"] = Poll.STATE_STARTED
+
+        # restart projector countdown given by the meeting
+        meeting = self.datastore.get(
+            FullQualifiedId(Collection("meeting"), poll["meeting_id"]),
+            [
+                "poll_couple_countdown",
+                "poll_countdown_id",
+            ],
+        )
+        if meeting.get("poll_couple_countdown") and meeting.get("poll_countdown_id"):
+            self.control_countdown(meeting["poll_countdown_id"], "restart")
         return instance
--- tests/system/action/poll/test_stop.py
+++ tests/system/action/poll/test_stop.py
@@ -3,11 +3,24 @@ from tests.system.action.base import BaseActionTestCase
 
 class PollStopActionTest(BaseActionTestCase):
     def test_stop_correct(self) -> None:
-        self.create_model("poll/1", {"state": "started"})
+        self.set_models(
+            {
+                "poll/1": {"state": "started", "meeting_id": 1},
+                "meeting/1": {"poll_couple_countdown": True, "poll_countdown_id": 1},
+                "projector_countdown/1": {
+                    "running": True,
+                    "default_time": 60,
+                    "countdown_time": 30.0,
+                },
+            }
+        )
         response = self.request("poll.stop", {"id": 1})
         self.assert_status_code(response, 200)
         poll = self.get_model("poll/1")
         assert poll.get("state") == "finished"
+        countdown = self.get_model("projector_countdown/1")
+        assert countdown.get("running") is False
+        assert countdown.get("countdown_time") == 60
 
     def test_stop_wrong_state(self) -> None:
         self.create_model("poll/1", {"state": "published"})
