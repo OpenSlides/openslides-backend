3e449bc4a9987e35ebe366ec2790d88f5cc6dd05
reiterl <ludwig.reiter@intevation.de>
2021-04-15 12:45:24 +0200

    Draft version of projector.toggle_stable action. Update tests. (#650)
    * Draft version of proctor.toggle_stable action. Update tests.
    
    * Check all projector meeting_ids
    
    * Add meeting_id to payload and simplify the code, add assert meeting_id in content object and projectors.
    
    * Update layout.
    
    * Small code changes to cleanup code.

---
+++ openslides_backend/action/actions/projector/toggle_stable.py
@@ -0,0 +1,83 @@
+from typing import Any, Dict
+
+from ....models.models import Projection, Projector
+from ....permissions.permissions import Permissions
+from ....shared.filters import And, FilterOperator
+from ....shared.patterns import Collection, FullQualifiedId, string_to_fqid
+from ....shared.schema import required_id_schema
+from ...generics.update import UpdateAction
+from ...util.assert_belongs_to_meeting import assert_belongs_to_meeting
+from ...util.default_schema import DefaultSchema
+from ...util.register import register_action
+from ...util.typing import ActionData
+from ..projection.create import ProjectionCreate
+from ..projection.delete import ProjectionDelete
+
+
+@register_action("projector.toggle_stable")
+class ProjectorToggleStable(UpdateAction):
+    """
+    Action to toggle stable projectors.
+    """
+
+    model = Projector()
+    schema = DefaultSchema(Projection()).get_default_schema(
+        title="Projector toggle stable schema",
+        required_properties=["content_object_id", "meeting_id"],
+        optional_properties=["options", "type"],
+        additional_required_fields={
+            "ids": {
+                "type": "array",
+                "items": required_id_schema,
+                "uniqueItems": True,
+                "minItems": 1,
+            },
+        },
+    )
+    permission = Permissions.Projector.CAN_MANAGE
+
+    def get_updated_instances(self, action_data: ActionData) -> ActionData:
+        for instance in action_data:
+            # check meeting ids from projector ids and content_object
+            meeting_id = instance["meeting_id"]
+            fqid_content_object = string_to_fqid(instance["content_object_id"])
+            assert_belongs_to_meeting(
+                self.datastore,
+                [fqid_content_object]
+                + [
+                    FullQualifiedId(Collection("projector"), id)
+                    for id in instance["ids"]
+                ],
+                meeting_id,
+            )
+
+            for projector_id in instance["ids"]:
+                filter_ = And(
+                    FilterOperator("current_projector_id", "=", projector_id),
+                    FilterOperator(
+                        "content_object_id", "=", instance["content_object_id"]
+                    ),
+                    FilterOperator("stable", "=", True),
+                )
+                if instance.get("type"):
+                    filter_ = And(
+                        filter_, FilterOperator("type", "=", instance["type"])
+                    )
+                results = self.datastore.filter(
+                    Collection("projection"), filter_, ["id"]
+                )
+                if results:
+                    self.execute_other_action(
+                        ProjectionDelete, [{"id": id_} for id_ in results]
+                    )
+                else:
+                    data: Dict[str, Any] = {
+                        "current_projector_id": projector_id,
+                        "stable": True,
+                        "type": instance.get("type"),
+                        "content_object_id": instance["content_object_id"],
+                        "options": instance.get("options"),
+                        "meeting_id": meeting_id,
+                    }
+                    self.execute_other_action(ProjectionCreate, [data])
+        return []
