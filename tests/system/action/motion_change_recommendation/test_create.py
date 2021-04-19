20e21b93b06415e48e40a9fb686a6bdec480f763
jsangmeister <33004050+jsangmeister@users.noreply.github.com>
2021-04-19 12:49:25 +0200

    Add line validation to motion_change_recommendation.create (#666)

--- tests/system/action/motion_change_recommendation/test_create.py
+++ tests/system/action/motion_change_recommendation/test_create.py
@@ -1,11 +1,15 @@
 from tests.system.action.base import BaseActionTestCase
+from tests.util import Response
 
 
 class MotionChangeRecommendationActionTest(BaseActionTestCase):
-    def test_create_good_required_fields(self) -> None:
+    def setUp(self) -> None:
+        super().setUp()
         self.set_models(
             {"meeting/1": {"motion_ids": [233]}, "motion/233": {"meeting_id": 1}}
         )
+
+    def test_create_good_required_fields(self) -> None:
         response = self.request(
             "motion_change_recommendation.create",
             {
@@ -26,9 +30,6 @@ class MotionChangeRecommendationActionTest(BaseActionTestCase):
         assert int(str(model.get("creation_time"))) > 1600246886
 
     def test_create_good_all_fields(self) -> None:
-        self.set_models(
-            {"meeting/1": {"motion_ids": [233]}, "motion/233": {"meeting_id": 1}}
-        )
         response = self.request(
             "motion_change_recommendation.create",
             {
@@ -64,9 +65,6 @@ class MotionChangeRecommendationActionTest(BaseActionTestCase):
         )
 
     def test_create_wrong_field(self) -> None:
-        self.set_models(
-            {"meeting/1": {"motion_ids": [233]}, "motion/233": {"meeting_id": 1}}
-        )
         response = self.request(
             "motion_change_recommendation.create",
             {
@@ -83,17 +81,100 @@ class MotionChangeRecommendationActionTest(BaseActionTestCase):
             response.json["message"],
         )
 
-    def test_create_title_change_recommendation(self) -> None:
+
+class MotionChangeRecommendationLineValidationTest(BaseActionTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.crid = 0
         self.set_models(
             {"meeting/1": {"motion_ids": [233]}, "motion/233": {"meeting_id": 1}}
         )
-        response = self.request(
+
+    def create_change_recommendation(
+        self, line_from: int, line_to: int, motion_id: int = 233
+    ) -> None:
+        self.crid += 1
+        self.create_model(
+            f"motion_change_recommendation/{self.crid}",
+            {
+                "meeting_id": 1,
+                "motion_id": motion_id,
+                "line_from": line_from,
+                "line_to": line_to,
+            },
+        )
+
+    def cr_request(self, line_from: int, line_to: int) -> Response:
+        return self.request(
             "motion_change_recommendation.create",
             {
-                "line_from": 0,
-                "line_to": 0,
-                "text": "new_title",
+                "line_from": line_from,
+                "line_to": line_to,
+                "text": "text",
                 "motion_id": 233,
             },
         )
+
+    def test_create_title_change_recommendation(self) -> None:
+        response = self.cr_request(0, 0)
+        self.assert_status_code(response, 200)
+
+    def test_create_change_recommendation_to_lt_from(self) -> None:
+        response = self.cr_request(42, 24)
+        self.assert_status_code(response, 400)
+        self.assertIn(
+            "Starting line must be smaller than ending line.",
+            response.json["message"],
+        )
+
+    def test_create_change_recommendation_other_not_colliding(self) -> None:
+        self.create_change_recommendation(1, 5)
+        self.create_change_recommendation(10, 15)
+        response = self.cr_request(6, 9)
+        self.assert_status_code(response, 200)
+
+    def test_create_change_recommendation_other_colliding(self) -> None:
+        self.create_change_recommendation(1, 5)
+        response = self.cr_request(1, 5)
+        self.assert_status_code(response, 400)
+        self.assertIn(
+            "The recommendation collides with an existing one",
+            response.json["message"],
+        )
+
+    def test_create_change_recommendation_partial_overlap_1(self) -> None:
+        self.create_change_recommendation(1, 5)
+        response = self.cr_request(4, 10)
+        self.assert_status_code(response, 400)
+        self.assertIn(
+            "The recommendation collides with an existing one",
+            response.json["message"],
+        )
+
+    def test_create_change_recommendation_partial_overlap_2(self) -> None:
+        self.create_change_recommendation(5, 10)
+        response = self.cr_request(1, 6)
+        self.assert_status_code(response, 400)
+        self.assertIn(
+            "The recommendation collides with an existing one",
+            response.json["message"],
+        )
+
+    def test_create_change_recommendation_min_overlap_1(self) -> None:
+        self.create_change_recommendation(1, 5)
+        response = self.cr_request(5, 10)
+        self.assert_status_code(response, 200)
+
+    def test_create_change_recommendation_min_overlap_2(self) -> None:
+        self.create_change_recommendation(5, 10)
+        response = self.cr_request(1, 5)
+        self.assert_status_code(response, 400)
+        self.assertIn(
+            "The recommendation collides with an existing one",
+            response.json["message"],
+        )
+
+    def test_create_change_recommendation_other_motion_not_colliding(self) -> None:
+        self.create_change_recommendation(1, 5, motion_id=42)
+        response = self.cr_request(1, 5)
         self.assert_status_code(response, 200)
