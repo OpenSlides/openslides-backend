df71daa138a1c17d90d93a4b5d874956ada3bc15
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2021-04-19 16:38:09 +0200

    Remove whoami

--- openslides_backend/presenter/__init__.py
+++ openslides_backend/presenter/__init__.py
@@ -1,8 +1,2 @@
-from . import (  # noqa
-    check_mediafile_id,
-    check_resource_id,
-    get_users,
-    server_time,
-    whoami,
-)
+from . import check_mediafile_id, check_resource_id, get_users, server_time  # noqa
 from .presenter_interface import *  # noqa
