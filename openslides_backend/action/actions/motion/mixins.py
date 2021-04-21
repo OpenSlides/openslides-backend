b072fea7eff5236d274b9b32c61b216b9f30bdc8
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-04-21 10:49:48 +0200

    Move one guardian condition up in the code. Refactor submitters test.

--- openslides_backend/action/actions/motion/mixins.py
+++ openslides_backend/action/actions/motion/mixins.py
@@ -7,22 +7,17 @@ from ...action import Action
 
 class PermissionHelperMixin(Action):
     def is_allowed_and_submitter(self, submitter_ids: List[int], state_id: int) -> bool:
+        if not submitter_ids:
+            return False
         state = self.datastore.get(
             FullQualifiedId(Collection("motion_state"), state_id),
             ["allow_submitter_edit"],
         )
         if not state.get("allow_submitter_edit"):
             return False
-
-        if not submitter_ids:
-            return False
-
         get_many_request = GetManyRequest(
             Collection("motion_submitter"), submitter_ids, ["user_id"]
         )
         result = self.datastore.get_many([get_many_request])
         submitters = result.get(Collection("motion_submitter"), {}).values()
-        for submitter in submitters:
-            if self.user_id == submitter.get("user_id"):
-                return True
-        return False
+        return any(self.user_id == s.get("user_id") for s in submitters)
