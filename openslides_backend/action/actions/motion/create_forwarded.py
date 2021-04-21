47e6743e5f98831152c29a373cf7a68e04f4c0bb
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-04-21 14:00:12 +0200

    Add draft motion.create_forwarded and tests.

---
+++ openslides_backend/action/actions/motion/create_forwarded.py
@@ -0,0 +1,113 @@
+import time
+from typing import Any, Dict
+
+from ....models.models import Motion
+from ....shared.exceptions import ActionException
+from ....shared.patterns import Collection, FullQualifiedId
+from ...mixins.create_action_with_dependencies import CreateActionWithDependencies
+from ...util.default_schema import DefaultSchema
+from ...util.register import register_action
+from ..agenda_item.agenda_creation import CreateActionWithAgendaItemMixin
+from ..agenda_item.create import AgendaItemCreate
+from ..list_of_speakers.create import ListOfSpeakersCreate
+from ..list_of_speakers.list_of_speakers_creation import (
+    CreateActionWithListOfSpeakersMixin,
+)
+from ..motion_submitter.create import MotionSubmitterCreateAction
+from .sequential_numbers_mixin import SequentialNumbersMixin
+from .set_number_mixin import SetNumberMixin
+
+
+@register_action("motion.create_forwarded")
+class MotionCreate(
+    CreateActionWithDependencies,
+    CreateActionWithAgendaItemMixin,
+    SequentialNumbersMixin,
+    SetNumberMixin,
+    CreateActionWithListOfSpeakersMixin,
+):
+    """
+    Create action for forwarded motions.
+    """
+
+    model = Motion()
+    schema = DefaultSchema(Motion()).get_create_schema(
+        optional_properties=[
+            "reason",
+        ],
+        required_properties=["meeting_id", "title", "text", "origin_id"],
+    )
+    dependencies = [AgendaItemCreate, ListOfSpeakersCreate]
+
+    def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
+        meeting = self.datastore.get(
+            FullQualifiedId(Collection("meeting"), instance["meeting_id"]),
+            [
+                "motions_default_workflow_id",
+            ],
+        )
+
+        # calculate state_id from workflow_id
+        workflow_id = meeting.get("motions_default_workflow_id")
+        if workflow_id:
+            workflow = self.datastore.get(
+                FullQualifiedId(Collection("motion_workflow"), workflow_id),
+                ["first_state_id"],
+            )
+            instance["state_id"] = workflow.get("first_state_id")
+        else:
+            raise ActionException(
+                "No matching default workflow defined on this meeting"
+            )
+
+        # check for origin_id
+        if instance.get("origin_id"):
+            meeting = self.datastore.get(
+                FullQualifiedId(Collection("meeting"), instance["meeting_id"]),
+                ["committee_id"],
+            )
+            forwarded_from = self.datastore.get(
+                FullQualifiedId(Collection("motion"), instance["origin_id"]),
+                ["meeting_id"],
+            )
+            forwarded_from_meeting = self.datastore.get(
+                FullQualifiedId(Collection("meeting"), forwarded_from["meeting_id"]),
+                ["committee_id"],
+            )
+            committee = self.datastore.get(
+                FullQualifiedId(
+                    Collection("committee"), forwarded_from_meeting["committee_id"]
+                ),
+                ["forward_to_committee_ids"],
+            )
+            if meeting["committee_id"] not in committee.get(
+                "forward_to_committee_ids", []
+            ):
+                raise ActionException(
+                    f"Committee id {meeting['committee_id']} not in {committee.get('forward_to_committee_ids', [])}"
+                )
+
+        # create submitters
+        submitter_ids = [self.user_id]
+        self.apply_instance(instance)
+        action_data = []
+        for user_id in submitter_ids:
+            action_data.append({"motion_id": instance["id"], "user_id": user_id})
+        self.execute_other_action(MotionSubmitterCreateAction, action_data)
+
+        instance["sequential_number"] = self.get_sequential_number(
+            instance["meeting_id"]
+        )
+        # set created and last_modified
+        timestamp = round(time.time())
+        instance["created"] = timestamp
+        instance["last_modified"] = timestamp
+        self.set_number(
+            instance,
+            instance["meeting_id"],
+            instance["state_id"],
+            instance.get("lead_motion_id"),
+            instance.get("category_id"),
+        )
+
+        return instance
---
+++ tests/system/action/motion/test_create_forwarded.py
@@ -0,0 +1,84 @@
+from tests.system.action.base import BaseActionTestCase
+
+
+class MotionCreateActionTest(BaseActionTestCase):
+    def test_correct_origin_id_set(self) -> None:
+        self.set_models(
+            {
+                "meeting/221": {"name": "name_XDAddEAW", "committee_id": 53},
+                "meeting/222": {
+                    "name": "name_SNLGsvIV",
+                    "motions_default_workflow_id": 12,
+                    "committee_id": 52,
+                },
+                "motion_workflow/12": {
+                    "name": "name_workflow1",
+                    "first_state_id": 34,
+                    "state_ids": [34],
+                },
+                "motion_state/34": {"name": "name_state34", "meeting_id": 222},
+                "motion/12": {
+                    "title": "title_FcnPUXJB",
+                    "meeting_id": 221,
+                    "state_id": 34,
+                },
+                "committee/52": {"name": "name_EeKbwxpa"},
+                "committee/53": {
+                    "name": "name_auSwgfJC",
+                    "forward_to_committee_ids": [52],
+                },
+            }
+        )
+        response = self.request(
+            "motion.create_forwarded",
+            {
+                "title": "test_Xcdfgee",
+                "meeting_id": 222,
+                "origin_id": 12,
+                "text": "test",
+            },
+        )
+        self.assert_status_code(response, 200)
+        model = self.get_model("motion/13")
+        assert model.get("title") == "test_Xcdfgee"
+        assert model.get("meeting_id") == 222
+        assert model.get("origin_id") == 12
+
+    def test_correct_origin_id_wrong_1(self) -> None:
+        self.set_models(
+            {
+                "meeting/221": {"name": "name_XDAddEAW", "committee_id": 53},
+                "meeting/222": {
+                    "name": "name_SNLGsvIV",
+                    "motions_default_workflow_id": 12,
+                    "committee_id": 52,
+                },
+                "motion_workflow/12": {
+                    "name": "name_workflow1",
+                    "first_state_id": 34,
+                    "state_ids": [34],
+                },
+                "motion_state/34": {"name": "name_state34", "meeting_id": 222},
+                "motion/12": {
+                    "title": "title_FcnPUXJB",
+                    "meeting_id": 221,
+                    "state_id": 34,
+                },
+                "committee/52": {"name": "name_EeKbwxpa"},
+                "committee/53": {
+                    "name": "name_auSwgfJC",
+                    "forward_to_committee_ids": [],
+                },
+            }
+        )
+        response = self.request(
+            "motion.create_forwarded",
+            {
+                "title": "test_Xcdfgee",
+                "text": "text",
+                "meeting_id": 222,
+                "origin_id": 12,
+            },
+        )
+        self.assert_status_code(response, 400)
+        assert "Committee id 52 not in []" in response.json["message"]
