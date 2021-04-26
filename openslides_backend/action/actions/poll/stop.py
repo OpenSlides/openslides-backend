94d3c1f17edf3dcf76424b04b9978636190c4845
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-04-26 11:25:14 +0200

    Calculate votescast, votesinvalid and votesvalid in poll.stop. Update tests.

--- openslides_backend/action/actions/poll/stop.py
+++ openslides_backend/action/actions/poll/stop.py
@@ -1,6 +1,8 @@
+from decimal import Decimal
 from typing import Any, Dict
 
 from ....models.models import Poll
+from ....services.datastore.commands import GetManyRequest
 from ....shared.exceptions import ActionException
 from ....shared.patterns import Collection, FullQualifiedId
 from ...generics.update import UpdateAction
@@ -22,7 +24,7 @@ class PollStopAction(CountdownControl, UpdateAction, PollPermissionMixin):
     def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
         poll = self.datastore.get(
             FullQualifiedId(self.model.collection, instance["id"]),
-            ["state", "meeting_id"],
+            ["state", "meeting_id", "voted_ids"],
         )
         if poll.get("state") != Poll.STATE_STARTED:
             raise ActionException(
@@ -36,8 +38,33 @@ class PollStopAction(CountdownControl, UpdateAction, PollPermissionMixin):
             [
                 "poll_couple_countdown",
                 "poll_countdown_id",
+                "users_enable_vote_weight",
             ],
         )
         if meeting.get("poll_couple_countdown") and meeting.get("poll_countdown_id"):
             self.control_countdown(meeting["poll_countdown_id"], "reset")
+
+        # calculate votescast, votesvalid, votesinvalid
+        voted_ids = poll.get("voted_ids", [])
+        instance["votescast"] = f"{len(voted_ids)}.000000"
+        if not meeting.get("users_enable_vote_weight") or not voted_ids:
+            instance["votesvalid"] = instance["votescast"]
+        else:
+            gmr = GetManyRequest(
+                Collection("user"), voted_ids, [f"vote_weight_${poll['meeting_id']}"]
+            )
+            gm_result = self.datastore.get_many([gmr])
+            users = gm_result.get(Collection("user"), {}).values()
+            instance["votesvalid"] = str(
+                sum(
+                    [
+                        Decimal(
+                            entry.get(f"vote_weight_${poll['meeting_id']}", "1.000000")
+                        )
+                        for entry in users
+                    ]
+                )
+            )
+        instance["votesinvalid"] = "0.000000"
+
         return instance
--- tests/system/action/poll/test_stop.py
+++ tests/system/action/poll/test_stop.py
@@ -18,8 +18,12 @@ class PollStopActionTest(BaseActionTestCase):
     def test_stop_correct(self) -> None:
         self.set_models(
             {
-                "poll/1": {"state": "started", "meeting_id": 1},
-                "meeting/1": {"poll_couple_countdown": True, "poll_countdown_id": 1},
+                "poll/1": {"state": "started", "meeting_id": 1, "voted_ids": [1]},
+                "meeting/1": {
+                    "poll_couple_countdown": True,
+                    "poll_countdown_id": 1,
+                    "users_enable_vote_weight": False,
+                },
                 "projector_countdown/1": {
                     "running": True,
                     "default_time": 60,
@@ -31,6 +35,38 @@ class PollStopActionTest(BaseActionTestCase):
         self.assert_status_code(response, 200)
         poll = self.get_model("poll/1")
         assert poll.get("state") == "finished"
+        assert poll.get("votescast") == "1.000000"
+        assert poll.get("votesinvalid") == "0.000000"
+        assert poll.get("votesvalid") == "1.000000"
+        countdown = self.get_model("projector_countdown/1")
+        assert countdown.get("running") is False
+        assert countdown.get("countdown_time") == 60
+
+    def test_stop_auto_calc_fields(self) -> None:
+        self.set_models(
+            {
+                "poll/1": {"state": "started", "meeting_id": 1, "voted_ids": [2, 3]},
+                "user/2": {"vote_weight_$1": "2.000000"},
+                "user/3": {"vote_weight_$1": "3.000000"},
+                "meeting/1": {
+                    "poll_couple_countdown": True,
+                    "poll_countdown_id": 1,
+                    "users_enable_vote_weight": True,
+                },
+                "projector_countdown/1": {
+                    "running": True,
+                    "default_time": 60,
+                    "countdown_time": 30.0,
+                },
+            }
+        )
+        response = self.request("poll.stop", {"id": 1})
+        self.assert_status_code(response, 200)
+        poll = self.get_model("poll/1")
+        assert poll.get("state") == "finished"
+        assert poll.get("votescast") == "2.000000"
+        assert poll.get("votesinvalid") == "0.000000"
+        assert poll.get("votesvalid") == "5.000000"
         countdown = self.get_model("projector_countdown/1")
         assert countdown.get("running") is False
         assert countdown.get("countdown_time") == 60
