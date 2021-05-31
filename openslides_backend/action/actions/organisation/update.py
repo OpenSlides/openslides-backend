e348708d3657ae6a419449000c9dc23f551a258e
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-05-31 13:44:14 +0200

    Add permissions to organisation.update.

--- openslides_backend/action/actions/organisation/update.py
+++ openslides_backend/action/actions/organisation/update.py
@@ -1,4 +1,9 @@
+from typing import Any, Dict
+
 from ....models.models import Organisation
+from ....permissions.management_levels import OrganisationManagementLevel
+from ....permissions.permission_helper import has_organisation_management_level
+from ....shared.exceptions import MissingPermission
 from ...generics.update import UpdateAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
@@ -20,6 +25,45 @@ class OrganisationUpdate(UpdateAction):
             "login_text",
             "theme",
             "custom_translations",
+            #     "enable_electronic_voting", TODO
             "reset_password_verbose_errors",
         ]
     )
+
+    def check_permissions(self, instance: Dict[str, Any]) -> None:
+        # check group A fields
+        if any(
+            [
+                field in instance
+                for field in [
+                    "name",
+                    "description",
+                    "legal_notice",
+                    "privacy_policy",
+                    "login_text",
+                    "theme",
+                    "custom_translations",
+                ]
+            ]
+        ) and not has_organisation_management_level(
+            self.datastore,
+            self.user_id,
+            OrganisationManagementLevel.CAN_MANAGE_ORGANISATION,
+        ):
+            raise MissingPermission(OrganisationManagementLevel.CAN_MANAGE_ORGANISATION)
+
+        # check group B fields
+        if any(
+            [
+                field in instance
+                for field in [
+                    "enable_electronic_voting",
+                    "reset_password_verbose_errors",
+                ]
+            ]
+        ) and not has_organisation_management_level(
+            self.datastore,
+            self.user_id,
+            OrganisationManagementLevel.SUPERADMIN,
+        ):
+            raise MissingPermission(OrganisationManagementLevel.SUPERADMIN)
