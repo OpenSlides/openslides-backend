c939aada51a20c39a8a28acdf0ea57e242e1b28d
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-06-03 13:21:34 +0200

    Rm internal and add an empty check_permissions to poll.vote.

--- openslides_backend/action/actions/poll/vote.py
+++ openslides_backend/action/actions/poll/vote.py
@@ -14,7 +14,7 @@ from ..vote.create import VoteCreate
 from ..vote.user_token_helper import get_user_token
 
 
-@register_action("poll.vote", internal=True)
+@register_action("poll.vote")
 class PollVote(UpdateAction):
     """
     Action to vote for a poll.
@@ -299,3 +299,6 @@ class PollVote(UpdateAction):
             <= self.poll.get("max_votes_amount", 1)
         ):
             raise ActionException("Total amount of votes is not in min-max-interval.")
+
+    def check_permissions(self, instance: Dict[str, Any]) -> None:
+        return
