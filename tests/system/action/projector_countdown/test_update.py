20957420f4a20d17964267a9a49641b1635f1eed
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-07-15 13:45:43 +0200

    Simplify the test.

--- tests/system/action/projector_countdown/test_update.py
+++ tests/system/action/projector_countdown/test_update.py
@@ -50,18 +50,11 @@ class ProjectorCountdownUpdate(BaseActionTestCase):
             {
                 "id": 2,
                 "title": "test",
-                "description": "good bla",
-                "default_time": 30,
-                "countdown_time": 20,
-                "running": True,
             },
         )
         self.assert_status_code(response, 200)
         model = self.get_model("projector_countdown/2")
         assert model.get("title") == "test"
-        assert model.get("description") == "good bla"
-        assert model.get("default_time") == 30
-        assert model.get("countdown_time") == 20
 
     def test_update_not_unique_title(self) -> None:
         response = self.request(
