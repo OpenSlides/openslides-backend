60cb86517114829c79de28c9683eee187aa47b91
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-08-04 14:29:41 +0200

    Add some tests for the check_mediafile_id presenter.

--- tests/system/presenter/test_check_mediafile_id.py
+++ tests/system/presenter/test_check_mediafile_id.py
@@ -1,4 +1,5 @@
 from .base import BasePresenterTestCase
+from openslides_backend.permissions.permissions import Permissions
 
 
 class TestCheckMediafileId(BasePresenterTestCase):
@@ -31,3 +32,61 @@ class TestCheckMediafileId(BasePresenterTestCase):
         status_code, data = self.request("check_mediafile_id", {"mediafile_id": 1})
         self.assertEqual(status_code, 200)
         self.assertEqual(data, {"ok": True, "filename": "the filename"})
+
+    def test_no_permissions(self) -> None:
+        self.set_models({
+            "mediafile/1": {"filename": "the filename", "is_directory": False},
+            "user/1": {"organization_management_level": None},
+            })
+        status_code, data = self.request("check_mediafile_id", {"mediafile_id": 1})
+        self.assertEqual(status_code, 403)
+
+    def test_permission_in_admin_group(self) -> None:
+        self.set_models({
+            "mediafile/1": {"filename": "the filename", "is_directory": False, "meeting_id": 1},
+            "meeting/1": {"admin_group_id": 2},
+            "group/2": {"user_ids": [1]},
+            "user/1": {"organization_management_level": None, "group_$1_ids": [2]},
+            })
+        status_code, data = self.request("check_mediafile_id", {"mediafile_id": 1})
+        self.assertEqual(status_code, 200)
+
+    def test_permission_logo(self) -> None:
+        self.set_models({
+            "mediafile/1": {"filename": "the filename", "is_directory": False, "meeting_id": 1, "used_as_logo_$_in_meeting_id": ["test"],"used_as_logo_$test_in_meeting_id": 1},
+            "meeting/1": {"enable_anonymous": True},
+            "user/1": {"organization_management_level": None},
+            })
+        status_code, data = self.request("check_mediafile_id", {"mediafile_id": 1})
+        self.assertEqual(status_code, 200)
+
+    def test_permission_font(self) -> None:
+        self.set_models({
+            "mediafile/1": {"filename": "the filename", "is_directory": False, "meeting_id": 1, "used_as_font_$_in_meeting_id": ["test"], "used_as_font_$test_in_meeting_id": 1},
+            "meeting/1": {"enable_anonymous": True},
+            "user/1": {"organization_management_level": None},
+            })
+        status_code, data = self.request("check_mediafile_id", {"mediafile_id": 1})
+        self.assertEqual(status_code, 200)
+    
+    def test_permission_projector_can_see(self) -> None:
+        self.set_models({
+            "mediafile/1": {"filename": "the filename", "is_directory": False, "meeting_id": 1, "projection_ids": [1]},
+            "meeting/1": {"default_group_id": 2},
+            "group/2": {"user_ids": [1], "permissions": [Permissions.Projector.CAN_SEE]},
+            "user/1": {"organization_management_level": None, "group_$1_ids": [2]},
+            "projection/1": {"meeting_id": 1, "current_projector_id": 1},
+            "projector/1": {"meeting_id": 1, "current_projection_ids": [1]},
+            })
+        status_code, data = self.request("check_mediafile_id", {"mediafile_id": 1})
+        self.assertEqual(status_code, 200)
+
+    def test_can_see_and_is_public(self) -> None:
+        self.set_models({
+            "mediafile/1": {"filename": "the filename", "is_directory": False, "meeting_id": 1, "is_public": True},
+            "meeting/1": {"default_group_id": 2},
+            "group/2": {"user_ids": [1], "permissions": [Permissions.Mediafile.CAN_SEE]},
+            "user/1": {"organization_management_level": None, "group_$1_ids": [2]},
+            })
+        status_code, data = self.request("check_mediafile_id", {"mediafile_id": 1})
+        self.assertEqual(status_code, 200)
