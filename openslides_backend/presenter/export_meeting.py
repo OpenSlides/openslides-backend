587d4f0265424181bc189fb498921f713e4d7f1a
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-08-12 13:45:01 +0200

    Move export meeting into a function. So it can be called from an action too.

--- openslides_backend/presenter/export_meeting.py
+++ openslides_backend/presenter/export_meeting.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List
 import fastjsonschema
 
 from ..models.base import model_registry
+from ..services.datastore.interface import DatastoreService
 from ..shared.filters import FilterOperator
 from ..shared.patterns import Collection, FullQualifiedId
 from ..shared.schema import schema_version
@@ -34,43 +35,42 @@ class ExportMeeting(BasePresenter):
     schema = export_meeting_schema
 
     def get_result(self) -> Any:
-        self.check_permissions()
-        export = {}
+        return {"export": export_meeting(self.datastore, self.data["meeting_id"])}
 
-        # handle collections with meeting_id
-        meeting_collections = self.get_collections_with_meeting_id()
-        for collection in meeting_collections:
-            res = self.datastore.filter(
-                Collection(collection),
-                FilterOperator("meeting_id", "=", self.data["meeting_id"]),
-            )
-            export[collection] = self.remove_meta_fields(list(res.values()))
 
-        # handle meeting
-        meeting = self.datastore.get(
-            FullQualifiedId(Collection("meeting"), self.data["meeting_id"])
+def export_meeting(datastore: DatastoreService, meeting_id: int) -> Dict[str, Any]:
+    export = {}
+
+    # handle collections with meeting_id
+    meeting_collections = get_collections_with_meeting_id()
+    for collection in meeting_collections:
+        res = datastore.filter(
+            Collection(collection),
+            FilterOperator("meeting_id", "=", meeting_id),
         )
-        export["meeting"] = self.remove_meta_fields([meeting])
+        export[collection] = remove_meta_fields(list(res.values()))
 
-        return {"export": export}
+    # handle meeting
+    meeting = datastore.get(FullQualifiedId(Collection("meeting"), meeting_id))
+    export["meeting"] = remove_meta_fields([meeting])
+    return export
 
-    def check_permissions(self) -> None:
-        pass
 
-    def get_collections_with_meeting_id(self) -> List[str]:
-        collections = [
-            c.collection
-            for c in model_registry
-            if model_registry[c]().has_field("meeting_id")
-        ]
-        return collections
+def get_collections_with_meeting_id() -> List[str]:
+    collections = [
+        c.collection
+        for c in model_registry
+        if model_registry[c]().has_field("meeting_id")
+    ]
+    return collections
 
-    def remove_meta_fields(self, res: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
-        list_without_meta_fields = []
-        for entry in res:
-            new_entry = {}
-            for fieldname in entry:
-                if not fieldname.startswith("meta_"):
-                    new_entry[fieldname] = entry[fieldname]
-            list_without_meta_fields.append(new_entry)
-        return list_without_meta_fields
+
+def remove_meta_fields(res: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    list_without_meta_fields = []
+    for entry in res:
+        new_entry = {}
+        for fieldname in entry:
+            if not fieldname.startswith("meta_"):
+                new_entry[fieldname] = entry[fieldname]
+        list_without_meta_fields.append(new_entry)
+    return list_without_meta_fields
