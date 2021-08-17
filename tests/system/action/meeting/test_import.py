eace30897b6420f05b0aa2890112314d1f193117
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-08-17 10:51:24 +0200

    Remove a test.
    The export and import test is no longer needed. Clone meeting should
    work with the action.

--- tests/system/action/meeting/test_import.py
+++ tests/system/action/meeting/test_import.py
@@ -2,7 +2,6 @@ import base64
 import time
 from typing import Any, Dict
 
-from openslides_backend.presenter.export_meeting import export_meeting
 from tests.system.action.base import BaseActionTestCase
 
 
@@ -1040,64 +1039,3 @@ class MeetingImport(BaseActionTestCase):
         response = self.request("meeting.import", request_data)
         self.assert_status_code(response, 400)
         assert "mediafile/1: Invalid fields foobar" in response.json["message"]
-
-    def test_import_with_export_meeting(self) -> None:
-        self.set_models(
-            {
-                "meeting/1": {
-                    "name": "Test",
-                    "admin_group_id": 1,
-                    "default_group_id": 1,
-                    "motions_default_amendment_workflow_id": 1,
-                    "motions_default_statute_amendment_workflow_id": 1,
-                    "motions_default_workflow_id": 1,
-                    "reference_projector_id": 1,
-                    "projector_countdown_default_time": 60,
-                    "projector_countdown_warning_time": 5,
-                    "projector_ids": [1],
-                    "group_ids": [1],
-                    "motion_state_ids": [1],
-                    "motion_workflow_ids": [1],
-                    "logo_$_id": [],
-                    "font_$_id": [],
-                    "default_projector_$_id": [],
-                },
-                "group/1": {
-                    "meeting_id": 1,
-                    "name": "testgroup",
-                    "admin_group_for_meeting_id": 1,
-                    "default_group_for_meeting_id": 1,
-                },
-                "motion_workflow/1": {
-                    "meeting_id": 1,
-                    "name": "blup",
-                    "first_state_id": 1,
-                    "default_amendment_workflow_meeting_id": 1,
-                    "default_statute_amendment_workflow_meeting_id": 1,
-                    "default_workflow_meeting_id": 1,
-                    "state_ids": [1],
-                },
-                "motion_state/1": {
-                    "css_class": "lightblue",
-                    "meeting_id": 1,
-                    "workflow_id": 1,
-                    "name": "test",
-                    "workflow_id": 1,
-                    "first_state_of_workflow_id": 1,
-                },
-                "projector/1": {
-                    "meeting_id": 1,
-                    "used_as_reference_projector_meeting_id": 1,
-                    "name": "Default projector",
-                    "used_as_default_$_in_meeting_id": [],
-                },
-            }
-        )
-
-        with self.datastore.get_database_context():
-            export = export_meeting(self.datastore, 1)
-        export["user"] = []
-        response = self.request(
-            "meeting.import", {"committee_id": 1, "meeting": export}
-        )
-        self.assert_status_code(response, 200)
