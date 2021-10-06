3479afd2c29637cfc6aec114fb538a79255ee31d
peb-adr <adrichter97@gmail.com>
2021-10-06 15:34:41 +0200

    load test-data only in dev setup (#31)
    * load test-data only in dev setup
    
    * remove unnecessary COPY from Dockerfile.dev
    
    Co-authored-by: Adrian Richter <adrian@intevation.de>

---
+++ entrypoint.dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+. entrypoint.sh
+
+# load test data in postgresql
+PGPASSWORD="$MEDIA_DATABASE_PASSWORD" psql -1 -h "$MEDIA_DATABASE_HOST" -U "$MEDIA_DATABASE_USER" -d "$MEDIA_DATABASE_NAME" -f src/test_data.sql
+
+exec "$@"
+
