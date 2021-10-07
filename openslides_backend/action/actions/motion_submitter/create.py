6a046acc4c92481c906960fcdad8337f32c1a083
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-10-07 16:51:03 +0200

    Make motion_submitter.create MultiAction again.

--- openslides_backend/action/actions/motion_submitter/create.py
+++ openslides_backend/action/actions/motion_submitter/create.py
@@ -11,16 +11,14 @@ from ...generics.create import CreateAction
 from ...mixins.create_action_with_inferred_meeting import (
     CreateActionWithInferredMeetingMixin,
 )
-from ...mixins.singular_action_mixin import SingularActionMixin
 from ...util.assert_belongs_to_meeting import assert_belongs_to_meeting
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
+from ...util.typing import ActionData
 
 
 @register_action("motion_submitter.create")
-class MotionSubmitterCreateAction(
-    CreateActionWithInferredMeetingMixin, SingularActionMixin, CreateAction
-):
+class MotionSubmitterCreateAction(CreateActionWithInferredMeetingMixin, CreateAction):
     """
     Action to create a motion submitter.
     """
@@ -33,6 +31,11 @@ class MotionSubmitterCreateAction(
     permission = Permissions.Motion.CAN_MANAGE_METADATA
 
     relation_field_for_meeting = "motion_id"
+    weight_map: Dict[int, int] = {}
+
+    def get_updated_instances(self, action_data: ActionData) -> ActionData:
+        self.weight_map = {}
+        return super().get_updated_instances(action_data)
 
     def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
         """
@@ -63,8 +66,13 @@ class MotionSubmitterCreateAction(
 
     def set_weight(self, instance: Dict[str, Any]) -> None:
         motion_id = instance["motion_id"]
-        filter_ = FilterOperator("motion_id", "=", motion_id)
-        max_weight = self.datastore.max(self.model.collection, filter_, "weight", "int")
-        if max_weight is None:
-            max_weight = 0
+        if motion_id in self.weight_map:
+            max_weight = self.weight_map[motion_id]
+            self.weight_map[motion_id] += 1
+        else:
+            filter_ = FilterOperator("motion_id", "=", motion_id)
+            max_weight = (
+                self.datastore.max(self.model.collection, filter_, "weight", "int") or 0
+            )
+            self.weight_map[motion_id] = max_weight + 1
         instance["weight"] = max_weight + 1
--- tests/system/action/motion_submitter/test_create.py
+++ tests/system/action/motion_submitter/test_create.py
@@ -46,6 +46,29 @@ class MotionSubmitterCreateActionTest(BaseActionTestCase):
         assert model.get("user_id") == 78
         assert model.get("weight") == 101
 
+    def test_create_weight_double_action(self) -> None:
+        self.set_models(
+            {
+                "meeting/111": {"name": "name_m123etrd"},
+                "motion/357": {"title": "title_YIDYXmKj", "meeting_id": 111},
+                "user/78": {"username": "username_loetzbfg", "meeting_ids": [111]},
+                "user/89": {"username": "username_ghjiuen2", "meeting_ids": [111]},
+                "user/93": {"username": "username_husztw", "meeting_ids": [111]},
+            }
+        )
+        response = self.request_multi(
+            "motion_submitter.create",
+            [
+                {"motion_id": 357, "user_id": 78},
+                {"motion_id": 357, "user_id": 89},
+                {"motion_id": 357, "user_id": 93},
+            ],
+        )
+        self.assert_status_code(response, 200)
+        self.assert_model_exists("motion_submitter/1", {"weight": 1, "user_id": 78})
+        self.assert_model_exists("motion_submitter/2", {"weight": 2, "user_id": 89})
+        self.assert_model_exists("motion_submitter/3", {"weight": 3, "user_id": 93})
+
     def test_create_not_unique(self) -> None:
         self.set_models(
             {
