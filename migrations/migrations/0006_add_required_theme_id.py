a529fd34b19e3bb4be4866db498190ccfd1cc584
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-10-11 13:29:50 +0200

    Add required theme_id migration and test.

---
+++ migrations/migrations/0006_add_required_theme_id.py
@@ -0,0 +1,41 @@
+from typing import List, Optional
+
+from datastore.migrations import BaseEvent, BaseMigration, CreateEvent, UpdateEvent
+
+
+class Migration(BaseMigration):
+
+    target_migration_index = 7
+
+    def position_init(self) -> None:
+        self.update_theme_id = False
+
+    def migrate_event(
+        self,
+        event: BaseEvent,
+    ) -> Optional[List[BaseEvent]]:
+
+        if event.fqid != "organization/1":
+            return None
+
+        if isinstance(event, CreateEvent):
+            self.update_theme_id = True
+            return [
+                event,
+                CreateEvent(
+                    "theme/1",
+                    {
+                        "id": 1,
+                        "name": "OpenSlides Blue",
+                        "primary_500": "#317796",
+                        "accent_500": "#2196f3",
+                        "warn_500": "#f06400",
+                    },
+                ),
+            ]
+        return None
+
+    def get_additional_events(self) -> Optional[List[BaseEvent]]:
+        if self.update_theme_id:
+            return [UpdateEvent("organization/1", {"theme_id": 1})]
+        return None
