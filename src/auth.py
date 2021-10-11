59722c4a59bdc72459e95dcb58a3c22ae513ac5b
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-10-11 09:30:12 +0200

    Rename var names MEDIA_PRESENTER to PRESENTER.

--- src/auth.py
+++ src/auth.py
@@ -56,6 +56,6 @@ def check_file_id(file_id, file_type, app, presenter_headers):
 
 
 def get_presenter_url(app):
-    presenter_host = app.config["MEDIA_PRESENTER_HOST"]
-    presenter_port = app.config["MEDIA_PRESENTER_PORT"]
+    presenter_host = app.config["PRESENTER_HOST"]
+    presenter_port = app.config["PRESENTER_PORT"]
     return f"http://{presenter_host}:{presenter_port}/system/presenter"
