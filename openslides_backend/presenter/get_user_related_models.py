7f2e68c0015d2073a94a4a855641a722e369c0f1
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-10-14 14:36:28 +0200

    First version of the get_user_related_models presenter.
    Add some tests. Perhaps there is still a problem with the permissions.

---
+++ openslides_backend/presenter/get_user_related_models.py
@@ -0,0 +1,129 @@
+from typing import Any, Dict, List
+
+import fastjsonschema
+
+from ..permissions.management_levels import (
+    CommitteeManagementLevel,
+    OrganizationManagementLevel,
+)
+from ..permissions.permission_helper import has_organization_management_level
+from ..shared.exceptions import MissingPermission
+from ..shared.filters import And, FilterOperator
+from ..shared.patterns import Collection, FullQualifiedId
+from ..shared.schema import schema_version
+from .base import BasePresenter
+from .presenter import register_presenter
+
+get_user_related_models_schema = fastjsonschema.compile(
+    {
+        "$schema": schema_version,
+        "type": "object",
+        "title": "get_user_related_models",
+        "description": "get user ids related models",
+        "properties": {
+            "user_ids": {
+                "type": "array",
+                "item": {"type": "integer"},
+            },
+        },
+        "required": ["user_ids"],
+        "additionalProperties": False,
+    }
+)
+
+
+@register_presenter("get_user_related_models")
+class GetUserRelatedModels(BasePresenter):
+    """
+    Collects related models of the user_ids.
+    """
+
+    schema = get_user_related_models_schema
+
+    def get_result(self) -> Any:
+        self.check_permissions()
+        result: Dict[str, Any] = {}
+        for user_id in self.data["user_ids"]:
+            result[str(user_id)] = {}
+            committees_data = self.get_committees_data(user_id)
+            meetings_data = self.get_meetings_data(user_id)
+            if committees_data:
+                result[str(user_id)]["committees"] = committees_data
+            if meetings_data:
+                result[str(user_id)]["meetings"] = meetings_data
+        return result
+
+    def check_permissions(self) -> None:
+        if not has_organization_management_level(
+            self.datastore, self.user_id, OrganizationManagementLevel.CAN_MANAGE_USERS
+        ):
+            raise MissingPermission(OrganizationManagementLevel.CAN_MANAGE_USERS)
+
+    def get_committees_data(self, user_id: int) -> List[Dict[str, Any]]:
+        committees = []
+        user = self.datastore.get(
+            FullQualifiedId(Collection("user"), user_id), ["committee_ids"]
+        )
+        return_committees = False
+        for committee_id in user.get("committee_ids", []):
+            user2 = self.datastore.get(
+                FullQualifiedId(Collection("user"), user_id),
+                [f"committee_${committee_id}_management_level"],
+            )
+            if (
+                CommitteeManagementLevel(
+                    user2.get(f"committee_${committee_id}_management_level")
+                )
+                >= CommitteeManagementLevel.CAN_MANAGE
+            ):
+                return_committees = True
+
+            committee = self.datastore.get(
+                FullQualifiedId(Collection("committee"), committee_id), ["name"]
+            )
+            committees.append(
+                {
+                    "id": committee_id,
+                    "name": committee.get("name", ""),
+                    "cml": user2.get(f"committee_${committee_id}_management_level", ""),
+                }
+            )
+        if return_committees:
+            return committees
+        return []
+
+    def get_meetings_data(self, user_id: int) -> List[Dict[str, Any]]:
+        meetings = []
+        user = self.datastore.get(
+            FullQualifiedId(Collection("user"), user_id), ["meeting_ids"]
+        )
+        for meeting_id in user.get("meeting_ids", ""):
+            meeting = self.datastore.get(
+                FullQualifiedId(Collection("meeting"), meeting_id),
+                ["name", "is_active_in_organization_id"],
+            )
+            filter_ = And(
+                FilterOperator("meeting_id", "=", meeting_id),
+                FilterOperator("user_id", "=", user_id),
+            )
+            submitter_ids = self.datastore.filter(
+                Collection("motion_submitter"), filter_
+            )
+            candidate_ids = self.datastore.filter(
+                Collection("assignment_candidate"), filter_
+            )
+            speaker_ids = self.datastore.filter(Collection("speaker"), filter_)
+            if submitter_ids or candidate_ids or speaker_ids:
+                meetings.append(
+                    {
+                        "id": meeting_id,
+                        "name": meeting.get("name"),
+                        "is_active_in_organization_id": meeting.get(
+                            "is_active_in_organization_id"
+                        ),
+                        "submitter_ids": list(submitter_ids),
+                        "candidate_ids": list(candidate_ids),
+                        "speaker_ids": list(speaker_ids),
+                    }
+                )
+        return meetings
---
+++ tests/system/presenter/test_get_user_related_models.py
@@ -0,0 +1,171 @@
+from openslides_backend.permissions.management_levels import OrganizationManagementLevel
+
+from .base import BasePresenterTestCase
+
+
+class TestGetUserRelatedModels(BasePresenterTestCase):
+    def test_get_user_related_models_simple(self) -> None:
+        status_code, data = self.request("get_user_related_models", {"user_ids": [1]})
+        self.assertEqual(status_code, 200)
+        assert data == {"1": {}}
+
+    def test_get_user_related_models_committee(self) -> None:
+        self.set_models(
+            {
+                "committee/1": {"name": "test"},
+                "user/1": {
+                    "committee_ids": [1],
+                    "committee_$1_management_level": "can_manage",
+                },
+            }
+        )
+        status_code, data = self.request("get_user_related_models", {"user_ids": [1]})
+        self.assertEqual(status_code, 200)
+        assert data == {
+            "1": {"committees": [{"id": 1, "cml": "can_manage", "name": "test"}]}
+        }
+
+    def test_get_user_related_models_committee_more_user(self) -> None:
+        self.set_models(
+            {
+                "committee/1": {"name": "test", "user_ids": [1, 2, 3]},
+                "user/1": {
+                    "committee_ids": [1],
+                    "committee_$1_management_level": "can_manage",
+                },
+                "user/2": {
+                    "committee_ids": [1],
+                    "committee_$1_management_level": "can_manage",
+                },
+                "user/3": {
+                    "committee_ids": [1],
+                },
+            }
+        )
+        status_code, data = self.request(
+            "get_user_related_models", {"user_ids": [1, 2, 3]}
+        )
+        self.assertEqual(status_code, 200)
+        assert data == {
+            "1": {"committees": [{"id": 1, "name": "test", "cml": "can_manage"}]},
+            "2": {"committees": [{"id": 1, "name": "test", "cml": "can_manage"}]},
+            "3": {},
+        }
+
+    def test_get_user_related_models_committee_more_committees(self) -> None:
+        self.set_models(
+            {
+                "committee/1": {"name": "test", "user_ids": [1]},
+                "committee/2": {"name": "test2", "user_ids": [1]},
+                "committee/3": {"name": "test3", "user_ids": [1]},
+                "user/1": {
+                    "committee_ids": [1, 2, 3],
+                    "committee_$1_management_level": "can_manage",
+                    "committee_$2_management_level": "can_manage",
+                },
+            }
+        )
+        status_code, data = self.request("get_user_related_models", {"user_ids": [1]})
+        self.assertEqual(status_code, 200)
+        assert data == {
+            "1": {
+                "committees": [
+                    {"id": 1, "cml": "can_manage", "name": "test"},
+                    {"id": 2, "cml": "can_manage", "name": "test2"},
+                    {"id": 3, "cml": "", "name": "test3"},
+                ]
+            }
+        }
+
+    def test_get_user_related_models_meeting(self) -> None:
+        self.set_models(
+            {
+                "user/1": {"meeting_ids": [1]},
+                "meeting/1": {"name": "test", "is_active_in_organization_id": 1},
+                "motion_submitter/2": {"user_id": 1, "meeting_id": 1},
+                "assignment_candidate/3": {"user_id": 1, "meeting_id": 1},
+                "speaker/4": {"user_id": 1, "meeting_id": 1},
+            }
+        )
+        status_code, data = self.request("get_user_related_models", {"user_ids": [1]})
+        self.assertEqual(status_code, 200)
+        assert data == {
+            "1": {
+                "meetings": [
+                    {
+                        "id": 1,
+                        "name": "test",
+                        "is_active_in_organization_id": 1,
+                        "submitter_ids": [2],
+                        "candidate_ids": [3],
+                        "speaker_ids": [4],
+                    }
+                ]
+            }
+        }
+
+    def test_get_user_related_models_meetings_more_user(self) -> None:
+        self.set_models(
+            {
+                "user/1": {"meeting_ids": [1]},
+                "user/2": {"meeting_ids": [1]},
+                "meeting/1": {"name": "test", "is_active_in_organization_id": 1},
+                "motion_submitter/2": {"user_id": 1, "meeting_id": 1},
+                "motion_submitter/3": {"user_id": 2, "meeting_id": 1},
+                "assignment_candidate/3": {"user_id": 1, "meeting_id": 1},
+                "assignment_candidate/4": {"user_id": 2, "meeting_id": 1},
+                "speaker/4": {"user_id": 1, "meeting_id": 1},
+                "speaker/5": {"user_id": 2, "meeting_id": 1},
+            }
+        )
+        status_code, data = self.request(
+            "get_user_related_models", {"user_ids": [1, 2]}
+        )
+        self.assertEqual(status_code, 200)
+        assert data == {
+            "1": {
+                "meetings": [
+                    {
+                        "id": 1,
+                        "name": "test",
+                        "is_active_in_organization_id": 1,
+                        "submitter_ids": [2],
+                        "candidate_ids": [3],
+                        "speaker_ids": [4],
+                    }
+                ]
+            },
+            "2": {
+                "meetings": [
+                    {
+                        "id": 1,
+                        "name": "test",
+                        "is_active_in_organization_id": 1,
+                        "submitter_ids": [3],
+                        "candidate_ids": [4],
+                        "speaker_ids": [5],
+                    }
+                ]
+            },
+        }
+
+    def test_get_user_related_models_missing_payload(self) -> None:
+        status_code, data = self.request("get_user_related_models", {})
+        self.assertEqual(status_code, 400)
+        assert "data must contain ['user_ids'] properties" in data["message"]
+
+    def test_get_user_related_models_no_permissions(self) -> None:
+        self.set_models({"user/1": {"organization_management_level": None}})
+        status_code, data = self.request("get_user_related_models", {"user_ids": [1]})
+        self.assertEqual(status_code, 403)
+
+    def test_get_user_related_models_permissions(self) -> None:
+        self.set_models(
+            {
+                "user/1": {
+                    "organization_management_level": OrganizationManagementLevel.CAN_MANAGE_USERS
+                }
+            }
+        )
+        status_code, data = self.request("get_user_related_models", {"user_ids": [1]})
+        self.assertEqual(status_code, 200)
