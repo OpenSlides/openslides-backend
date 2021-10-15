9012cd9e2a757f38dc5b7e043784d69d860cd044
reiterl <ludwig.reiter@intevation.de>
2021-10-15 09:10:26 +0200

    Add the option publish_immediately to poll.create. (#980)

--- tests/system/action/poll/test_create.py
+++ tests/system/action/poll/test_create.py
@@ -61,6 +61,29 @@ class CreatePoll(BaseActionTestCase):
         assert global_option.get("used_as_global_option_in_poll_id") == 1
         assert global_option.get("meeting_id") == 1
 
+    def test_create_correct_publish_immediately(self) -> None:
+        response = self.request(
+            "poll.create",
+            {
+                "title": "test",
+                "type": "analog",
+                "content_object_id": "assignment/1",
+                "pollmethod": "Y",
+                "options": [{"text": "test2", "Y": "10.000000"}],
+                "meeting_id": 1,
+                "global_yes": True,
+                "global_no": True,
+                "global_abstain": True,
+                "onehundred_percent_base": "Y",
+                "min_votes_amount": 5,
+                "max_votes_amount": 10,
+                "publish_immediately": True,
+            },
+        )
+        self.assert_status_code(response, 200)
+        poll = self.get_model("poll/1")
+        assert poll.get("state") == "published"
+
     def test_create_three_options(self) -> None:
         response = self.request(
             "poll.create",
