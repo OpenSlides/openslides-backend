d11baa36666f5b7cadde39eb0e3e64c3b61e6684
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-10-21 14:13:00 +0200

    Simplify the code, remove not needed parts.

--- openslides_backend/action/actions/organization/initial_import.py
+++ openslides_backend/action/actions/organization/initial_import.py
@@ -4,7 +4,7 @@ from datastore.shared.util import DeletedModelsBehaviour
 
 from ....models.checker import Checker, CheckException
 from ....models.models import Organization
-from ....shared.exceptions import ActionException, MissingPermission
+from ....shared.exceptions import ActionException
 from ....shared.filters import FilterOperator
 from ....shared.interfaces.event import EventType
 from ....shared.interfaces.write_request import WriteRequest
@@ -13,7 +13,7 @@ from ...action import Action
 from ...mixins.singular_action_mixin import SingularActionMixin
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
-from ...util.typing import ActionData, ActionResultElement, ActionResults
+from ...util.typing import ActionData, ActionResults
 
 
 @register_action("organization.initial_import", internal=True)
@@ -38,20 +38,12 @@ class OrganizationInitialImport(SingularActionMixin, Action):
         """
         self.user_id = user_id
         self.index = 0
-        action_data = self.get_updated_instances(action_data)
         instance = next(iter(action_data))
         self.validate_instance(instance)
-        try:
-            self.check_permissions(instance)
-        except MissingPermission as e:
-            msg = f"You are not allowed to perform action {self.name}."
-            e.message = msg + " " + e.message
-            raise e
-        instance = self.base_update_instance(instance)
+        instance = self.update_instance(instance)
         self.write_requests.extend(self.create_write_requests(instance))
         final_write_request = self.process_write_requests()
-        result = [self.create_action_result_element(instance)]
-        return (final_write_request, result)
+        return (final_write_request, [None])
 
     def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
         data = instance["data"]
@@ -92,11 +84,3 @@ class OrganizationInitialImport(SingularActionMixin, Action):
                     )
                 )
         return write_requests
-
-    def create_action_result_element(
-        self, instance: Dict[str, Any]
-    ) -> Optional[ActionResultElement]:
-        return {"id": 1}
-
-    def check_permissions(self, instance: Dict[str, Any]) -> None:
-        return
