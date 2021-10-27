74169355544047d82cb17108814079505b53e819
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-10-27 15:18:05 +0200

    Add some more tests for organization.initial_import.
    Add four tests to check for data problems. This checks the checker,
    which is included in the action.

--- tests/system/action/organization/test_initial_import.py
+++ tests/system/action/organization/test_initial_import.py
@@ -21,3 +21,48 @@ class OrganizationInitialImport(BaseActionTestCase):
                 self.assert_model_exists(
                     f"{collection}/{id_}", request_data["data"][collection][id_]
                 )
+
+    def test_initial_import_wrong_field(self) -> None:
+        self.datastore.truncate_db()
+        request_data = {"data": self.get_initial_data()}
+        request_data["data"]["meeting"]["1"]["test_field"] = "test"
+        response = self.request("organization.initial_import", request_data)
+        self.assert_status_code(response, 400)
+        assert (
+            "meeting/1: Invalid fields test_field (value: test)"
+            in response.json["message"]
+        )
+
+    def test_initial_import_wrong_type(self) -> None:
+        self.datastore.truncate_db()
+        request_data = {"data": self.get_initial_data()}
+        request_data["data"]["organization"]["1"]["theme_id"] = "test"
+        response = self.request("organization.initial_import", request_data)
+        self.assert_status_code(response, 400)
+        assert (
+            "organization/1/theme_id: Type error: Type is not RelationField(to={Collection('theme'): 'theme_for_organization_id'}, is_list_field=False, on_delete=OnDelete.SET_NULL, required=True, constraints={}, equal_fields=[])"
+            in response.json["message"]
+        )
+
+    def test_initial_import_wrong_relation(self) -> None:
+        self.datastore.truncate_db()
+        request_data = {"data": self.get_initial_data()}
+        request_data["data"]["organization"]["1"]["theme_id"] = 666
+        response = self.request("organization.initial_import", request_data)
+        self.assert_status_code(response, 400)
+        assert (
+            "Relation Error:  points to theme/666/theme_for_organization_id, but the reverse relation for it is corrupt"
+            in response.json["message"]
+        )
+        assert (
+            "Relation Error:  points to organization/1/theme_id, but the reverse relation for it is corrupt"
+            in response.json["message"]
+        )
+
+    def test_inital_import_missing_required(self) -> None:
+        self.datastore.truncate_db()
+        request_data = {"data": self.get_initial_data()}
+        del request_data["data"]["organization"]["1"]["theme_id"]
+        response = self.request("organization.initial_import", request_data)
+        self.assert_status_code(response, 400)
+        assert "organization/1: Missing fields theme_id" in response.json["message"]
