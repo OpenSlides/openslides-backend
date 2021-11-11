53fc81cfb8ca4cf80999f15ce5aa765b6f30a729
jsangmeister <33004050+jsangmeister@users.noreply.github.com>
2021-11-11 09:26:28 +0100

    Add test to execute migrations on sql dump (#1015)

---
+++ migrations/tests/test_with_sql_dump.py
@@ -0,0 +1,17 @@
+import pytest
+from datastore.migrations.core.migration_handler import MigrationHandler
+from datastore.shared.di import injector
+from datastore.shared.postgresql_backend import ConnectionHandler
+
+from migrate import load_migrations
+
+
+@pytest.mark.skip()
+def test_with_sql_dump(write, finalize, assert_model):
+    connection_handler = injector.get(ConnectionHandler)
+    with connection_handler.get_connection_context():
+        with connection_handler.get_current_connection().cursor() as cursor:
+            cursor.execute(open("tests/dump.sql", "r").read(), [])
+    migration_handler = injector.get(MigrationHandler)
+    migration_handler.register_migrations(*load_migrations("migrations"))
+    migration_handler.finalize()
