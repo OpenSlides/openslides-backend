9a6827042d6a0ca7dde57a592f54a1e0ffedda87
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-11-16 14:31:15 +0100

    Modify chat_group.update: Add a name unique check, add a test.

--- openslides_backend/action/actions/chat_group/update.py
+++ openslides_backend/action/actions/chat_group/update.py
@@ -1,5 +1,10 @@
+from typing import Any, Dict
+
 from ....models.models import ChatGroup
 from ....permissions.permissions import Permissions
+from ....shared.exceptions import ActionException
+from ....shared.filters import FilterOperator
+from ....shared.patterns import FullQualifiedId
 from ...generics.update import UpdateAction
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
@@ -17,3 +22,19 @@ class ChatGroupUpdate(ChatEnabledMixin, UpdateAction):
         optional_properties=["name", "read_group_ids", "write_group_ids"],
     )
     permission = Permissions.Chat.CAN_MANAGE
+
+    def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
+        instance = super().update_instance(instance)
+        if "name" in instance:
+            chat_group = self.datastore.get(
+                FullQualifiedId(self.model.collection, instance["id"]), ["name"]
+            )
+            if instance["name"] != chat_group.get("name"):
+                name_exists = self.datastore.exists(
+                    self.model.collection,
+                    FilterOperator("name", "=", instance["name"]),
+                    lock_result=False,
+                )
+                if name_exists:
+                    raise ActionException("The name of the chat group must be unique.")
+        return instance
