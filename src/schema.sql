ec01be5f67029a02ed7b2a534662189dc5302d77
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-11-30 11:53:50 +0100

    create 'media' schema and use it for the media service.
    In the schema.sql create the schema and use it in the SQL commands.

--- src/schema.sql
+++ src/schema.sql
@@ -1,11 +1,13 @@
 -- Postgres 9.1 or higher required.
-CREATE TABLE IF NOT EXISTS mediafile_data (
+CREATE SCHEMA media;
+
+CREATE TABLE IF NOT EXISTS media.mediafile_data (
     id int PRIMARY KEY,
     data bytea,
     mimetype varchar(255)
 );
 
-CREATE TABLE IF NOT EXISTS resource_data (
+CREATE TABLE IF NOT EXISTS media.resource_data (
     id int PRIMARY KEY,
     data bytea,
     mimetype varchar(255)
