ae8d9ee1f890f7cc57e214cfbec942b575cedd8e
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2021-12-16 11:25:54 +0100

    Adjust setup to datastore changes

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -2,13 +2,8 @@
 
 set -e
 
-export DATASTORE_DATABASE_HOST=${DATASTORE_DATABASE_HOST:-postgresql}
-export DATASTORE_DATABASE_PORT=${DATASTORE_DATABASE_PORT:-5432}
-export DATASTORE_DATABASE_USER=${DATASTORE_DATABASE_USER:-openslides}
-export DATASTORE_DATABASE_NAME=${DATASTORE_DATABASE_NAME:-openslides}
-export DATASTORE_DATABASE_PASSWORD=${DATASTORE_DATABASE_PASSWORD:-openslides}
-
-./wait.sh $DATASTORE_WRITER_HOST $DATASTORE_WRITER_PORT
+source scripts/export_datastore_variables.sh
+scripts/wait.sh $DATASTORE_WRITER_HOST $DATASTORE_WRITER_PORT
 
 printf "\nMigrations:\n"
 python migrations/migrate.py finalize
--- migrations/entrypoint.sh
+++ migrations/entrypoint.sh
@@ -4,7 +4,6 @@ source wait.sh
 
 if [[ -f /datastore-service/scripts/system/create_schema.sh ]]; then
     pushd /datastore-service/
-    export PGPASSWORD="$DATASTORE_DATABASE_PASSWORD"
     scripts/system/create_schema.sh
     popd
 fi
