b308154f3c13443ee7da642c6db0f9880ceae52c
Ludwig Reiter <ludwig.reiter@intevation.de>
2021-12-17 14:28:21 +0100

    Add organization mail defaults migration and migration test.

---
+++ migrations/migrations/0009_add_organization_mail_defaults.py
@@ -0,0 +1,44 @@
+from typing import List, Optional
+
+from datastore.migrations import BaseEvent, BaseMigration, CreateEvent
+from datastore.shared.util import collection_from_fqid
+
+
+class Migration(BaseMigration):
+    """
+    This migration adds some defaults of organization.
+    """
+
+    target_migration_index = 10
+    collection = "organization"
+
+    def migrate_event(
+        self,
+        event: BaseEvent,
+    ) -> Optional[List[BaseEvent]]:
+        collection = collection_from_fqid(event.fqid)
+        if collection == self.collection and isinstance(event, CreateEvent):
+            event.data["users_email_sender"] = "OpenSlides"
+            event.data["users_email_subject"] = "OpenSlides access data"
+            event.data[
+                "users_email_body"
+            ] = """\
+      Dear {name},
+
+
+
+      this is your personal OpenSlides login:
+
+          {url}
+
+          username: {username}
+
+          password: {password}
+
+
+
+      This email was generated automatically."""
+            event.data["url"] = "http://example.com:8000"
+            return [event]
+        else:
+            return None
