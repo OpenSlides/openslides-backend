8ba6344a875f7e250a36a21633f53861361c41b4
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2022-02-03 10:58:06 +0100

    Remove skip from poll tests

--- tests/system/action/poll/test_reset.py
+++ tests/system/action/poll/test_reset.py
@@ -1,5 +1,3 @@
-import pytest
-
 from openslides_backend.permissions.permissions import Permissions
 from tests.system.action.base import BaseActionTestCase
 
@@ -25,7 +23,6 @@ class PollResetActionTest(BaseActionTestCase):
             "vote/3": {"option_id": 2, "meeting_id": 1},
         }
 
-    @pytest.mark.skip()
     def test_reset_correct(self) -> None:
         self.set_models(
             {
@@ -91,7 +88,6 @@ class PollResetActionTest(BaseActionTestCase):
     def test_reset_no_permissions(self) -> None:
         self.base_permission_test(self.permission_test_model, "poll.reset", {"id": 1})
 
-    @pytest.mark.skip()
     def test_reset_permissions(self) -> None:
         self.base_permission_test(
             self.permission_test_model,
@@ -100,7 +96,6 @@ class PollResetActionTest(BaseActionTestCase):
             Permissions.Poll.CAN_MANAGE,
         )
 
-    @pytest.mark.skip()
     def test_reset_not_allowed_to_vote_again(self) -> None:
         self.set_models(
             {
