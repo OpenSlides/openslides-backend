69b6635ef2314910e1e72b398d317ac50eb4ed4b
Ludwig Reiter <ludwig.reiter@intevation.de>
2022-02-11 14:10:39 +0100

    Update the unique name check of chat_groups. It checks that the name is unique in a meeting.

--- openslides_backend/action/actions/chat_group/mixins.py
+++ openslides_backend/action/actions/chat_group/mixins.py
@@ -1,21 +1,32 @@
 from typing import Any, Dict
 
+from ....models.models import Model
+from ....services.datastore.interface import DatastoreService
 from ....shared.exceptions import ActionException
-from ....shared.filters import FilterOperator
+from ....shared.filters import And, FilterOperator
 from ....shared.patterns import Collection, FullQualifiedId
 from ...action import Action
 
 ONE_ORGANIZATION_ID = 1
 
 
-class ChatEnabledMixin(Action):
-    def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
+class MeetingIdProvider:
+    datastore: DatastoreService
+    model: Model
+
+    def fetch_meeting_id(self, instance: Dict[str, Any]) -> int:
         meeting_id = instance.get("meeting_id")
         if not meeting_id:
             chat_group = self.datastore.get(
                 FullQualifiedId(self.model.collection, instance["id"]), ["meeting_id"]
             )
             meeting_id = chat_group["meeting_id"]
+        return meeting_id
+
+
+class ChatEnabledMixin(MeetingIdProvider, Action):
+    def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
+        meeting_id = self.fetch_meeting_id(instance)
         meeting = self.datastore.get(
             FullQualifiedId(Collection("meeting"), meeting_id),
             ["enable_chat"],
@@ -30,11 +41,15 @@ class ChatEnabledMixin(Action):
         return instance
 
 
-class CheckUniqueNameMixin(Action):
+class CheckUniqueNameMixin(MeetingIdProvider, Action):
     def check_name_unique(self, instance: Dict[str, Any]) -> None:
+        meeting_id = self.fetch_meeting_id(instance)
         name_exists = self.datastore.exists(
             self.model.collection,
-            FilterOperator("name", "=", instance["name"]),
+            And(
+                FilterOperator("name", "=", instance["name"]),
+                FilterOperator("meeting_id", "=", meeting_id),
+            ),
         )
         if name_exists:
             raise ActionException("The name of a chat group must be unique.")
