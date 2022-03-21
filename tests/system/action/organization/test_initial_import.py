25bc7f8f16da4edcb17927db7e38db3cfabfa712
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2022-03-21 11:18:10 +0100

    Update to python 3.10 and update dependencies

--- tests/system/action/organization/test_initial_import.py
+++ tests/system/action/organization/test_initial_import.py
@@ -60,7 +60,7 @@ class OrganizationInitialImport(BaseActionTestCase):
         self.assert_status_code(response, 400)
         print(response.json)
         assert (
-            "organization/1/theme_id: Type error: Type is not RelationField(to={Collection('theme'): 'theme_for_organization_id'}, is_list_field=False, on_delete=<OnDelete.SET_NULL: 'SET_NULL'>, required=True, constraints={}, equal_fields=[])"
+            "organization/1/theme_id: Type error: Type is not RelationField(to={Collection('theme'): 'theme_for_organization_id'}, is_list_field=False, on_delete=OnDelete.SET_NULL, required=True, constraints={}, equal_fields=[])"
             in response.json["message"]
         )
 
