e93e9830211ed8ddf00d5e634c65a858226755a9
Ludwig Reiter <ludwig.reiter@intevation.de>
2022-04-20 10:27:13 +0200

    Make group.set_permission more robust.
    Add a test, which shows an KeyError and update the action to fix it.

--- tests/system/action/group/test_set_permission.py
+++ tests/system/action/group/test_set_permission.py
@@ -86,6 +86,24 @@ class GroupSetPermissionActionTest(BaseActionTestCase):
         model = self.get_model("group/11")
         assert model.get("permissions") == ["agenda_item.can_manage"]
 
+    def test_set_permissions_missing_permissions_in_group(self) -> None:
+        self.set_models(
+            {
+                "meeting/1": {"is_active_in_organization_id": 1},
+                "group/11": {
+                    "name": "group_11",
+                    "meeting_id": 1,
+                },
+            }
+        )
+        response = self.request(
+            "group.set_permission",
+            {"id": 11, "permission": "motion.can_create", "set": True},
+        )
+        self.assert_status_code(response, 200)
+        model = self.get_model("group/11")
+        assert model.get("permissions") == ["motion.can_create"]
+
     def test_set_permissions_no_permissions(self) -> None:
         self.base_permission_test(
             {
