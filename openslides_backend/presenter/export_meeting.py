9953ce574dde219f0d73b575ec499ad29e62569f
Ludwig Reiter <ludwig.reiter@intevation.de>
2022-05-09 09:04:32 +0200

    Remove organization_tags and add users to export.

--- openslides_backend/presenter/export_meeting.py
+++ openslides_backend/presenter/export_meeting.py
@@ -1,11 +1,20 @@
-from typing import Any
+from typing import Any, Dict
 
 import fastjsonschema
 
 from ..action.actions.meeting.export_helper import export_meeting
+from ..models.fields import (
+    TemplateCharField,
+    TemplateDecimalField,
+    TemplateHTMLStrictField,
+    TemplateRelationListField,
+)
+from ..models.models import User
 from ..permissions.management_levels import OrganizationManagementLevel
 from ..permissions.permission_helper import has_organization_management_level
+from ..services.datastore.commands import GetManyRequest
 from ..shared.exceptions import PermissionDenied
+from ..shared.patterns import Collection, FullQualifiedId
 from ..shared.schema import required_id_schema, schema_version
 from .base import BasePresenter
 from .presenter import register_presenter
@@ -41,4 +50,80 @@ class Export(BasePresenter):
             msg += f" Missing permission: {OrganizationManagementLevel.SUPERADMIN}"
             raise PermissionDenied(msg)
         export_data = export_meeting(self.datastore, self.data["meeting_id"])
+        self.exclude_organization_tags(export_data)
+        self.add_users(export_data, self.data["meeting_id"])
         return export_data
+
+    def exclude_organization_tags(self, export_data: Dict[str, Any]) -> None:
+        self.get_meeting_from_json(export_data)["organization_tag_ids"] = None
+
+    def add_users(self, export_data: Dict[str, Any], meeting_id: int) -> None:
+        user_ids = self.get_meeting_from_json(export_data)["user_ids"]
+        if not user_ids:
+            return
+        gmr = GetManyRequest(
+            Collection("user"),
+            user_ids,
+            [
+                "id",
+                "username",
+                "pronoun",
+                "title",
+                "first_name",
+                "last_name",
+                "is_active",
+                "is_physical_person",
+                "password",
+                "default",
+                "can_change_own_password",
+                "gender",
+                "email",
+                "default_number",
+                "default_structure_level",
+                "default_vote_weight",
+                "last_email_send",
+                "is_demo_user",
+                "organization_management_level",
+            ],
+        )
+        users: Any = self.datastore.get_many([gmr])[Collection("user")]
+        # change keys to strings
+        users = {str(key): value for key, value in users.items()}
+        # remove meta_* keys
+        users = {
+            key: {
+                key_inner: value_inner
+                for key_inner, value_inner in value.items()
+                if not key_inner.startswith("meta_")
+            }
+            for key, value in users.items()
+        }
+
+        for user_key in users:
+            for field in User().get_fields():
+                if isinstance(
+                    field,
+                    (
+                        TemplateCharField,
+                        TemplateHTMLStrictField,
+                        TemplateDecimalField,
+                        TemplateRelationListField,
+                    ),
+                ):
+                    field_name = field.get_structured_field_name(meeting_id)
+                    user = self.datastore.get(
+                        FullQualifiedId(Collection("user"), int(user_key)), [field_name]
+                    )
+                    if user.get(field_name):
+                        users[user_key][field_name] = user.get(field_name)
+                        users[user_key][field.get_template_field_name()] = [
+                            str(meeting_id)
+                        ]
+            users[user_key]["meeting_ids"] = [meeting_id]
+            users[user_key]["is_present_in_meeting_ids"] = [meeting_id]
+
+        export_data["user"] = users
+
+    def get_meeting_from_json(self, export_data: Any) -> Any:
+        key = next(iter(export_data["meeting"]))
+        return export_data["meeting"][key]
