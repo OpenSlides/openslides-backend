fb1f3fa498a5b96ed98c03fa460f841617074546
Ludwig Reiter <ludwig.reiter@intevation.de>
2022-07-20 12:29:48 +0200

    Remove tests.
    You need can_manage_metadata for follow-recommendation, so the check for
    neighbour is bypassed.

--- tests/system/action/motion/test_follow_recommendation.py
+++ tests/system/action/motion/test_follow_recommendation.py
@@ -76,46 +76,6 @@ class MotionFollowRecommendationActionText(BaseActionTestCase):
         assert model.get("state_extension") == "test_test_test"
         assert model.get("last_modified", 0) >= check_time
 
-    def test_follow_recommendation_not_neighbour(self) -> None:
-        self.set_models(
-            {
-                "meeting/222": {
-                    "name": "name_SNLGsvIV",
-                    "is_active_in_organization_id": 1,
-                },
-                "motion_state/76": {
-                    "meeting_id": 222,
-                    "name": "test0",
-                    "motion_ids": [],
-                    "next_state_ids": [],
-                    "previous_state_ids": [],
-                    "show_state_extension_field": True,
-                    "show_recommendation_extension_field": True,
-                },
-                "motion_state/77": {
-                    "meeting_id": 222,
-                    "name": "test1",
-                    "motion_ids": [22],
-                    "first_state_of_workflow_id": 76,
-                    "next_state_ids": [],
-                    "previous_state_ids": [],
-                },
-                "motion/22": {
-                    "meeting_id": 222,
-                    "title": "test1",
-                    "state_id": 77,
-                    "recommendation_id": 76,
-                    "recommendation_extension": "test_test_test",
-                },
-            }
-        )
-        response = self.request("motion.follow_recommendation", {"id": 22})
-        self.assert_status_code(response, 400)
-        assert (
-            "State '76' is not in next or previous states of the state '77'."
-            in response.json["message"]
-        )
-
     def test_follow_recommendation_missing_recommendation_id(self) -> None:
         self.set_models(
             {
