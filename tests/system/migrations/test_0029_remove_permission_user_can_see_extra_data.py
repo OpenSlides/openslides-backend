11c709fa3099622d8df653140a8dec68c97303c1
Ludwig Reiter <ludwig.reiter@intevation.de>
2022-09-13 12:01:45 +0200

    Add migration to remove permission 'user.can_see_extra_data'.

---
+++ tests/system/migrations/test_0029_remove_permission_user_can_see_extra_data.py
@@ -0,0 +1,70 @@
+def test_migration(write, finalize, assert_model):
+    write(
+        {
+            "type": "create",
+            "fqid": "group/1",
+            "fields": {"permissions": ["user.can_see_extra_data"]},
+        }
+    )
+    write(
+        {
+            "type": "update",
+            "fqid": "group/1",
+            "fields": {"permissions": ["user.can_see"]},
+        }
+    )
+    write(
+        {
+            "type": "update",
+            "fqid": "group/1",
+            "fields": {"permissions": ["user.can_see_extra_data"]},
+        }
+    )
+    write(
+        {
+            "type": "update",
+            "fqid": "group/1",
+            "list_fields": {
+                "add": {},
+                "remove": {"permissions": ["user.can_see_extra_data"]},
+            },
+        }
+    )
+    write(
+        {
+            "type": "update",
+            "fqid": "group/1",
+            "list_fields": {
+                "add": {"permissions": ["user.can_see_extra_data"]},
+                "remove": {},
+            },
+        }
+    )
+
+    finalize("0029_remove_permission_user_can_see_extra_data")
+
+    assert_model(
+        "group/1",
+        {"permissions": [], "meta_deleted": False, "meta_position": 1},
+        position=1,
+    )
+    assert_model(
+        "group/1",
+        {"permissions": ["user.can_see"], "meta_deleted": False, "meta_position": 2},
+        position=2,
+    )
+    assert_model(
+        "group/1",
+        {"permissions": [], "meta_deleted": False, "meta_position": 3},
+        position=3,
+    )
+    assert_model(
+        "group/1",
+        {"permissions": [], "meta_deleted": False, "meta_position": 4},
+        position=4,
+    )
+    assert_model(
+        "group/1",
+        {"permissions": [], "meta_deleted": False, "meta_position": 5},
+        position=5,
+    )
