a9d90e1549dcdef4c34dc94c0169994a606c11e3
Ralf Peschke <ralf.peschke@intevation.de>
2022-11-17 14:54:02 +0100

    Issue1536 Exception if there is no instance and no user_id given for get_user_scope (#1537)

--- tests/system/presenter/test_get_user_scope.py
+++ tests/system/presenter/test_get_user_scope.py
@@ -80,3 +80,15 @@ class TestGetUSerScope(BasePresenterTestCase):
                 },
             },
         )
+
+    def test_without_user_None(self) -> None:
+        status_code, data = self.request("get_user_scope", {"user_ids": [None]})
+        self.assertEqual(status_code, 400)
+        self.assertIn(
+            "There is no user_id given to get the user_scope!", data["message"]
+        )
+
+    def test_without_user_empty_list(self) -> None:
+        status_code, data = self.request("get_user_scope", {"user_ids": []})
+        self.assertEqual(status_code, 200)
+        assert data == {}
