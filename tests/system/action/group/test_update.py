3398946c12e84ce9b224379013f1cfb3b44f19ad
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2023-01-30 17:47:15 +0100

    Add permissions array to group.update

--- tests/system/action/group/test_update.py
+++ tests/system/action/group/test_update.py
@@ -3,44 +3,79 @@ from tests.system.action.base import BaseActionTestCase
 
 
 class GroupUpdateActionTest(BaseActionTestCase):
-    def test_update_correct(self) -> None:
+    def setUp(self) -> None:
+        super().setUp()
         self.set_models(
             {
                 "meeting/1": {"is_active_in_organization_id": 1},
-                "group/111": {"name": "name_srtgb123", "meeting_id": 1},
+                "group/111": {
+                    "name": "name_srtgb123",
+                    "meeting_id": 1,
+                    "permissions": [Permissions.User.CAN_MANAGE],
+                },
             }
         )
+
+    def test_update_correct(self) -> None:
         response = self.request("group.update", {"id": 111, "name": "name_Xcdfgee"})
         self.assert_status_code(response, 200)
         model = self.get_model("group/111")
         assert model.get("name") == "name_Xcdfgee"
 
-    def test_update_wrong_id(self) -> None:
-        self.set_models(
+    def test_update_permissions(self) -> None:
+        response = self.request(
+            "group.update",
             {
-                "meeting/1": {"is_active_in_organization_id": 1},
-                "group/111": {"name": "name_srtgb123", "meeting_id": 1},
-            }
+                "id": 111,
+                "permissions": [
+                    Permissions.User.CAN_MANAGE,
+                    Permissions.Motion.CAN_MANAGE,
+                ],
+            },
         )
+        self.assert_status_code(response, 200)
+        model = self.get_model("group/111")
+        assert model.get("permissions") == [
+            Permissions.User.CAN_MANAGE,
+            Permissions.Motion.CAN_MANAGE,
+        ]
+
+    def test_update_permissions_remove(self) -> None:
+        response = self.request(
+            "group.update", {"id": 111, "permissions": [Permissions.Motion.CAN_MANAGE]}
+        )
+        self.assert_status_code(response, 200)
+        model = self.get_model("group/111")
+        assert model.get("permissions") == [Permissions.Motion.CAN_MANAGE]
+
+    def test_update_permissions_redundant(self) -> None:
+        response = self.request(
+            "group.update",
+            {
+                "id": 111,
+                "permissions": [Permissions.User.CAN_MANAGE, Permissions.User.CAN_SEE],
+            },
+        )
+        self.assert_status_code(response, 200)
+        model = self.get_model("group/111")
+        assert model.get("permissions") == [Permissions.User.CAN_MANAGE]
+
+    def test_update_wrong_id(self) -> None:
         response = self.request("group.update", {"id": 112, "name": "name_Xcdfgee"})
         self.assert_status_code(response, 400)
         model = self.get_model("group/111")
         assert model.get("name") == "name_srtgb123"
 
-    def test_update_no_permissions(self) -> None:
+    def test_update_forbidden(self) -> None:
         self.base_permission_test(
-            {
-                "group/111": {"name": "name_srtgb123", "meeting_id": 1},
-            },
+            {},
             "group.update",
             {"id": 111, "name": "name_Xcdfgee"},
         )
 
-    def test_update_permissions(self) -> None:
+    def test_update_allowed(self) -> None:
         self.base_permission_test(
-            {
-                "group/111": {"name": "name_srtgb123", "meeting_id": 1},
-            },
+            {},
             "group.update",
             {"id": 111, "name": "name_Xcdfgee"},
             Permissions.User.CAN_MANAGE,
