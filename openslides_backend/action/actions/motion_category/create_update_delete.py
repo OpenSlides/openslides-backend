dc31b9adb32ecf731b7e7abff71d0af4eed30604
Ludwig Reiter <ludwig.reiter@intevation.de>
2023-02-06 14:05:21 +0100

    Add an unique check for motion_category.prefix.
    And add tests.

--- openslides_backend/action/actions/motion_category/create_update_delete.py
+++ openslides_backend/action/actions/motion_category/create_update_delete.py
@@ -1,12 +1,48 @@
+from typing import Any, Dict
+
 from ....models.models import MotionCategory
 from ....permissions.permissions import Permissions
+from ....shared.exceptions import ActionException
+from ....shared.filters import And, FilterOperator, Not
+from ....shared.patterns import fqid_from_collection_and_id
+from ...action import Action
 from ...action_set import ActionSet
+from ...generics.update import UpdateAction
 from ...mixins.sequential_numbers_mixin import SequentialNumbersMixin
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action_set
 
 
-class MotionCategoryCreate(SequentialNumbersMixin):
+class PrefixUniqueMixin(Action):
+    def update_instance(self, instance: Dict[str, Any]) -> Dict[str, Any]:
+        instance = super().update_instance(instance)
+        if instance.get("prefix"):
+            meeting_id = instance.get("meeting_id")
+            if meeting_id is None:
+                workflow = self.datastore.get(
+                    fqid_from_collection_and_id("motion_category", instance["id"]),
+                    ["meeting_id"],
+                )
+                meeting_id = workflow.get("meeting_id")
+            if self.datastore.exists(
+                "motion_category",
+                And(
+                    FilterOperator("prefix", "=", instance["prefix"]),
+                    Not(FilterOperator("id", "=", instance["id"])),
+                    FilterOperator("meeting_id", "=", meeting_id),
+                ),
+            ):
+                raise ActionException(
+                    f"Prefix '{instance['prefix']}' is not unique in the meeting."
+                )
+        return instance
+
+
+class MotionCategoryCreate(PrefixUniqueMixin, SequentialNumbersMixin):
+    pass
+
+
+class MotionCategoryUpdate(PrefixUniqueMixin, UpdateAction):
     pass
 
 
@@ -27,3 +63,4 @@ class MotionCategoryActionSet(ActionSet):
     delete_schema = DefaultSchema(MotionCategory()).get_delete_schema()
     permission = Permissions.Motion.CAN_MANAGE
     CreateActionClass = MotionCategoryCreate
+    UpdateActionClass = MotionCategoryUpdate
