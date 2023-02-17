d6ab83ec9ee2bbbd25f9c9fcd2b3a5847523df96
Ludwig Reiter <ludwig.reiter@intevation.de>
2023-02-17 13:49:39 +0100

    Update set_data, use a different logic, add more tests.

--- tests/system/action/meeting_user/test_set_data.py
+++ tests/system/action/meeting_user/test_set_data.py
@@ -51,7 +51,7 @@ class MeetingUserSetData(BaseActionTestCase):
             "meeting_user/5", {"meeting_id": 10, "user_id": 1, **test_dict}
         )
 
-    def test_set_data_with_meeting_user_and_wrong_id(self) -> None:
+    def test_set_data_with_meeting_user_and_wrong_meeting_id(self) -> None:
         self.set_models(
             {
                 "meeting/10": {
@@ -62,12 +62,29 @@ class MeetingUserSetData(BaseActionTestCase):
             }
         )
         test_dict = {
-            "id": 12,
-            "user_id": 1,
-            "meeting_id": 10,
+            "id": 5,
+            "meeting_id": 12,
             "comment": "test bla",
         }
-        with pytest.raises(AssertionError):
+        with pytest.raises(AssertionError, match="Not permitted to change meeting_id."):
+            self.request("meeting_user.set_data", test_dict)
+
+    def test_set_data_with_meeting_user_and_wrong_user_id(self) -> None:
+        self.set_models(
+            {
+                "meeting/10": {
+                    "is_active_in_organization_id": 1,
+                    "meeting_user_ids": [5],
+                },
+                "meeting_user/5": {"user_id": 1, "meeting_id": 10},
+            }
+        )
+        test_dict = {
+            "id": 5,
+            "user_id": 3,
+            "comment": "test bla",
+        }
+        with pytest.raises(AssertionError, match="Not permitted to change user_id."):
             self.request("meeting_user.set_data", test_dict)
 
     def test_set_data_without_meeting_user(self) -> None:
@@ -91,3 +108,23 @@ class MeetingUserSetData(BaseActionTestCase):
         response = self.request("meeting_user.set_data", test_dict)
         self.assert_status_code(response, 200)
         self.assert_model_exists("meeting_user/1", test_dict)
+
+    def test_set_data_missing_identifiers(self) -> None:
+        self.set_models(
+            {
+                "meeting/10": {
+                    "is_active_in_organization_id": 1,
+                    "meeting_user_ids": [5],
+                },
+                "meeting_user/5": {"user_id": 1, "meeting_id": 10},
+            }
+        )
+        test_dict = {
+            "comment": "test bla",
+        }
+        response = self.request("meeting_user.set_data", test_dict)
+        self.assert_status_code(response, 400)
+        assert (
+            "Identifier for meeting_user instance required, but neither id nor meeting_id/user_id is given."
+            == response.json["message"]
+        )
