1039b0ebef6dbac821d098b6b00887a113959d23
Ludwig Reiter <ludwig.reiter@intevation.de>
2023-02-21 11:25:02 +0100

    Update 0038 migration, rm replacement and reverse field too.

--- openslides_backend/migrations/migrations/0038_remove_user_default_projector.py
+++ openslides_backend/migrations/migrations/0038_remove_user_default_projector.py
@@ -1,10 +1,58 @@
-from datastore.migrations import RemoveFieldsMigration
+from typing import Any, Dict, List, Optional
+
+from datastore.migrations.core.base_migration import BaseMigration
+from datastore.migrations.core.events import (
+    BaseEvent,
+    CreateEvent,
+    DeleteFieldsEvent,
+    ListUpdateEvent,
+    UpdateEvent,
+)
+from datastore.shared.util import collection_from_fqid
 
 
-class Migration(RemoveFieldsMigration):
+class Migration(BaseMigration):
     """
     This migration removes the field `meeting/default_projector_$user_ids`.
     """
 
     target_migration_index = 39
-    collection_fields_map = {"meeting": ["default_projector_$user_ids"]}
+    collection_fields_map = {
+        "meeting": ["default_projector_$user_ids"],
+        "projector": ["used_as_default_$user_in_meeting_id"],
+    }
+
+    def remove_fields(self, obj: Dict[str, Any], fields: List[str]) -> None:
+        for field in fields:
+            if field in obj:
+                del obj[field]
+
+    def remove_replacement(self, obj: Dict[str, Any]) -> None:
+        for field in ("default_projector_$_ids", "used_as_default_$_in_meeting_id"):
+            if obj.get(field):
+                obj.pop(field, None)
+
+    def migrate_event(
+        self,
+        event: BaseEvent,
+    ) -> Optional[List[BaseEvent]]:
+        collection = collection_from_fqid(event.fqid)
+        if collection not in self.collection_fields_map:
+            return None
+        fields = self.collection_fields_map[collection]
+
+        if isinstance(event, CreateEvent) or isinstance(event, UpdateEvent):
+            self.remove_fields(event.data, fields)
+            self.remove_replacement(event.data)
+
+        elif isinstance(event, DeleteFieldsEvent):
+            for field in fields:
+                if field in event.data:
+                    event.data.remove(field)
+
+        elif isinstance(event, ListUpdateEvent):
+            self.remove_fields(event.add, fields)
+            self.remove_fields(event.remove, fields)
+            self.remove_replacement(event.add)
+            self.remove_replacement(event.remove)
+        return [event]
--- tests/system/migrations/test_0038_remove_user_default_projector.py
+++ tests/system/migrations/test_0038_remove_user_default_projector.py
@@ -3,15 +3,54 @@ def test_migration(write, finalize, assert_model):
         {
             "type": "create",
             "fqid": "meeting/1",
-            "fields": {"a": 1, "default_projector_$user_ids": [1]},
-        }
+            "fields": {
+                "a": 1,
+                "default_projector_$_ids": ["user"],
+                "default_projector_$user_ids": [1],
+            },
+        },
+        {
+            "type": "create",
+            "fqid": "projector/1",
+            "fields": {
+                "b": 1,
+                "used_as_default_$_in_meeting_id": ["user"],
+                "used_as_default_$user_in_meeting_id": 1,
+            },
+        },
+        {
+            "type": "create",
+            "fqid": "projector/2",
+            "fields": {"b": 2},
+        },
     )
     write(
         {
             "type": "update",
             "fqid": "meeting/1",
-            "fields": {"a": 2, "default_projector_$user_ids": [2]},
-        }
+            "fields": {
+                "a": 2,
+                "default_projector_$_ids": ["user"],
+                "default_projector_$user_ids": [2],
+            },
+        },
+        {
+            "type": "update",
+            "fqid": "projector/1",
+            "fields": {
+                "default_projector_$_ids": [],
+                "default_projector_$user_ids": None,
+            },
+        },
+        {
+            "type": "update",
+            "fqid": "projector/2",
+            "fields": {
+                "used_as_default_$_in_meeting_id": ["user"],
+                "used_as_default_$user_in_meeting_id": 1,
+                "b": 3,
+            },
+        },
     )
     write({"type": "delete", "fqid": "meeting/1"})
     write({"type": "restore", "fqid": "meeting/1"})
@@ -37,4 +76,23 @@ def test_migration(write, finalize, assert_model):
         "meeting/1",
         {"a": 2, "meta_deleted": False, "meta_position": 4},
         position=4,
+    )
+    assert_model(
+        "projector/1", {"b": 1, "meta_deleted": False, "meta_position": 1}, position=1
+    )
+    assert_model(
+        "projector/1",
+        {
+            "b": 1,
+            "meta_deleted": False,
+            "default_projector_$_ids": [],
+            "meta_position": 2,
+        },
+        position=2,
+    )
+    assert_model(
+        "projector/2", {"b": 2, "meta_deleted": False, "meta_position": 1}, position=1
+    )
+    assert_model(
+        "projector/2", {"b": 3, "meta_deleted": False, "meta_position": 2}, position=2
     )
