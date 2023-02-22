aeb7e3f441919c9df3c4424e47c064d6811a00c7
Ludwig Reiter <ludwig.reiter@intevation.de>
2023-02-22 13:35:21 +0100

    Small refactoring of the code, reduce duplication.

--- openslides_backend/migrations/migrations/0038_remove_user_default_projector.py
+++ openslides_backend/migrations/migrations/0038_remove_user_default_projector.py
@@ -1,17 +1,18 @@
 from typing import Any, Dict, List, Optional
 
-from datastore.migrations.core.base_migration import BaseMigration
 from datastore.migrations.core.events import (
     BaseEvent,
     CreateEvent,
-    DeleteFieldsEvent,
     ListUpdateEvent,
     UpdateEvent,
+)
+from datastore.migrations.migrations.remove_fields_migration import (
+    RemoveFieldsMigration,
 )
 from datastore.shared.util import collection_from_fqid
 
 
-class Migration(BaseMigration):
+class Migration(RemoveFieldsMigration):
     """
     This migration removes the field `meeting/default_projector_$user_ids`.
     """
@@ -21,14 +22,13 @@ class Migration(BaseMigration):
         "meeting": ["default_projector_$user_ids"],
         "projector": ["used_as_default_$user_in_meeting_id"],
     }
+    collection_fields_replacement_map = {
+        "meeting": ["default_projector_$_ids"],
+        "projector": ["used_as_default_$_in_meeting_id"],
+    }
 
-    def remove_fields(self, obj: Dict[str, Any], fields: List[str]) -> None:
+    def remove_replacement(self, obj: Dict[str, Any], fields: List[str]) -> None:
         for field in fields:
-            if field in obj:
-                del obj[field]
-
-    def remove_replacement(self, obj: Dict[str, Any]) -> None:
-        for field in ("default_projector_$_ids", "used_as_default_$_in_meeting_id"):
             if obj.get(field) and "user" in obj[field]:
                 obj[field].remove("user")
                 if not obj[field]:
@@ -38,23 +38,17 @@ class Migration(BaseMigration):
         self,
         event: BaseEvent,
     ) -> Optional[List[BaseEvent]]:
+        events = super().migrate_event(event)
+        if events is None:
+            return events
+        assert len(events) == 1
+        event = events[0]
         collection = collection_from_fqid(event.fqid)
-        if collection not in self.collection_fields_map:
-            return None
-        fields = self.collection_fields_map[collection]
+        fields = self.collection_fields_replacement_map[collection]
 
         if isinstance(event, CreateEvent) or isinstance(event, UpdateEvent):
-            self.remove_fields(event.data, fields)
-            self.remove_replacement(event.data)
-
-        elif isinstance(event, DeleteFieldsEvent):
-            for field in fields:
-                if field in event.data:
-                    event.data.remove(field)
-
+            self.remove_replacement(event.data, fields)
         elif isinstance(event, ListUpdateEvent):
-            self.remove_fields(event.add, fields)
-            self.remove_fields(event.remove, fields)
-            self.remove_replacement(event.add)
-            self.remove_replacement(event.remove)
+            self.remove_replacement(event.add, fields)
+            self.remove_replacement(event.remove, fields)
         return [event]
