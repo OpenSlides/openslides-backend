f88193097e48472e8c9275b45946abf3cf8f6ce3
Ludwig Reiter <ludwig.reiter@intevation.de>
2023-04-12 11:53:05 +0200

    Add another test, renaming of some tests.

--- tests/system/action/topic/test_import.py
+++ tests/system/action/topic/test_import.py
@@ -41,7 +41,7 @@ class TopicJsonImport(BaseActionTestCase):
         self.assert_model_not_exists("topic/1")
         self.assert_model_not_exists("action_worker/2")
 
-    def test_import_duplicates_in_db(self) -> None:
+    def test_import_duplicate_in_db(self) -> None:
         self.set_models(
             {
                 "topic/1": {"title": "test", "meeting_id": 22},
@@ -52,7 +52,7 @@ class TopicJsonImport(BaseActionTestCase):
         self.assert_status_code(response, 200)
         self.assert_model_not_exists("topic/2")
 
-    def test_import_dublicates_in_db_2(self) -> None:
+    def test_import_duplicate_and_topic_deleted_so_imported(self) -> None:
         self.set_models(
             {
                 "topic/1": {"title": "test", "meeting_id": 22},
@@ -78,6 +78,30 @@ class TopicJsonImport(BaseActionTestCase):
         response = self.request("topic.import", {"id": 3, "import": True})
         self.assert_status_code(response, 200)
         self.assert_model_exists("topic/2", {"title": "test"})
+
+    def test_import_duplicate_so_not_imported(self) -> None:
+        self.set_models(
+            {
+                "topic/1": {"title": "test", "meeting_id": 22},
+                "meeting/22": {"topic_ids": [1]},
+            }
+        )
+        response = self.request(
+            "topic.json_upload",
+            {
+                "meeting_id": 22,
+                "data": [
+                    {
+                        "title": "test",
+                    }
+                ],
+            },
+        )
+        self.assert_status_code(response, 200)
+        self.assert_model_exists("action_worker/3")
+        response = self.request("topic.import", {"id": 3, "import": True})
+        self.assert_status_code(response, 200)
+        self.assert_model_not_exists("topic/2")
 
     def test_import_with_upload(self) -> None:
         response = self.request(
