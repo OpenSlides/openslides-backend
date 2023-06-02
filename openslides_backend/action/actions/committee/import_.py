04f49f115b1ee0b6b11fb905599d759016b6dbd7
Ludwig Reiter <ludwig.reiter@intevation.de>
2023-06-02 08:42:03 +0200

    Add version of organization tags handling

--- openslides_backend/action/actions/committee/import_.py
+++ openslides_backend/action/actions/committee/import_.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Set, cast
 
 from ....models.models import ActionWorker
 from ....permissions.management_levels import OrganizationManagementLevel
@@ -8,6 +8,7 @@ from ...mixins.import_mixins import ImportMixin, ImportState, Lookup, ResultType
 from ...util.default_schema import DefaultSchema
 from ...util.register import register_action
 from ..meeting.create import MeetingCreate
+from ..organization_tag.create import OrganizationTagCreate
 from .create import CommitteeCreate
 from .update import CommitteeUpdateAction
 
@@ -48,6 +49,8 @@ class CommitteeImport(ImportMixin):
         create_committee_payload: List[Dict[str, Any]] = []
         update_committee_payload: List[Dict[str, Any]] = []
         self.error = False
+        if any(entry["data"].get("organization_tags") for entry in data):
+            self.handle_organization_tags(data)
         for entry in data:
             if entry["state"] == ImportState.NEW:
                 new_committee = self.get_committee_data_from_entry(entry)
@@ -143,6 +146,37 @@ class CommitteeImport(ImportMixin):
     def get_committee_data_from_entry(self, entry: Dict[str, Any]) -> Dict[str, Any]:
         return {
             field: entry["data"][field]
-            for field in ("name", "description", "id")
+            for field in ("name", "description", "id", "organization_tag_ids")
             if field in entry["data"]
         }
+
+    def handle_organization_tags(self, data: List[Dict[str, Any]]) -> None:
+        create_otnames: Set[str] = set()
+        otname_to_id: Dict[str, int] = {}
+        for entry in data:
+            for otentry in entry["data"].get("organization_tag_ids", []):
+                if otentry["info"] == ImportState.NEW:
+                    create_otnames.add(cast(str, otentry["value"]))
+                elif entry["info"] == ImportState.DONE:
+                    otname_to_id[otentry["value"]] = otentry["id"]
+        # create payload and execute create action
+        created_names: List[str] = list(create_otnames)
+        create_ots_payload = [
+            {"name": otname, "organization_id": ONE_ORGANIZATION_ID}
+            for otname in created_names
+        ]
+        ot_create_results = self.execute_other_action(
+            OrganizationTagCreate, create_ots_payload
+        )
+        created_ids = [(r or {})["id"] for r in (ot_create_results or [])]
+        for name, id_ in zip(created_names, created_ids):
+            otname_to_id[name] = id_
+        # set the organization_tag_ids
+        for entry in data:
+            if entry["data"].get("organization_tag_ids"):
+                collect_ot_ids: List[int] = []
+                for otentry in entry["data"]["organization_tags"]:
+                    id_ = otname_to_id[otentry["value"]]
+                    if id_ not in collect_ot_ids:
+                        collect_ot_ids.append(id_)
+                entry["data"]["organization_tag_ids"] = collect_ot_ids
