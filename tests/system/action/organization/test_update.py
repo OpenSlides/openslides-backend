34e3078a6f0acf5beb2e1c8c72ad06134a7cfc34
Ralf Peschke <ralf.peschke@intevation.de>
2023-06-06 13:46:27 +0200

    new fields and save_saml_account- action for sso (#1733)

--- tests/system/action/organization/test_update.py
+++ tests/system/action/organization/test_update.py
@@ -1,21 +1,45 @@
+from textwrap import dedent
+
 from openslides_backend.permissions.management_levels import OrganizationManagementLevel
 from openslides_backend.shared.util import ONE_ORGANIZATION_FQID
 from tests.system.action.base import BaseActionTestCase
 
 
 class OrganizationUpdateActionTest(BaseActionTestCase):
+    saml_attr_mapping = {
+        "saml_id": "username",
+        "title": "title",
+        "first_name": "firstName",
+        "last_name": "lastName",
+        "email": "email",
+        "gender": "gender",
+        "pronoun": "pronoun",
+        "is_active": "is_active",
+        "is_physical_person": "is_person",
+    }
+
     def test_update(self) -> None:
         self.create_model(
             "organization/3", {"name": "aBuwxoYU", "description": "XrHbAWiF"}
         )
         response = self.request(
             "organization.update",
-            {"id": 3, "name": "testtest", "description": "blablabla"},
+            {
+                "id": 3,
+                "name": "testtest",
+                "description": "blablabla",
+                "saml_attr_mapping": self.saml_attr_mapping,
+            },
         )
         self.assert_status_code(response, 200)
-        model = self.get_model("organization/3")
-        assert model.get("name") == "testtest"
-        assert model.get("description") == "blablabla"
+        self.assert_model_exists(
+            "organization/3",
+            {
+                "name": "testtest",
+                "description": "blablabla",
+                "saml_attr_mapping": self.saml_attr_mapping,
+            },
+        )
 
     def test_update_some_more_fields(self) -> None:
         self.update_model(
@@ -57,27 +81,62 @@ class OrganizationUpdateActionTest(BaseActionTestCase):
                 "users_email_replyto": " email@replyto.de  ",
                 "users_email_subject": "email subject",
                 "users_email_body": "Dear {name},\n\nthis is your personal OpenSlides login:\n\n{url}\nUsername: {username}\nPassword: {password}\n\n\nThis email was generated automatically.",
+                "saml_enabled": True,
+                "saml_login_button_text": "Text for SAML login button",
+                "saml_attr_mapping": self.saml_attr_mapping,
+                "saml_metadata_idp": dedent(
+                    """
+                    <md:EntityDescriptor xmlns="urn:oasis:names:tc:SAML:2.0:metadata"
+                        xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata"
+                        xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" xmlns:ds="http://www.w3.org/2000/09/xmldsig#"
+                        entityID="https://auth.digiv.de/auth/realms/demo">
+                        </md:IDPSSODescriptor>
+                    </md:EntityDescriptor>
+                    """
+                ),
+                "saml_metadata_sp": dedent(
+                    """
+                    <EntityDescriptor
+                    xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata"
+                    xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
+                    xmlns:ds="http://www.w3.org/2000/09/xmldsig#"
+                    entityID="http://localhost:9004/saml/metadata">
+                    </EntityDescriptor>
+                    """
+                ),
+                "saml_private_key": "private key dependency",
             },
         )
         self.assert_status_code(response, 200)
-        model = self.get_model(ONE_ORGANIZATION_FQID)
-        assert model.get("name") == "testtest"
-        assert model.get("description") == "blablabla"
-        assert model.get("legal_notice") == "GYjDABmD"
-        assert model.get("privacy_policy") == "test1"
-        assert model.get("login_text") == "test2"
-        assert model.get("theme_id") == 2
-        assert model.get("theme_ids") == [1, 2]
-        assert model.get("reset_password_verbose_errors") is False
-        assert model.get("enable_chat") is True
-        assert model.get("url") == "https://openslides.example.com"
-        assert model.get("users_email_sender") == "email_sender"
-        assert model.get("users_email_replyto") == "email@replyto.de"
-        assert model.get("users_email_subject") == "email subject"
-        assert (
-            model.get("users_email_body")
-            == "Dear {name},\n\nthis is your personal OpenSlides login:\n\n{url}\nUsername: {username}\nPassword: {password}\n\n\nThis email was generated automatically."
+        organization = self.assert_model_exists(
+            ONE_ORGANIZATION_FQID,
+            {
+                "name": "testtest",
+                "description": "blablabla",
+                "legal_notice": "GYjDABmD",
+                "privacy_policy": "test1",
+                "login_text": "test2",
+                "theme_id": 2,
+                "theme_ids": [1, 2],
+                "reset_password_verbose_errors": False,
+                "enable_chat": True,
+                "url": "https://openslides.example.com",
+                "users_email_sender": "email_sender",
+                "users_email_replyto": "email@replyto.de",
+                "users_email_subject": "email subject",
+                "users_email_body": "Dear {name},\n\nthis is your personal OpenSlides login:\n\n{url}\nUsername: {username}\nPassword: {password}\n\n\nThis email was generated automatically.",
+                "saml_enabled": True,
+                "saml_login_button_text": "Text for SAML login button",
+                "saml_attr_mapping": self.saml_attr_mapping,
+                "saml_private_key": "private key dependency",
+            },
         )
+        assert (
+            'xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"'
+            in organization["saml_metadata_idp"]
+        )
+        assert "http://www.w3.org/2000/09/xmldsig#" in organization["saml_metadata_sp"]
+
         self.assert_model_exists(
             "theme/1", {"organization_id": 1, "theme_for_organization_id": None}
         )
@@ -121,6 +180,57 @@ class OrganizationUpdateActionTest(BaseActionTestCase):
         self.assert_status_code(response, 400)
         assert (
             "users_email_sender must not contain '[', ']', '\\'."
+            in response.json["message"]
+        )
+
+    def test_update_broken_saml_attr_mapping1(self) -> None:
+        self.create_model(
+            "organization/3", {"name": "aBuwxoYU", "description": "XrHbAWiF"}
+        )
+        response = self.request(
+            "organization.update",
+            {
+                "id": 3,
+                "saml_attr_mapping": "This is not a valid JSON formated sso-configuration",
+            },
+        )
+        self.assert_status_code(response, 400)
+        assert "data.saml_attr_mapping must be object" in response.json["message"]
+
+    def test_update_broken_saml_attr_mapping2(self) -> None:
+        self.create_model(
+            "organization/3", {"name": "aBuwxoYU", "description": "XrHbAWiF"}
+        )
+        response = self.request(
+            "organization.update", {"id": 3, "saml_attr_mapping": ["f1", "f2"]}
+        )
+        self.assert_status_code(response, 400)
+        assert "data.saml_attr_mapping must be object" in response.json["message"]
+
+    def test_update_broken_saml_attr_mapping_missing_saml_id(self) -> None:
+        self.create_model(
+            "organization/3", {"name": "aBuwxoYU", "description": "XrHbAWiF"}
+        )
+        response = self.request(
+            "organization.update", {"id": 3, "saml_attr_mapping": {"x": "y"}}
+        )
+        self.assert_status_code(response, 400)
+        assert (
+            "data.saml_attr_mapping must contain ['saml_id'] properties"
+            in response.json["message"]
+        )
+
+    def test_update_broken_saml_attr_mapping_unknown_field(self) -> None:
+        self.create_model(
+            "organization/3", {"name": "aBuwxoYU", "description": "XrHbAWiF"}
+        )
+        response = self.request(
+            "organization.update",
+            {"id": 3, "saml_attr_mapping": {"saml_id": "111", "unkown_field": "xxx"}},
+        )
+        self.assert_status_code(response, 400)
+        assert (
+            "data.saml_attr_mapping must not contain {'unkown_field'} properties"
             in response.json["message"]
         )
 
