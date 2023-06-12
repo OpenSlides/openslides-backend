55146fa0bb9a5f09b698157d991b489806fc9cc7
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2023-06-12 09:24:34 +0200

    Generate readable default passwords

--- openslides_backend/action/actions/user/password_mixin.py
+++ openslides_backend/action/actions/user/password_mixin.py
@@ -1,20 +1,11 @@
 from typing import Any, Dict
 
 from ...action import Action
-from ...util.crypto import get_random_string
 
 
-class PasswordCreateMixin(Action):
+class PasswordMixin(Action):
     def set_password(self, instance: Dict[str, Any]) -> Dict[str, Any]:
-        password = instance.get("default_password", "")
+        password = instance["default_password"]
         hashed_password = self.auth.hash(password)
         instance["password"] = hashed_password
         return instance
-
-    def generate_and_set_password(self, instance: Dict[str, Any]) -> Dict[str, Any]:
-        instance["default_password"] = self.generate_password()
-        return self.set_password(instance)
-
-    @staticmethod
-    def generate_password() -> str:
-        return get_random_string(10)
