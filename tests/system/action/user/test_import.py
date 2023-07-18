e22aa85ea865fa5acc0aad00b2c7f527c0408627
Ludwig Reiter <ludwig.reiter@intevation.de>
2023-07-18 15:36:00 +0200

    Update user.import with saml_id code

--- tests/system/action/user/test_import.py
+++ tests/system/action/user/test_import.py
@@ -151,6 +151,91 @@ class UserJsonImport(BaseActionTestCase):
             }
         }
 
+    def test_import_with_saml_id(self) -> None:
+        self.set_models(
+            self.get_action_worker_data(
+                6,
+                ImportState.NEW,
+                {"saml_id": {"value": "testsaml", "info": ImportState.NEW}},
+            )
+        )
+        response = self.request("user.import", {"id": 6, "import": True})
+        self.assert_status_code(response, 200)
+        self.assert_model_exists(
+            "user/2",
+            {
+                "username": "testsaml",
+                "saml_id": "testsaml",
+            },
+        )
+
+    def test_import_saml_id_error_new_and_saml_id_exists(self) -> None:
+        """Set saml_id 'testsaml' to user 1, add the import user 1 will be
+        found and the import should result in an error."""
+        self.set_models(
+            {
+                "user/1": {"saml_id": "testsaml"},
+                **self.get_action_worker_data(
+                    6,
+                    ImportState.NEW,
+                    {"saml_id": {"value": "testsaml", "info": ImportState.NEW}},
+                ),
+            }
+        )
+        response = self.request("user.import", {"id": 6, "import": True})
+        self.assert_status_code(response, 200)
+        entry = response.json["results"][0][0]["rows"][0]
+        assert entry["state"] == ImportState.ERROR
+        assert entry["messages"] == [
+            "Error: want to create a new user, but saml_id already exists."
+        ]
+
+    def test_import_done_with_saml_id(self) -> None:
+        self.set_models(
+            {
+                "user/2": {"username": "test", "saml_id": "testsaml"},
+                **self.get_action_worker_data(
+                    6,
+                    ImportState.DONE,
+                    {
+                        "saml_id": {"value": "testsaml", "info": ImportState.DONE},
+                        "id": 2,
+                        "first_name": "Hugo",
+                    },
+                ),
+            }
+        )
+        response = self.request("user.import", {"id": 6, "import": True})
+        self.assert_status_code(response, 200)
+        self.assert_model_exists(
+            "user/2",
+            {
+                "username": "test",
+                "saml_id": "testsaml",
+                "first_name": "Hugo",
+            },
+        )
+
+    def test_import_done_error_missing_user(self) -> None:
+        self.set_models(
+            {
+                **self.get_action_worker_data(
+                    6,
+                    ImportState.DONE,
+                    {
+                        "saml_id": {"value": "testsaml", "info": ImportState.NEW},
+                        "first_name": "Hugo",
+                        "id": 2,
+                    },
+                ),
+            }
+        )
+        response = self.request("user.import", {"id": 6, "import": True})
+        self.assert_status_code(response, 200)
+        entry = response.json["results"][0][0]["rows"][0]
+        assert entry["state"] == ImportState.ERROR
+        assert entry["messages"] == ["Error: want to update, but missing user in db."]
+
     def test_import_error_at_state_new(self) -> None:
         self.set_models(
             {
