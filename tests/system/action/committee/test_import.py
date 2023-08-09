8a6097230e0aa741ddaadf525f6a0e35d2993a7a
Ludwig Reiter <ludwig.reiter@intevation.de>
2023-08-09 10:45:14 +0200

    Reuse skipped tests

--- tests/system/action/committee/test_import.py
+++ tests/system/action/committee/test_import.py
@@ -1,5 +1,3 @@
-import pytest
-
 from openslides_backend.action.mixins.import_mixins import ImportState
 from openslides_backend.models.models import Meeting
 from openslides_backend.permissions.management_levels import OrganizationManagementLevel
@@ -135,7 +133,6 @@ class CommitteeImport(BaseActionTestCase):
         assert entry["state"] == ImportState.ERROR
         assert entry["messages"] == ["Want to update committee, but id mismatches."]
 
-    @pytest.mark.skip
     def test_import_new_and_done(self) -> None:
         self.set_models(
             {
@@ -165,7 +162,7 @@ class CommitteeImport(BaseActionTestCase):
                                     "organization_tags": [
                                         {
                                             "value": "ot1",
-                                            "info": ImportState.DONE,
+                                            "info": ImportState.NEW,
                                             "id": 7,
                                         },
                                         {"value": "ot2", "info": ImportState.NEW},
@@ -173,7 +170,7 @@ class CommitteeImport(BaseActionTestCase):
                                     "committee_managers": [
                                         {
                                             "value": "u1",
-                                            "info": ImportState.DONE,
+                                            "info": ImportState.NEW,
                                             "id": 5,
                                         },
                                         {"value": "u2", "info": ImportState.WARNING},
@@ -195,7 +192,7 @@ class CommitteeImport(BaseActionTestCase):
         )
         self.assert_model_exists(
             "user/5",
-            {"username": "u1", "commttee_management_ids": [14]},
+            {"username": "u1", "committee_management_ids": [14]},
         )
         self.assert_model_exists(
             "committee/14",
@@ -215,7 +212,6 @@ class CommitteeImport(BaseActionTestCase):
             {"name": "test2", "receive_forwardings_from_committee_ids": [14]},
         )
 
-    @pytest.mark.skip
     def test_import_reuse_fresh_created(self) -> None:
         self.set_models(
             {
@@ -248,10 +244,10 @@ class CommitteeImport(BaseActionTestCase):
                                         "info": ImportState.NEW,
                                     },
                                     "forward_to_committees": [
-                                        {"value": "test1", "info": ImportState.DONE}
+                                        {"value": "test1", "info": ImportState.NEW}
                                     ],
                                     "organization_tags": [
-                                        {"value": "ot", "info": ImportState.DONE}
+                                        {"value": "ot", "info": ImportState.NEW}
                                     ],
                                 },
                             },
