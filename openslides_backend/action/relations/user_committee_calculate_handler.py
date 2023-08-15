a09a95af9b0951d0fe27d599291e59de0a1491f6
Ludwig Reiter <ludwig.reiter@intevation.de>
2023-08-15 15:38:34 +0200

    Update method docstring

--- openslides_backend/action/relations/user_committee_calculate_handler.py
+++ openslides_backend/action/relations/user_committee_calculate_handler.py
@@ -19,15 +19,15 @@ from .typing import ListUpdateElement, RelationUpdates
 class UserCommitteeCalculateHandler(CalculatedFieldHandler):
     """
     CalculatedFieldHandler to fill the user.committee_ids and the related committee.user_ids
-    by catching modifications of UserMeeting.group_ids and User.committee_management_ids.
+    by catching modifications of MeetingUser.group_ids and User.committee_management_ids.
     A user belongs to a committee, if he is member of a meeting in the committee via group or
     he has rights on CommitteeManagementLevel.
     Problem: The changes come from 2 different collections, both could add or remove user/committee_relations.
     This method will calculate additions and removals by comparing the instances of datastore.changed_models and
     the stored db-content.
-    Calculates only 1 time per user on
-    1. user.committee_managment_ids, if changed, otherwise may not be triggered
-    2. UserMeeting.group_ids with lowest id of all changed ones
+    Calculates per user on
+    1. user.committee_managment_ids, if changed
+    2. MeetingUser.group_ids of all changes
     """
 
     def process_field(
