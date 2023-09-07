ff1539ab0238739c9d93545067f52d111bde7b40
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2023-09-07 14:24:43 +0200

    Clean up migration

--- tests/system/migrations/test_0045_fix_amendment_paragraph.py
+++ tests/system/migrations/test_0045_fix_amendment_paragraph.py
@@ -1,33 +1,5 @@
-
-
-def test_migration(write, finalize, assert_model, read_model):
-    """
-    ids for collections:
-     1x meeting_user (will be created)
-     2x committee
-     3x mediafile
-     4x meeting
-     5x group
-     6x motion
-     7x projector
-     8x poll
-     9x option
-    10x vote
-    11x personal_note
-    12x speaker
-    13x assignment_candidate
-    14x motion_submitter
-    15x chat_message
-    16x motion_state
-    17x list_of_speakers
-    18x assignment
-    19x chat_group
-    20x theme
-    21x motion_workflow
-    22x user
-    """
+def test_migration(write, finalize, assert_model):
     write(
-        # motions
         {
             "type": "create",
             "fqid": "motion/61",
@@ -42,26 +14,10 @@ def test_migration(write, finalize, assert_model, read_model):
         },
         {
             "type": "create",
-            "fqid": "motion/23880",
+            "fqid": "motion/62",
             "fields": {
-                "id": 23880,
-                "title": "Änderungsantrag zu 01",
-                "reason": "",
-                "created": 1678286817,
-                "state_id": 3564,
-                "meeting_id": 177,
-                "sort_weight": 10000,
-                "last_modified": 1678286817,
-                "meta_position": 13607,
-                "submitter_ids": [25353],
-                "lead_motion_id": 23879,
-                "category_weight": 10000,
-                "sequential_number": 2,
-                "start_line_number": 1,
-                "workflow_timestamp": 1678286817,
-                "list_of_speakers_id": 25721,
-                "amendment_paragraph_$": ["0"],
-                "amendment_paragraph_$0": "<p>Dieser Test Antrag soll weitergeleitet werden. Und dann dazu ein &Auml;nderungsantrag gestellt werden.</p>",
+                "id": 62,
+                "title": "test",
             },
         },
     )
@@ -80,24 +36,9 @@ def test_migration(write, finalize, assert_model, read_model):
         },
     )
     assert_model(
-        "motion/23880",
+        "motion/62",
         {
-            "id": 23880,
-            "title": "Änderungsantrag zu 01",
-            "reason": "",
-            "created": 1678286817,
-            "state_id": 3564,
-            "meeting_id": 177,
-            "sort_weight": 10000,
-            "last_modified": 1678286817,
-            "meta_position": 13607,
-            "submitter_ids": [25353],
-            "lead_motion_id": 23879,
-            "category_weight": 10000,
-            "sequential_number": 2,
-            "start_line_number": 1,
-            "workflow_timestamp": 1678286817,
-            "list_of_speakers_id": 25721,
-            "amendment_paragraphs": "<p>Dieser Test Antrag soll weitergeleitet werden. Und dann dazu ein &Auml;nderungsantrag gestellt werden.</p>",
+            "id": 62,
+            "title": "test",
         },
     )
