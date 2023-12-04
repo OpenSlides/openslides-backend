c3047df4f567b8fdcadc3b03bd9162e0be003c7a
Luisa <luisa.beerboom@intevation.de>
2023-12-04 14:29:57 +0100

    cleanup

--- openslides_backend/presenter/get_forwarding_meetings.py
+++ openslides_backend/presenter/get_forwarding_meetings.py
@@ -19,7 +19,7 @@ get_forwarding_meetings_schema = fastjsonschema.compile(
         "properties": {
             "meeting_id": required_id_schema,
         },
-        "required": ["meeting_id"]
+        "required": ["meeting_id"],
     }
 )
 
