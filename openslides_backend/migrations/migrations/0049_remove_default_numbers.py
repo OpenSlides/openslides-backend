d1ced357ec2506e294b3144884989a10a0ee4ad5
Luisa <luisa.beerboom@intevation.de>
2024-02-07 15:21:27 +0100

    Add migration

---
+++ openslides_backend/migrations/migrations/0049_remove_default_numbers.py
@@ -0,0 +1,26 @@
+from datastore.migrations import BaseModelMigration
+from datastore.shared.util import fqid_from_collection_and_id
+from datastore.writer.core import BaseRequestEvent, RequestUpdateEvent
+
+
+class Migration(BaseModelMigration):
+    """
+    This migration removes all default_number fields from user models
+    """
+
+    target_migration_index = 50
+
+    def migrate_models(self) -> list[BaseRequestEvent] | None:
+        events: list[BaseRequestEvent] = []
+        db_models = self.reader.get_all("user")
+        for id_, model in db_models.items():
+            if "default_number" in model:
+                events.append(
+                    RequestUpdateEvent(
+                        fqid_from_collection_and_id("user", id_),
+                        {
+                            "default_number": None,
+                        },
+                    )
+                )
+        return events
---
+++ tests/system/migrations/test_0049_remove_default_numbers.py
@@ -0,0 +1,51 @@
+def test_migration(write, finalize, assert_model):
+    write(
+        {
+            "type": "create",
+            "fqid": "user/1",
+            "fields": {
+                "id": 1,
+                "default_number": "WeAreNumberOne",
+            },
+        },
+    )
+    write(
+        {
+            "type": "create",
+            "fqid": "user/2",
+            "fields": {
+                "id": 2,
+                "default_number": "",
+            },
+        },
+    )
+    write(
+        {
+            "type": "create",
+            "fqid": "user/3",
+            "fields": {
+                "id": 3,
+            },
+        },
+    )
+
+    finalize("0049_remove_default_numbers")
+
+    assert_model(
+        "user/1",
+        {
+            "id": 1,
+        },
+    )
+    assert_model(
+        "user/2",
+        {
+            "id": 2,
+        },
+    )
+    assert_model(
+        "user/3",
+        {
+            "id": 3,
+        },
+    )
