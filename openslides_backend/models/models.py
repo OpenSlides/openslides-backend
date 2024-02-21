48edf75fbc1ce344d08694af8b04e6b3ef211b6e
Joshua Sangmeister <33004050+jsangmeister@users.noreply.github.com>
2024-02-21 16:12:40 +0100

    Enhance speaker creation (#2231)
    * Enhance speaker creation
    
    * Fix tests
    
    * Add setting to meeting.update
    
    * Fix readd_last
    
    * Update meta repo
    
    * Fix models.py
    
    ---------
    
    Co-authored-by: rrenkert <raimund@renkert.org>

--- openslides_backend/models/models.py
+++ openslides_backend/models/models.py
@@ -421,7 +421,11 @@ class Meeting(Model, MeetingModelMixin):
     )
     list_of_speakers_present_users_only = fields.BooleanField(default=False)
     list_of_speakers_show_first_contribution = fields.BooleanField(default=False)
+    list_of_speakers_allow_multiple_speakers = fields.BooleanField(default=False)
     list_of_speakers_enable_point_of_order_speakers = fields.BooleanField(default=True)
+    list_of_speakers_can_create_point_of_order_for_others = fields.BooleanField(
+        default=False
+    )
     list_of_speakers_enable_point_of_order_categories = fields.BooleanField(
         default=False
     )
