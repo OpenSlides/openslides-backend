861babfcaa1d8adf3bed429f7aa1f964aa756624
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2024-04-18 15:33:02 +0200

    Fix entrypoint & dc.local.yml

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 source scripts/export_datastore_variables.sh
 scripts/wait.sh $DATASTORE_WRITER_HOST $DATASTORE_WRITER_PORT
 
-# printf "\nMigrations:\n"
-# python openslides_backend/migrations/migrate.py finalize
-# printf "\n"
+printf "\nMigrations:\n"
+python openslides_backend/migrations/migrate.py finalize
+printf "\n"
 
 exec "$@"
