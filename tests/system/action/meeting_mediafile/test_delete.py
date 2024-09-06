af81250c1c1459c0cb64121e8a20b9b05a3086a6
Luisa <luisa.beerboom@intevation.de>
2024-09-06 15:25:44 +0200

    Complex meeting_mediafile delete test

--- tests/system/action/meeting_mediafile/test_delete.py
+++ tests/system/action/meeting_mediafile/test_delete.py
@@ -30,8 +30,19 @@ class MeetingMediafileDelete(BaseActionTestCase):
         self.create_meeting()
         self.set_models(
             {
-                "meeting/1": {"mediafile_ids": [10], "meeting_mediafile_ids": [2]},
-                "group/1": {"name": "X", "meeting_id": 1, "meeting_mediafile_access_group_ids": [2], "meeting_mediafile_inherited_access_group_ids": [2]},
+                "meeting/1": {
+                    "mediafile_ids": [10],
+                    "meeting_mediafile_ids": [2],
+                    "list_of_speakers_ids": [3],
+                    "all_projection_ids": [4],
+                    "logo_projector_main_id": 2,
+                },
+                "group/1": {
+                    "name": "X",
+                    "meeting_id": 1,
+                    "meeting_mediafile_access_group_ids": [2],
+                    "meeting_mediafile_inherited_access_group_ids": [2],
+                },
                 "mediafile/10": {
                     "title": "hOi",
                     "meeting_mediafile_ids": [2],
@@ -43,7 +54,20 @@ class MeetingMediafileDelete(BaseActionTestCase):
                     "inherited_access_group_ids": [1],
                     "mediafile_id": 10,
                     "is_public": False,
+                    "list_of_speakers_id": 3,
+                    "projection_ids": [4],
+                    "attachment_ids": ["topic/5"],
+                    "used_as_logo_projector_main_in_meeting_id": 1,
                 },
+                "list_of_speakers/3": {
+                    "meeting_id": 1,
+                    "content_object_id": "meeting_mediafile/2",
+                },
+                "projection/4": {
+                    "meeting_id": 1,
+                    "content_object_id": "meeting_mediafile/2",
+                },
+                "topic/5": {"meeting_id": 1, "attachment_meeting_mediafile_ids": [2]},
             }
         )
         response = self.request("meeting_mediafile.delete", {"id": 2})
@@ -51,3 +75,6 @@ class MeetingMediafileDelete(BaseActionTestCase):
         self.assert_model_deleted("meeting_mediafile/2")
         self.assert_model_exists("mediafile/10")
         self.assert_model_exists("group/1")
+        self.assert_model_deleted("list_of_speakers/3")
+        self.assert_model_deleted("projection/4")
+        self.assert_model_exists("topic/5")
