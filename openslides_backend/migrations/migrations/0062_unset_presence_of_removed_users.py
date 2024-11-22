95362095685ab7e67b948d8ef50a5b41c05691cf
Hannes Janott <hannes.janott@openslides.com>
2024-11-22 09:58:25 +0100

    refactor migration

--- openslides_backend/migrations/migrations/0062_unset_presence_of_removed_users.py
+++ openslides_backend/migrations/migrations/0062_unset_presence_of_removed_users.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
-from typing import Any
 
 from datastore.migrations import BaseModelMigration
+from datastore.reader.core import GetManyRequestPart
 from datastore.writer.core import BaseRequestEvent, RequestUpdateEvent
 
 from openslides_backend.shared.patterns import fqid_from_collection_and_id
+
+from ...shared.filters import And, FilterOperator
 
 
 class Migration(BaseModelMigration):
@@ -17,51 +19,39 @@ class Migration(BaseModelMigration):
     def migrate_models(self) -> list[BaseRequestEvent] | None:
         present_users_per_meeting: dict[int, list[int]] = defaultdict(list)
         meetings_per_present_user: dict[int, list[int]] = defaultdict(list)
-        meetings = self.reader.get_all(
-            "meeting", ["group_ids", "present_user_ids", "meeting_user_ids"]
+
+        meeting_users = self.reader.filter(
+            "meeting_user",
+            And(
+                FilterOperator("group_ids", "=", "[]"),
+                FilterOperator("meta_deleted", "!=", True),
+            ),
+            ["user_id", "meeting_id"],
         )
-        users = self.reader.get_all(
-            "user", ["is_present_in_meeting_ids", "meeting_user_ids"]
-        )
+        for meeting_user in meeting_users.values():
+            user_id = meeting_user["user_id"]
+            meeting_id = meeting_user["meeting_id"]
+            meetings_per_present_user[user_id].append(meeting_id)
+            present_users_per_meeting[meeting_id].append(user_id)
 
-        def collect_removals(
-            meeting_id: int,
-            user_id: int,
-            user: dict[str, Any],
-            meetings_per_present_user: dict[int, list[int]],
-            present_users_per_meeting: dict[int, list[int]],
-        ) -> None:
-            if not (
-                (meeting_user_ids := user.get("meeting_user_ids", []))
-                and any(
-                    meeting_user_id in meeting_user_ids
-                    for meeting_user_id in meeting.get("meeting_user_ids", [])
+        meetings = self.reader.get_many(
+            [
+                GetManyRequestPart(
+                    "meeting",
+                    [meeting_id for meeting_id in present_users_per_meeting],
+                    ["present_user_ids"],
                 )
-            ):
-                meetings_per_present_user[user_id].append(meeting_id)
-                present_users_per_meeting[meeting_id].append(user_id)
-
-        for meeting_id, meeting in meetings.items():
-            for user_id in meeting.get("present_user_ids", []):
-                user = users.get(user_id, dict())
-                collect_removals(
-                    meeting_id,
-                    user_id,
-                    user,
-                    meetings_per_present_user,
-                    present_users_per_meeting,
+            ]
+        ).get("meeting", dict())
+        users = self.reader.get_many(
+            [
+                GetManyRequestPart(
+                    "user",
+                    [present_user_id for present_user_id in meetings_per_present_user],
+                    ["is_present_in_meeting_ids"],
                 )
-
-        for user_id, user in users.items():
-            for meeting_id in user.get("is_present_in_meeting_ids", []):
-                collect_removals(
-                    meeting_id,
-                    user_id,
-                    user,
-                    meetings_per_present_user,
-                    present_users_per_meeting,
-                )
-
+            ]
+        ).get("user", dict())
         return [
             *[
                 RequestUpdateEvent(
@@ -77,6 +67,7 @@ class Migration(BaseModelMigration):
                     },
                 )
                 for meeting_id, user_ids in present_users_per_meeting.items()
+                if meetings
             ],
             *[
                 RequestUpdateEvent(
--- tests/system/migrations/test_0062_unset_presence_of_removed_users.py
+++ tests/system/migrations/test_0062_unset_presence_of_removed_users.py
@@ -3,37 +3,46 @@ from typing import Any
 
 def create_data() -> dict[str, dict[str, Any]]:
     return {
-        "meeting/11": {
+        "meeting/1": {
             "id": 1,
             "name": "meeting name",
-            "present_user_ids": [1, 2, 3],
-            "meeting_user_ids": [3],
+            "present_user_ids": [1, 3, 2],
+            "meeting_user_ids": [3, 4, 5],
+            "group_ids": [10],
         },
-        "meeting/111": {
+        "meeting/2": {
             "id": 1,
             "name": "meeting name",
-            "present_user_ids": [2, 3],
-            "meeting_user_ids": [4],
+            "present_user_ids": [2],
+            "meeting_user_ids": [6],
         },
         "user/1": {
             "id": 1,
-            "username": "wrong_user",
-            "is_present_in_meeting_ids": [11],
+            "username": "correct_user",
+            "is_present_in_meeting_ids": [1],
+            "meeting_user_ids": [3],
         },
         "user/2": {
             "id": 2,
-            "username": "correct_user",
-            "is_present_in_meeting_ids": [11, 111],
-            "meeting_user_ids": [3, 4],
+            "username": "wrong_user",
+            "is_present_in_meeting_ids": [2, 1],
+            # meeting users do exist after user left meeting but have no group ids
+            "meeting_user_ids": [
+                5,
+                6,
+            ],
         },
         "user/3": {
-            "id": 2,
+            "id": 3,
             "username": "correct_user",
-            "is_present_in_meeting_ids": [11, 111],
-            "meeting_user_ids": [],
+            "is_present_in_meeting_ids": [1],
+            "meeting_user_ids": [4],
         },
-        "meeting_user/3": {"id": 3, "user_id": 2, "meeting_id": 11},
-        "meeting_user/4": {"id": 4, "user_id": 2, "meeting_id": 111},
+        "meeting_user/3": {"id": 3, "user_id": 1, "meeting_id": 1, "group_ids": [10]},
+        "meeting_user/4": {"id": 4, "user_id": 3, "meeting_id": 1, "group_ids": [10]},
+        "meeting_user/5": {"id": 5, "user_id": 2, "meeting_id": 1, "group_ids": []},
+        "meeting_user/6": {"id": 6, "user_id": 2, "meeting_id": 2, "group_ids": []},
+        "group/10": {"id": 10, "meeting_user_ids": [3, 4], "meeting_id": 1},
     }
 
 
@@ -44,10 +53,9 @@ def test_migration_both_ways(write, finalize, assert_model):
 
     finalize("0062_unset_presence_of_removed_users")
 
-    data["meeting/11"]["present_user_ids"] = [2]
-    data["meeting/111"]["present_user_ids"] = [2]
-    data["user/1"]["is_present_in_meeting_ids"] = []
-    data["user/3"]["is_present_in_meeting_ids"] = []
+    data["meeting/1"]["present_user_ids"] = [1, 3]
+    data["meeting/2"]["present_user_ids"] = []
+    data["user/2"]["is_present_in_meeting_ids"] = []
 
     for fqid, fields in data.items():
         assert_model(fqid, fields)
@@ -55,16 +63,15 @@ def test_migration_both_ways(write, finalize, assert_model):
 
 def test_migration_one_way(write, finalize, assert_model):
     data = create_data()
-    data["meeting/11"]["present_user_ids"] = [2]
+    data["meeting/1"]["present_user_ids"] = [1, 3]
+    data["meeting/2"]["present_user_ids"] = []
 
     for fqid, fields in data.items():
         write({"type": "create", "fqid": fqid, "fields": fields})
 
     finalize("0062_unset_presence_of_removed_users")
 
-    data["user/1"]["is_present_in_meeting_ids"] = []
-    data["meeting/111"]["present_user_ids"] = [2]
-    data["user/3"]["is_present_in_meeting_ids"] = []
+    data["user/2"]["is_present_in_meeting_ids"] = []
 
     for fqid, fields in data.items():
         assert_model(fqid, fields)
@@ -72,16 +79,15 @@ def test_migration_one_way(write, finalize, assert_model):
 
 def test_migration_other_way(write, finalize, assert_model):
     data = create_data()
-    data["user/1"]["is_present_in_meeting_ids"] = []
+    data["user/2"]["is_present_in_meeting_ids"] = []
 
     for fqid, fields in data.items():
         write({"type": "create", "fqid": fqid, "fields": fields})
 
     finalize("0062_unset_presence_of_removed_users")
 
-    data["meeting/11"]["present_user_ids"] = [2]
-    data["meeting/111"]["present_user_ids"] = [2]
-    data["user/3"]["is_present_in_meeting_ids"] = []
+    data["meeting/1"]["present_user_ids"] = [1, 3]
+    data["meeting/2"]["present_user_ids"] = []
 
     for fqid, fields in data.items():
         assert_model(fqid, fields)
