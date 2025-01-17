fbc897fdf00aa5ad4ad7e0aff7ad756fb24a94ab
Hannes Janott <hannes.janott@openslides.com>
2025-01-17 11:26:39 +0100

    initial classes

---
+++ openslides_backend/services/database/mapped_fields.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+from openslides_backend.shared.patterns import Collection, Field, FullQualifiedId
+
+# Postgres only supports 1664 columns per query, so choose a number below that
+MAX_UNIQUE_FIELDS_PER_QUERY = 1000
+
+
+class MappedFields:
+    """
+    Container class for mapped fields and all related data. Only automatically calculated field is
+    `needs_whole_model`, all other fields have to be set by the user.
+    """
+
+    per_fqid: dict[FullQualifiedId, list[Field]]
+    unique_fields: list[Field]
+    collections: list[Collection]
+    fqids: list[FullQualifiedId]
+    needs_whole_model: bool
+
+    def __init__(self, mapped_fields: list[Field] = []) -> None:
+        self.per_fqid = defaultdict(list)
+        self.unique_fields = mapped_fields
+        self.collections = []
+        self.post_init()
+
+    def post_init(self) -> None:
+        self.fqids = list(self.per_fqid.keys())
+        self.needs_whole_model = (
+            len(self.unique_fields) == 0
+            or any(len(fields) == 0 for fields in self.per_fqid.values())
+            or len(self.unique_fields) > MAX_UNIQUE_FIELDS_PER_QUERY
+        )
