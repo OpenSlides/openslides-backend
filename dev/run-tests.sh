5468d41e25954da9aa508eca5a088a5db8633381
Hannes Janott <hannes.janott@openslides.com>
2025-06-11 14:03:51 +0200

    do not wait for auth in run-tests

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -5,7 +5,7 @@ export COMPOSE_DOCKER_CLI_BUILD=0
 DC="docker compose -f dev/docker-compose.dev.yml"
 
 $DC up --build --detach
-$DC exec -T backend scripts/wait.sh auth 9004
+# $DC exec -T backend scripts/wait.sh auth 9004
 $DC exec -T backend ./entrypoint.sh pytest --cov
 error=$?
 $DC down --volumes
