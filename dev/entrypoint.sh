c8eb6ab600f53689a9a9d5a7b9140f2b349d3279
Hannes Janott <hannes.janott@openslides.com>
2025-06-11 12:13:00 +0200

    dev/entrypoint wait for database

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -7,6 +7,9 @@ source scripts/export_database_variables.sh
 printf "\nOpenslides Database:\n"
 python cli/create_schema.py
 printf "\n"
+echo "enter entrypoint.sh"
+meta/dev/scripts/wait-for-database.sh
+echo "database is started"
 
 # TODO: Re-add this code
 # printf "\nMigrations:\n"
