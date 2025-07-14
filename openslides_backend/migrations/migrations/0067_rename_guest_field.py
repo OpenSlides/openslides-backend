db4bd7722d27e1ff481ef4827d79dcebcd7dfb65
vkrasnovyd <v.krasnovyd@proton.me>
2025-07-14 09:17:23 +0200

    Update migration

--- openslides_backend/migrations/migrations/0067_rename_guest_field.py
+++ openslides_backend/migrations/migrations/0067_rename_guest_field.py
@@ -1,7 +1,12 @@
-from datastore.migrations import RenameFieldMigration
+from datastore.migrations import BaseModelMigration
+from datastore.writer.core import BaseRequestEvent, RequestUpdateEvent
+
+from openslides_backend.shared.patterns import fqid_from_collection_and_id
+
+from ...shared.filters import And, FilterOperator
 
 
-class Migration(RenameFieldMigration):
+class Migration(BaseModelMigration):
     """
     This migration renames:
     user/guest
@@ -15,3 +20,23 @@ class Migration(RenameFieldMigration):
     collection = "user"
     old_field = "guest"
     new_field = "external"
+
+    def migrate_models(self) -> list[BaseRequestEvent]:
+        db_models = self.reader.filter(
+            collection=self.collection,
+            filter=And(
+                FilterOperator(self.old_field, "!=", None),
+                FilterOperator("meta_deleted", "!=", True),
+            ),
+        )
+
+        return [
+            RequestUpdateEvent(
+                fqid_from_collection_and_id(self.collection, id),
+                {
+                    self.old_field: None,
+                    self.new_field: model.get(self.old_field),
+                },
+            )
+            for id, model in db_models.items()
+        ]
