5e6fc1095cdbc64ebf3e82faefb54be7764e4e90
Hannes Janott <hannes.janott@openslides.com>
2025-07-24 14:49:38 +0200

    no deletion test for los

--- tests/system/action/list_of_speakers/test_delete.py
+++ tests/system/action/list_of_speakers/test_delete.py
@@ -2,7 +2,14 @@ from tests.system.action.base import BaseActionTestCase
 
 
 class ListOfSpeakersDeleteActionTest(BaseActionTestCase):
-    def test_delete_correct(self) -> None:
+    """
+    This action is backend internal and can only successfully be called by the content objects delete action.
+    Correct deletion shall be assured through tests of the possible content objects.
+    Since in this example topic has the reference required and is not deleted cascadingly
+    it's not possible to test successful deletion here.
+    """
+
+    def test_delete_correct_id(self) -> None:
         self.create_meeting(78)
         self.set_models(
             {
@@ -30,10 +37,14 @@ class ListOfSpeakersDeleteActionTest(BaseActionTestCase):
         )
         response = self.request("list_of_speakers.delete", {"id": 111})
 
-        self.assert_status_code(response, 200)
-        self.assert_model_not_exists("list_of_speakers/111")
-        self.assert_model_not_exists("projection/1")
-        self.assert_model_not_exists("topic/42")
+        self.assert_status_code(response, 400)
+        self.assertEqual(
+            response.json["message"],
+            "Update of topic/42: You try to set following required fields to an empty value: ['list_of_speakers_id']",
+        )
+        self.assert_model_exists("list_of_speakers/111")
+        self.assert_model_exists("projection/1")
+        self.assert_model_exists("topic/42")
 
     def test_delete_wrong_id(self) -> None:
         self.create_meeting(78)
