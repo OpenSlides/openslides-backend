6b0403405cc4adc6013719ec7505df9fca9d4349
Hannes Janott <hannes.janott@openslides.com>
2025-07-28 12:24:29 +0200

    Complete structure level list of speakers tests (#3051)
    * fix structure level and list of speakers tests
    * more fixes

--- tests/system/action/structure_level_list_of_speakers/test_delete.py
+++ tests/system/action/structure_level_list_of_speakers/test_delete.py
@@ -8,21 +8,28 @@ class StructureLevelListOfSpeakersDeleteTest(BaseActionTestCase):
             {
                 "meeting/1": {
                     "list_of_speakers_default_structure_level_time": 600,
-                    "structure_level_ids": [1],
-                    "list_of_speakers_ids": [2],
-                    "structure_level_list_of_speakers_ids": [3],
+                },
+                "topic/32": {
+                    "title": "leet improvement discussion",
+                    "sequential_number": 32,
+                    "meeting_id": 1,
                 },
                 "structure_level/1": {
+                    "name": "monkey",
                     "meeting_id": 1,
                     "structure_level_list_of_speakers_ids": [3],
                 },
                 "list_of_speakers/2": {
                     "meeting_id": 1,
+                    "sequential_number": 2,
+                    "content_object_id": "topic/32",
                     "structure_level_list_of_speakers_ids": [3],
                 },
                 "structure_level_list_of_speakers/3": {
                     "structure_level_id": 1,
                     "list_of_speakers_id": 2,
+                    "initial_time": 400,
+                    "remaining_time": 300,
                     "meeting_id": 1,
                 },
             }
@@ -32,18 +39,18 @@ class StructureLevelListOfSpeakersDeleteTest(BaseActionTestCase):
         self.assert_model_exists(
             "meeting/1",
             {
-                "structure_level_list_of_speakers_ids": [],
+                "structure_level_list_of_speakers_ids": None,
             },
         )
         self.assert_model_exists(
             "structure_level/1",
             {
-                "structure_level_list_of_speakers_ids": [],
+                "structure_level_list_of_speakers_ids": None,
             },
         )
         self.assert_model_exists(
             "list_of_speakers/2",
             {
-                "structure_level_list_of_speakers_ids": [],
+                "structure_level_list_of_speakers_ids": None,
             },
         )
