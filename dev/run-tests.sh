c451c640bebcd9d1d414b55811af143e37e2e4c5
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-09-09 12:47:31 +0200

    Supress closing container messages and replace complicated local pwd path with simpler one

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -18,7 +18,7 @@ done
 
 # Setup
 IMAGE_TAG=openslides-backend-tests
-LOCAL_PWD=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+LOCAL_PWD=$(dirname "$0")
 export COMPOSE_DOCKER_CLI_BUILD=0
 
 # Helpers
