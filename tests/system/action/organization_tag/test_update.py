0d5b13e171de9a36c871193bd6192ba8665376d7
Viktoriia Krasnovyd <114735598+vkrasnovyd@users.noreply.github.com>
2025-10-08 15:32:13 +0200

    [rel-DB] Complete organization tests (#3159)

--- tests/system/action/organization_tag/test_update.py
+++ tests/system/action/organization_tag/test_update.py
@@ -1,9 +1,13 @@
+from openslides_backend.permissions.management_levels import OrganizationManagementLevel
 from tests.system.action.base import BaseActionTestCase
 
 
 class OrganizationTagUpdateActionTest(BaseActionTestCase):
-    def test_update_correct(self) -> None:
+    def setUp(self) -> None:
+        super().setUp()
         self.set_models({"organization_tag/1": {"name": "old", "color": "#000000"}})
+
+    def test_update_correct(self) -> None:
         response = self.request(
             "organization_tag.update", {"id": 1, "name": "test", "color": "#121212"}
         )
@@ -13,32 +17,25 @@ class OrganizationTagUpdateActionTest(BaseActionTestCase):
         )
 
     def test_no_permission(self) -> None:
-        self.set_models(
-            {
-                "user/1": {"organization_management_level": "can_manage_users"},
-                "organization_tag/1": {"name": "test", "color": "#000000"},
-            }
+        self.set_organization_management_level(
+            OrganizationManagementLevel.CAN_MANAGE_USERS
         )
         response = self.request(
             "organization_tag.update", {"id": 1, "name": "test", "color": "#121212"}
         )
         self.assert_status_code(response, 403)
-        assert (
-            "You are not allowed to perform action organization_tag.update. Missing OrganizationManagementLevel: can_manage_organization"
-            in response.json["message"]
+        self.assertEqual(
+            "You are not allowed to perform action organization_tag.update. Missing OrganizationManagementLevel: can_manage_organization",
+            response.json["message"],
         )
 
     def test_permission(self) -> None:
-        self.set_models(
-            {
-                "user/1": {"organization_management_level": "can_manage_organization"},
-                "organization_tag/1": {"name": "old", "color": "#000000"},
-            }
+        self.base_permission_test(
+            {},
+            "organization_tag.update",
+            {"id": 1, "name": "test", "color": "#121212"},
+            OrganizationManagementLevel.CAN_MANAGE_ORGANIZATION,
         )
-        response = self.request(
-            "organization_tag.update", {"id": 1, "name": "test", "color": "#121212"}
-        )
-        self.assert_status_code(response, 200)
         self.assert_model_exists(
             "organization_tag/1", {"name": "test", "color": "#121212"}
         )
