27879af39f2cecf3e406c0b3cb4a4ddeea80175a
Hannes Janott <hannes.janott@openslides.com>
2025-10-14 16:33:29 +0200

    base pre_migration for vote service

--- openslides_backend/migrations/migrations_reldb/0070_init_reldb.py
+++ openslides_backend/migrations/migrations_reldb/0070_init_reldb.py
@@ -6,6 +6,7 @@ from math import ceil
 from typing import Any
 
 from psycopg.rows import dict_row
+from psycopg.types.json import Jsonb
 
 from meta.dev.src.helper_get_names import HelperGetNames  # type: ignore # noqa
 from openslides_backend.models.base import Model, model_registry
@@ -19,6 +20,15 @@ from openslides_backend.models.fields import (
 )
 from openslides_backend.models.models import *  # type: ignore # noqa # necessary to fill model_registry
 from openslides_backend.services.postgresql.db_connection_handling import os_conn_pool
+from openslides_backend.shared.patterns import (
+    FullQualifiedId,
+    Id,
+    collection_and_id_from_fqid,
+    collection_from_fqid,
+    fqid_from_collection_and_id,
+    id_from_fqid,
+)
+from openslides_backend.shared.typing import Collection, PartialModel
 
 RELATION_LIST_FIELD_CLASSES = [RelationListField, GenericRelationListField]
 
@@ -153,7 +163,9 @@ class Sql_helper:
     # END OF FUNCTION
 
     @staticmethod
-    def get_insert_intermediate_t_commands(field: Field, data: dict[str, Any]) -> list:
+    def get_insert_intermediate_t_commands(
+        field: Field, data: dict[str, Any]
+    ) -> list[tuple[str, list[Any]]]:
         """
         Purpose:
             Parses the dictionary data into a sql friendly string format.
@@ -194,6 +206,47 @@ class Sql_helper:
 
     # END OF FUNCTION
 
+    @staticmethod
+    def get_select_collection_command(
+        collection: Collection, mapped_fields: list[str]
+    ) -> str:
+        """
+        Purpose:
+            Generates SQL to get all of the collections models from the models->>data column.
+        Input:
+            - collection: collection that will be retrieved.
+            - mapped_fields: the fields/columns that are to be retrieved.
+        Returns:
+            - sql string that can be executed.
+        """
+        mf_string = ",".join(mapped_fields)
+        return f"""
+            SELECT {mf_string} FROM models
+            WHERE fqid LIKE '{fqid_from_collection_and_id(collection, '%')}';
+            """
+
+    # END OF FUNCTION
+
+    @staticmethod
+    def get_update_models_command(
+        models: list[dict[str, Any]],
+    ) -> tuple[str, list[tuple[Jsonb, FullQualifiedId],]]:
+        """
+        Purpose:
+            Generates SQL to be executed with executemany for updating models by overwriting their models->>data column.
+        Input:
+            - models: list of all elements that need to be updated. Its dicts must contain 'fqid' and 'data'.
+        Returns:
+            - SQL string that can be executed.
+            - Argument list to be directly passed with SQL. Holding a tuple for each model with Jsonb and the fqid.
+        """
+        return (
+            """UPDATE models SET data=%s WHERE fqid=%s""",
+            [(Jsonb(elem["data"]), elem["fqid"]) for elem in models],
+        )
+
+    # END OF FUNCTION
+
 
 # END HELPER CLASS
 
@@ -222,6 +275,220 @@ def data_definition() -> None:
 # END OF FUNCTION
 
 
+def poll_routine(polls: list[dict[str, Any]], options: dict[Id, PartialModel]) -> None:
+    """
+    Purpose:
+        Alters all poll models specified by the new vote service.
+    Input:
+        - polls: list of polls that shall be modified. Needs 'fqid' and 'data'.
+        - options: options containing all necessary information.
+    Returns:
+        n/a
+    Commentary:
+        changes to poll
+        type'analog', 'named', 'pseudoanonymous', 'cryptographic' -> visibility'manually', 'named', 'secret'
+        pollmethod -> method 'approval', 'selection', 'rating-score', 'rating-approval'
+        state `published` -> `finished` + published boolean
+        x -> config?
+            min_votes_amount
+            max_votes_amount
+            max_votes_per_option
+            global_yes
+            global_no
+            global_abstain
+            onehundred_percent_base TODO after the client is done
+            entitled_users_at_stop TODO after the client is done
+        x -> result?
+            option_ids
+            global_option_id
+            votesvalid
+            votesinvalid
+            votescast
+    """
+    for poll in polls:
+        collection, id_ = collection_and_id_from_fqid(poll["fqid"])
+        data = poll["data"]
+        result = dict()
+        config = dict()
+
+        # for config
+        global_no = data.pop("global_no", None)
+        global_yes = data.pop("global_yes", None)
+        global_abstain = data.pop("global_abstain", None)
+        min_votes_amount = data.pop("min_votes_amount", None)
+        max_votes_amount = data.pop("max_votes_amount", None)
+        max_votes_per_option = data.pop("max_votes_per_option", None)
+        onehundred_percent_base = data.pop("onehundred_percent_base", None)
+        entitled_users_at_stop = data.pop("entitled_users_at_stop", None)
+        config["global_no"] = global_no
+        config["global_yes"] = global_yes
+        config["global_abstain"] = global_abstain
+        config["min_votes_amount"] = min_votes_amount
+        config["max_votes_amount"] = max_votes_amount
+        config["max_votes_per_option"] = max_votes_per_option
+        config["onehundred_percent_base"] = onehundred_percent_base
+        config["entitled_users_at_stop"] = entitled_users_at_stop
+
+        # for result
+        option_ids = data.pop("option_ids", None)
+        global_option_id = data.pop("global_option_id", None)
+        votesvalid = data.pop("votesvalid", None)
+        votesinvalid = data.pop("votesinvalid", None)
+        votescast = data.pop("votescast", None)
+        result["option_ids"] = option_ids
+        result["global_option_id"] = global_option_id
+        result["votesvalid"] = votesvalid
+        result["votesinvalid"] = votesinvalid
+        result["votescast"] = votescast
+
+        content_object_type = collection_from_fqid(data["content_object_id"])
+
+        pollmethod = data.pop("pollmethod")
+
+        type_ = data.pop("type")
+        match type_:
+            case "analog":
+                data["visibility"] = "manually"
+            case "pseudoanonymous", "cryptographic":
+                data["visibility"] = "secret"
+            case _:
+                data["visibility"] = type_
+
+        if (state := data.get("state", "")) and state == "published":
+            data["state"] = "finished"
+            data["published"] = True
+
+        match content_object_type:
+            case "motion":
+                data["method"] = "approval"
+                result = dict() if pollmethod == "YNA" else {"allow_abstain": False}
+            case "assignment" | "topic":
+                data["method"] = "rating-approval"
+                result = dict() if pollmethod == "YNA" else {"allow_abstain": False}
+            case _:
+                raise Exception(
+                    f"Polls with this content object type ({content_object_type}) do not exist."
+                )
+
+        if option_id := data.pop("option_id", 0):
+            option = options[option_id]
+            result["option_ids?"] = option["content_object_id?"]
+
+        data["result"] = json_dumps(result)
+        data["config"] = json_dumps(config)
+
+
+def vote_routine(votes: list[dict[str, Any]], options: dict[Id, PartialModel]) -> None:
+    """
+    Purpose:
+        Alters all vote models specified by the new vote service.
+    Input:
+        - votes: list of votes that shall be modified. Needs 'fqid' and 'data'.
+        - options: options containing all necessary information.
+    Returns:
+        n/a
+    Commentary:
+        changes to vote
+        pop option_id
+        poll_id = option/poll_id
+    """
+    for vote in votes:
+        collection, id_ = collection_and_id_from_fqid(vote["fqid"])
+        data = vote["data"]
+        if option_id := data.pop("option_id", 0):
+            option = options[option_id]
+            data["poll_id"] = option["poll_id"]
+
+
+def pre_migration() -> None:
+    """
+    Purpose:
+        Migrates all vote_service related models so that the data will fit the table scheme.
+    Input:
+        n/a
+    Returns:
+        n/a
+    """
+    definitions: dict = {
+        "poll": {
+            "del": [
+                "description",
+                "backend",
+                "is_pseudoanonymized",
+                "live_voting_enabled",
+                "live_votes",
+                "crypt_key",
+                "crypt_signature",
+                "votes_raw",
+                "votes_signature",
+            ],
+            "routine": poll_routine,
+        },
+        "user": {
+            "del": ["option_ids"],
+            "renames": {
+                "vote_ids": "acting_vote_ids",
+                "delegated_vote_ids": "represented_vote_ids",
+            },
+        },
+        "motion": {"del": ["option_ids"]},
+        "meeting": {
+            "del": ["option_ids", "vote_ids", "poll_default_backend"],
+            "set": {
+                "poll_default_allow_invalid": False,
+                "poll_default_allow_vote_split": False,
+            },
+        },
+        "vote": {
+            "del": ["meeting_id", "option_id"],
+            "renames": {
+                "user_id": "acting_user_id",
+                "delegated_user_id": "represented_user_id",
+            },
+            "routine": vote_routine,
+        },
+        "assignment": {"del": ["option_id"]},
+        "organization": {"del": ["vote_decrypt_public_main_key"]},
+    }
+
+    # 1) Prepare connection & cursor
+    with os_conn_pool.connection() as conn:
+        with conn.cursor() as cur:
+
+            # 2) BEGIN TRANSACTION
+            with conn.transaction():
+                # 2.1) Get options out of the way
+                # needed to generate poll/result and vote
+                cur.execute(Sql_helper.get_select_collection_command("option", ["*"]))
+                options = {
+                    id_from_fqid(elem["fqid"]): elem["data"] for elem in cur.fetchall()
+                }
+                cur.execute("DELETE FROM models WHERE fqid LIKE 'option/%';")
+
+                for collection, definition in definitions.items():
+                    cur.execute(
+                        Sql_helper.get_select_collection_command(collection, ["*"])
+                    )
+                    result = cur.fetchall()
+                    # 2.2) Do generic changes: Delete, rename, set
+                    for elem in result:
+                        collection, id_ = collection_and_id_from_fqid(elem["fqid"])
+                        data = elem["data"]
+                        for field_name in definition.get("del", []):
+                            data.pop(field_name, None)
+                        for old_field, new_field in definition.get(
+                            "renames", {}
+                        ).items():
+                            data[new_field] = data.pop(old_field, None)
+                        if "set" in definition:
+                            data.update(definition["set"])
+                    # 2.3) Run routine
+                    if "routine" in definition:
+                        definition["routine"](result, options)
+                    # 3) Apply changes for collection
+                    cur.executemany(*Sql_helper.get_update_models_command(result))
+
+
 def data_manipulation() -> None:
     """
     Purpose:
@@ -236,6 +503,8 @@ def data_manipulation() -> None:
         Thus it is worth the idea to set the intermediate tables to INITIALLY DEFERRED as well
         so they can be run immediately. This is a measure we already took for default tables.
     """
+    pre_migration()
+
     data_chunk: list[dict[str, Any]]
     collection: str
     table_name: str
