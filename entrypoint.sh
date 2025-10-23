0f3e1ec56f6f99883d3822147a22c974d6f6b280
rrenkert <raimund.renkert@intevation.de>
2025-10-23 16:58:27 +0200

    Add schema creation to entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 source scripts/export_database_variables.sh
 
+printf "Create schema.\n"
+python cli/create_schema.py
+printf "\n"
+
 if [ ! $ANONYMOUS_ONLY ]; then
   meta/dev/scripts/wait-for-database.sh
 fi
