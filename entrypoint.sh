542acef64a43f6ffe44e378431c333067d1c0311
Hannes Janott <hannes.janott@openslides.com>
2025-11-13 11:35:52 +0100

    wait for database

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+printf "enter entrypoint.sh"
+set -e
+
+printf "\nOpenslides DBMS:\n"
+printf "Export env variables for database.\n"
 source scripts/export_database_variables.sh
+
+if [ ! $ANONYMOUS_ONLY ]; then
+  meta/dev/scripts/wait-for-database.sh
+  printf "DBMS is started.\n"
+fi
 
 printf "Create schema.\n"
 python cli/create_schema.py
 printf "\n"
-
-if [ ! $ANONYMOUS_ONLY ]; then
-  meta/dev/scripts/wait-for-database.sh
-fi
 
 exec "$@"
