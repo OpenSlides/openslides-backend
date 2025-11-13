88a058c067db38515d0124cb87a86211dcf47b54
Hannes Janott <hannes.janott@openslides.com>
2025-11-13 14:37:22 +0100

    ls debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ printf "\nOpenslides DBMS:\n"
 printf "Export env variables for database.\n"
 source scripts/export_database_variables.sh
 
+ls meta
+ls meta/dev/scripts
 if [ ! $ANONYMOUS_ONLY ]; then
   meta/dev/scripts/wait-for-database.sh
   printf "DBMS is started.\n"
