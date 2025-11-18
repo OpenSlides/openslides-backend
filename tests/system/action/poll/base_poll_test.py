5d864297fb11900cb937270d9344e1c5cb4ecdc8
Viktoriia Krasnovyd <114735598+vkrasnovyd@users.noreply.github.com>
2025-11-18 16:15:22 +0100

    [rel-DB] Complete poll tests (#3209)

--- tests/system/action/poll/base_poll_test.py
+++ tests/system/action/poll/base_poll_test.py
@@ -1,10 +1,26 @@
-import pytest
+from typing import Any
 
 from tests.system.action.base import BaseActionTestCase
 
 
-@pytest.mark.skip(reason="During development of relational DB not necessary")
 class BasePollTestCase(BaseActionTestCase):
     def setUp(self) -> None:
         super().setUp()
         self.vote_service.clear_all()
+
+    def create_assignment(
+        self, base: int, meeting_id: int, assignment_data: dict[str, Any] = {}
+    ) -> None:
+        self.set_models(
+            {
+                f"assignment/{base}": {
+                    "title": "just do it",
+                    "meeting_id": meeting_id,
+                    **assignment_data,
+                },
+                f"list_of_speakers/{base + 100}": {
+                    "content_object_id": f"assignment/{base}",
+                    "meeting_id": meeting_id,
+                },
+            }
+        )
