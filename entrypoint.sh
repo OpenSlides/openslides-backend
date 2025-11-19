c63e0ddc4411de2db6a09fd9c442e173b96c94fa
Hannes Janott <hannes.janott@openslides.com>
2025-11-19 12:05:31 +0100

    git revert adding extended database

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,6 @@ printf "\nOpenslides DBMS:\n"
 printf "Export env variables for database.\n"
 source scripts/export_database_variables.sh
 
-ls meta
-ls meta/dev/scripts
 if [ ! $ANONYMOUS_ONLY ]; then
   meta/dev/scripts/wait-for-database.sh
   printf "DBMS is started.\n"
