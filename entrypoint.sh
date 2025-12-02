766eb9fcfb95b7436007d0d560d5f8ce82bddabf
vkrasnovyd <viktoriia.krasnovyd@intevation.de>
2025-12-02 18:04:48 +0100

    Re-order

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 source scripts/export_database_variables.sh
 
-printf "Create schema.\n"
-python cli/create_schema.py
-printf "\n"
-
 if [ ! $ANONYMOUS_ONLY ]; then
   meta/dev/scripts/wait-for-database.sh
 fi
+
+printf "Create schema.\n"
+python cli/create_schema.py
+printf "\n"
 
 exec "$@"
