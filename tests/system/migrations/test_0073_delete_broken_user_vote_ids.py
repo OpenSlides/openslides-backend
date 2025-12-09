330cc48cd9abdf43fc028f971b9a512c274f160d
Hannes Janott <hannes.janott@openslides.com>
2025-12-09 14:59:59 +0100

    add delegated_user_id to migration

--- tests/system/migrations/test_0073_delete_broken_user_vote_ids.py
+++ tests/system/migrations/test_0073_delete_broken_user_vote_ids.py
@@ -8,23 +8,35 @@ def test_migration_simple(write, finalize, assert_model):
     write(
         # halfed relation
         {"type": "create", "fqid": "user/1", "fields": {"vote_ids": [2]}},
-        {"type": "create", "fqid": "vote/2", "fields": {"user_id": None}},
+        {"type": "create", "fqid": "user/2", "fields": {"delegated_vote_ids": [2]}},
+        {
+            "type": "create",
+            "fqid": "vote/2",
+            "fields": {"user_id": None, "delegated_user_id": None},
+        },
         # healthy relation
         {"type": "create", "fqid": "user/3", "fields": {"vote_ids": [4]}},
         {"type": "create", "fqid": "vote/4", "fields": {"user_id": 3}},
         # deleted vote
         {"type": "create", "fqid": "user/5", "fields": {"vote_ids": [6]}},
-        {"type": "create", "fqid": "vote/6", "fields": {"user_id": 5}},
+        {"type": "create", "fqid": "user/6", "fields": {"delegated_vote_ids": [6]}},
+        {
+            "type": "create",
+            "fqid": "vote/6",
+            "fields": {"user_id": 5, "delegated_user_id": 6},
+        },
         {"type": "delete", "fqid": "vote/6"},
     )
 
     finalize("0073_delete_broken_user_vote_ids")
 
     assert_model("user/1", {"vote_ids": []})
+    assert_model("user/2", {"delegated_vote_ids": []})
     assert_model("vote/2", {})
 
     assert_model("user/3", {"vote_ids": [4]})
     assert_model("vote/4", {"user_id": 3})
 
     assert_model("user/5", {"vote_ids": []})
-    assert_model("vote/6", {"user_id": 5, "meta_deleted": True})
+    assert_model("user/6", {"delegated_vote_ids": []})
+    assert_model("vote/6", {"user_id": 5, "delegated_user_id": 6, "meta_deleted": True})
