dcda9e2b597398fb4337b4c60492714d9fc25a2e
Hannes Janott <hannes.janott@openslides.com>
2025-12-12 12:45:42 +0100

    increase last non rel index

--- openslides_backend/migrations/migration_helper.py
+++ openslides_backend/migrations/migration_helper.py
@@ -43,7 +43,7 @@ class MigrationCommand(StrEnum):
 # relative path to the migrations
 MIGRATIONS_PATH = "openslides_backend/migrations/migrations/"
 MODULE_PATH = MIGRATIONS_PATH.replace("/", ".")
-LAST_NON_REL_MIGRATION = 72
+LAST_NON_REL_MIGRATION = 73
 
 
 class MigrationHelper:
