532c0d1bbaf3f848e31e19b6c0a2b6b05d80b426
Hannes Janott <hannes.janott@openslides.com>
2026-01-22 17:57:38 +0100

    add test for unrecoverable projections to be deleted

--- tests/system/migrations/test_0075_remove_broken_projection_connection.py
+++ tests/system/migrations/test_0075_remove_broken_projection_connection.py
@@ -24,3 +24,28 @@ def test_simple(write, finalize, assert_model):
         "projection/1",
         {"content_object_id": "meeting/1"},
     )
+
+
+def test_unrecoverable(write, finalize, assert_model):
+    test_data: dict[str, dict[str, Any]] = {
+        "projection/1": {"content_object_id": None},
+        "meeting/1": {"projection_ids": []},
+    }
+
+    write(
+        *[
+            {"type": "create", "fqid": fqid, "fields": data}
+            for fqid, data in test_data.items()
+        ]
+    )
+
+    finalize("0075_remove_broken_projection_connection")
+
+    assert_model(
+        "meeting/1",
+        {"projection_ids": []},
+    )
+    assert_model(
+        "projection/1",
+        {"meta_deleted": True},
+    )
