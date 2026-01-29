cdfbad5704e01256a62d49feff4217a871ca25b7
Luisa <luisa.beerboom@intevation.de>
2026-01-29 19:34:26 +0100

    Added deleted proj to 76

--- tests/system/migrations/test_0076_projector_scroll_minimum.py
+++ tests/system/migrations/test_0076_projector_scroll_minimum.py
@@ -6,6 +6,7 @@ def test_simple(write, finalize, assert_model):
         "projector/1": {"scroll": -1},
         "projector/2": {"scroll": 0},
         "projector/3": {"scroll": 1},
+        "projector/4": {"scroll": -1},
     }
 
     write(
@@ -14,10 +15,12 @@ def test_simple(write, finalize, assert_model):
             for fqid, data in test_data.items()
         ]
     )
+    write({"type": "delete", "fqid": "projector/4"})
 
     finalize("0076_projector_scroll_minimum")
 
     test_data["projector/1"]["scroll"] = 0
+    test_data["projector/4"]["meta_deleted"] = True
 
     for fqid, model in test_data.items():
         assert_model(fqid, model)
