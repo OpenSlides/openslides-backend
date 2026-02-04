1cb5cf0fe4b3ad67481b3b2ce9b960a5810a93b0
Luisa <luisa.beerboom@intevation.de>
2026-02-04 15:52:58 +0100

    Improve migration 0075 test

--- tests/system/migrations/test_0075_remove_broken_projection_connection.py
+++ tests/system/migrations/test_0075_remove_broken_projection_connection.py
@@ -4,7 +4,8 @@ from typing import Any
 def test_simple(write, finalize, assert_model):
     test_data: dict[str, dict[str, Any]] = {
         "projection/1": {"content_object_id": None},
-        "meeting/1": {"projection_ids": [1]},
+        "projection/2": {},
+        "meeting/1": {"projection_ids": [1, 2]},
     }
 
     write(
@@ -18,10 +19,14 @@ def test_simple(write, finalize, assert_model):
 
     assert_model(
         "meeting/1",
-        {"projection_ids": [1]},
+        {"projection_ids": [1, 2]},
     )
     assert_model(
         "projection/1",
+        {"content_object_id": "meeting/1"},
+    )
+    assert_model(
+        "projection/2",
         {"content_object_id": "meeting/1"},
     )
 
@@ -30,7 +35,8 @@ def test_deleted(write, finalize, assert_model):
     test_data: dict[str, dict[str, Any]] = {
         "projection/1": {"content_object_id": None},
         "projection/2": {"content_object_id": "meeting/1"},
-        "meeting/1": {"projection_ids": [1, 2]},
+        "projection/3": {},
+        "meeting/1": {"projection_ids": [1, 2, 3]},
     }
 
     write(
@@ -39,6 +45,7 @@ def test_deleted(write, finalize, assert_model):
             for fqid, data in test_data.items()
         ],
         {"type": "delete", "fqid": "projection/1"},
+        {"type": "delete", "fqid": "projection/3"},
     )
 
     finalize("0075_remove_broken_projection_connection")
@@ -51,11 +58,16 @@ def test_deleted(write, finalize, assert_model):
         "projection/1",
         {"meta_deleted": True},
     )
+    assert_model(
+        "projection/3",
+        {"meta_deleted": True},
+    )
 
 
 def test_unrecoverable(write, finalize, assert_model):
     test_data: dict[str, dict[str, Any]] = {
         "projection/1": {"content_object_id": None},
+        "projection/2": {},
         "meeting/1": {"projection_ids": []},
     }
 
@@ -74,5 +86,9 @@ def test_unrecoverable(write, finalize, assert_model):
     )
     assert_model(
         "projection/1",
+        {"meta_deleted": True},
+    )
+    assert_model(
+        "projection/2",
         {"meta_deleted": True},
     )
