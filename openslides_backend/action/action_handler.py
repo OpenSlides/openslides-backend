7676148ce2c27ed7c112028a886d98eac6da8fd9
Hannes Janott <hannes.janott@openslides.com>
2026-02-18 11:22:15 +0100

    improve handle separately

--- openslides_backend/action/action_handler.py
+++ openslides_backend/action/action_handler.py
@@ -151,18 +151,20 @@ class ActionHandler(BaseHandler):
                                 )
 
                             for element in payload:
-                                try:
-                                    result = self.execute_write_requests(
-                                        lambda e: transform_to_list(
+                                for list_element in element["data"]:
+                                    try:
+                                        result = self.execute_write_requests(
+                                            lambda e: transform_to_list(
                                             self.perform_action(e)
                                         ),
-                                        element,
-                                    )
-                                    results.append(result)
-                                except ActionException as exception:
-                                    error = cast(ActionError, exception.get_json())
-                                    results.append(error)
-                                self.datastore.reset()
+                                            element,
+                                        )
+                                        self.datastore.connection.commit()
+                                        results.append(result)
+                                    except ActionException as exception:
+                                        error = cast(ActionError, exception.get_json())
+                                        results.append(error)
+                                    self.datastore.reset()
 
                         # execute cleanup methods
                         for on_success in self.on_success:
