7cc70ade86b417f3e2e7d9d3af4d07cd27348cdb
Adrian Richter <adrian@intevation.de>
2026-03-19 12:41:17 +0100

    Improve code style

--- cli/get_everything.py
+++ cli/get_everything.py
@@ -1,15 +1,12 @@
 import logging
 import os
 import sys
+from json import dumps
 
 from datastore.reader.app import register_services
 
-from openslides_backend.presenter.check_database import check_meetings
-from openslides_backend.presenter.check_database_all import check_everything
 from openslides_backend.shared.env import Environment
 from openslides_backend.wsgi import OpenSlidesBackendServices
-
-from json import dumps
 
 
 def main() -> int:
@@ -23,8 +20,6 @@ def main() -> int:
     datastore = services.datastore()
 
     everything = datastore.get_everything()
-
-    #print(dumps(everything))
     print(dumps(everything, indent=4, sort_keys=True, default=str))
 
     return 0
