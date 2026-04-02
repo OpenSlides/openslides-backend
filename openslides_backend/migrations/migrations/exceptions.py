5d1de9f406543335307cc66ed31a380829d20dd0
Luisa <luisa.beerboom@intevation.de>
2026-04-02 17:40:29 +0200

    Begin testing

--- openslides_backend/migrations/migrations/exceptions.py
+++
@@ -1,4 +0,0 @@
-class MigrationException(Exception):
-    def __init__(self, errors: list[str]):
-        err_str = '\n* '.join(errors)
-        super().__init__(f"Migration exception:\n* {err_str}")
