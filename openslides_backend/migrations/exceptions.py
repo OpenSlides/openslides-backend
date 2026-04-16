e478a245ab6a8833b8b319ff3f1f244c9968ebe6
luisa-beerboom <101706784+luisa-beerboom@users.noreply.github.com>
2026-04-16 11:11:06 +0200

    Pre-migrate corrupted unique-fields (#3453)

---
+++ openslides_backend/migrations/exceptions.py
@@ -0,0 +1,7 @@
+from ..shared.exceptions import View400Exception
+
+
+class MigrationException(View400Exception):
+    def __init__(self, errors: list[str]):
+        err_str = "\n* ".join(sorted(errors))
+        super().__init__(f"Migration exception:\n* {err_str}")
