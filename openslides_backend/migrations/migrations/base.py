e3731c8a56fb67623b201b56711ed31209a3439d
Hannes Janott <hannes.janott@openslides.com>
2026-04-29 16:51:05 +0200

    improve docs

--- openslides_backend/migrations/migrations/base.py
+++ openslides_backend/migrations/migrations/base.py
@@ -8,7 +8,11 @@ class BaseMigration:
     @staticmethod
     def check_prerequisites(curs: Cursor[DictRow]) -> str:
         """
-        Checks all prerequisites for the migration.
+        This function should be overridden by subclasses to implement the desired behavior.
+        Purpose:
+            Checks all prerequisites for the migration.
+        Input:
+            cursor
         Returns:
             All errors collected. Empty string otherwise.
         """
@@ -17,13 +21,18 @@ class BaseMigration:
     @staticmethod
     def data_definition(curs: Cursor[DictRow]) -> None:
         """
-        Applies all manual SQL DDL changes necessary.
-        Triggers and views are automatically recreated.
+        This function should be overridden by subclasses to implement the desired behavior.
+        Purpose:
+            Applies all manual SQL DDL changes necessary.
+            (Triggers and views are automatically recreated by the framework.)
+        Input:
+            cursor
         """
 
     @staticmethod
     def data_manipulation(curs: Cursor[DictRow]) -> None:
         """
+        This function should be overridden by subclasses to implement the desired behavior.
         Purpose:
             Writes all data changes necessary after the DDL changes.
         Input:
@@ -33,6 +42,7 @@ class BaseMigration:
     @staticmethod
     def cleanup(curs: Cursor[DictRow]) -> None:
         """
+        This function should be overridden by subclasses to implement the desired behavior.
         Purpose:
             Deletes leftovers of the migration.
         Input:
