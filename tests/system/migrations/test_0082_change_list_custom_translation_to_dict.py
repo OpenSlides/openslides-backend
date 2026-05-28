2942971d25956ef5c17263893eb54c0bcdd418a1
Luisa <luisa.beerboom@intevation.de>
2026-05-28 14:44:50 +0200

    Add case with actual text

--- tests/system/migrations/test_0082_change_list_custom_translation_to_dict.py
+++ tests/system/migrations/test_0082_change_list_custom_translation_to_dict.py
@@ -21,7 +21,32 @@ def test_migration(write, finalize, assert_model):
                 },
             }
             for i, ct in enumerate([new_ct1, new_ct2, old_ct1, old_ct2], 1)
-        ]
+        ],
+        {
+            "type": "create",
+            "fqid": "meeting/5",
+            "fields": {
+                "id": 5,
+                "custom_translations": [
+                    {"original": "'Tis but a scratch.", "translation": "My arm's off."},
+                    {"original": "No it isn't.", "translation": "It definitely is."},
+                    {"original": "I've had worse.", "translation": "I'm a liar."},
+                    {
+                        "original": "Just a flesh wound.",
+                        "translation": "Quite a severe injury, actually.",
+                    },
+                    {"original": "I'm invincible.", "translation": "I'm a loony."},
+                    {
+                        "original": "The Black Knight always triumphs!",
+                        "translation": "The Black Knight is not triumphing this time!",
+                    },
+                    {
+                        "original": "All right, we'll call it a draw.",
+                        "translation": "I've lost.",
+                    },
+                ],
+            },
+        },
     )
     write(
         {"type": "delete", "fqid": "meeting/1"},
@@ -52,5 +77,20 @@ def test_migration(write, finalize, assert_model):
             "id": 4,
             "custom_translations": old_ct2,
             "meta_deleted": True,
+        },
+    )
+    assert_model(
+        "meeting/5",
+        {
+            "id": 5,
+            "custom_translations": {
+                "'Tis but a scratch.": "My arm's off.",
+                "No it isn't.": "It definitely is.",
+                "I've had worse.": "I'm a liar.",
+                "Just a flesh wound.": "Quite a severe injury, actually.",
+                "I'm invincible.": "I'm a loony.",
+                "The Black Knight always triumphs!": "The Black Knight is not triumphing this time!",
+                "All right, we'll call it a draw.": "I've lost.",
+            },
         },
     )
