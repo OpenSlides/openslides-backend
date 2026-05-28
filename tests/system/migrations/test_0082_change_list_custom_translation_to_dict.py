4c2116d6857b779e31a2a411eb3fe38b61ed422d
Luisa <luisa.beerboom@intevation.de>
2026-05-28 14:28:46 +0200

    Migrate old custom translations

---
+++ tests/system/migrations/test_0082_change_list_custom_translation_to_dict.py
@@ -0,0 +1,56 @@
+def test_migration(write, finalize, assert_model):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    new_ct1: dict[str, str] = {alphabet[i]: alphabet[i + 1] for i in range(0, 26, 2)}
+    new_ct2: dict[str, str] = {alphabet[i]: alphabet[-(i + 1)] for i in range(13)}
+    old_ct1: list[dict[str, str]] = [
+        {"original": original, "translation": translation}
+        for original, translation in new_ct1.items()
+    ]
+    old_ct2: list[dict[str, str]] = [
+        {"original": original, "translation": translation}
+        for original, translation in new_ct2.items()
+    ]
+    write(
+        *[
+            {
+                "type": "create",
+                "fqid": f"meeting/{i}",
+                "fields": {
+                    "id": i,
+                    "custom_translations": ct,
+                },
+            }
+            for i, ct in enumerate([new_ct1, new_ct2, old_ct1, old_ct2], 1)
+        ]
+    )
+    write(
+        {"type": "delete", "fqid": "meeting/1"},
+        {"type": "delete", "fqid": "meeting/4"},
+    )
+
+    finalize("0082_change_list_custom_translation_to_dict")
+
+    assert_model(
+        "meeting/1",
+        {
+            "id": 1,
+            "custom_translations": new_ct1,
+            "meta_deleted": True,
+        },
+    )
+    assert_model(
+        "meeting/2",
+        {"id": 2, "custom_translations": new_ct2},
+    )
+    assert_model(
+        "meeting/3",
+        {"id": 3, "custom_translations": new_ct1},
+    )
+    assert_model(
+        "meeting/4",
+        {
+            "id": 4,
+            "custom_translations": old_ct2,
+            "meta_deleted": True,
+        },
+    )
