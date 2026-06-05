224eb42170e79a236c1e1df02e2f66d49573be9c
vkrasnovyd <viktoriia.krasnovyd@intevation.de>
2026-06-05 16:25:56 +0200

    Remove unused vote adapter methods

--- openslides_backend/services/vote/interface.py
+++ openslides_backend/services/vote/interface.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from typing import Any, Literal, Protocol
+from typing import Any, Protocol
 
 from ..shared.authenticated_service import AuthenticatedServiceInterface
 
@@ -10,21 +10,4 @@ class VoteService(AuthenticatedServiceInterface, Protocol):
     """
 
     @abstractmethod
-    def create(self, payload: dict[str, Any]) -> dict[str, Any]: ...
-
-    @abstractmethod
-    def update(self, id: int, payload: dict[str, Any]) -> dict[str, Any]: ...
-
-    @abstractmethod
     def delete(self, id: int) -> dict[str, Any]: ...
-
-    @abstractmethod
-    def start(self, id: int) -> dict[str, Any]: ...
-
-    @abstractmethod
-    def finalize(
-        self, id: int, optional_attributes: list[Literal["publish", "anonymize"]] = []
-    ) -> dict[str, Any]: ...
-
-    @abstractmethod
-    def reset(self, id: int) -> dict[str, Any]: ...
