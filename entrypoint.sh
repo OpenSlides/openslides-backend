73cc95579469a26300748ba9761ac0eb1f6d612d
Jan Malte Behrens <janmtbehrens@gmail.com>
2026-06-11 15:20:57 +0200

    fix: Prod skips keycloak wait if ANONYMOUS_ONLY is not set

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,17 +7,17 @@ printf "\nOpenslides DBMS:\n"
 printf "Export env variables for database.\n"
 source scripts/export_database_variables.sh
 
-# Wait for Database
 if [ ! $ANONYMOUS_ONLY ]; then
+  # Wait for Database
   meta/dev/scripts/wait-for-database.sh
   printf "DBMS is started.\n"
-fi
 
-# Wait for Keycloak
-until curl -f "$KEYCLOAK_URL_INTERNAL/realms/openslides/.well-known/openid-configuration" > /dev/null 2>&1; do
-  printf "Waiting for keycloak\n"
-  sleep 3
-done
+  # Wait for Keycloak
+  until curl -f "$KEYCLOAK_URL_INTERNAL/realms/openslides/.well-known/openid-configuration" > /dev/null 2>&1; do
+    printf "Waiting for keycloak\n"
+    sleep 3
+  done
+fi
 
 printf "Creating schema ...\n"
 python cli/create_schema.py
