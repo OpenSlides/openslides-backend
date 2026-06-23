4a13451584cb27cbe831d8ee2c4aebcafe45a24f
Luisa <luisa.beerboom@intevation.de>
2026-06-23 12:41:10 +0200

    Generalize health route recognition

--- openslides_backend/http/thread_worker.py
+++ openslides_backend/http/thread_worker.py
@@ -8,7 +8,12 @@ class OpenSlidesThreadWorker(ThreadWorker):
         # Peek into the sockets content to be able to read out the path part of the header (which ought to be the part after the first whitespace).
         peek = str(conn.sock.recv(4096, MSG_PEEK)).split()
 
-        if len(peek) >= 2 and "system/action/health" in peek[1]:
+        if (
+            len(peek) >= 2
+            and (stripped_path := peek[1].strip("/")).startswith("system/")
+            and stripped_path.endswith("/health")
+            and len(stripped_path.split("/")) == 3
+        ):
             # If the health route is recognized, set data_ready to True to stop handle from returning _DEFER and force it into working instead.
             # Since the message could be read, the data is probably there already,
             # the health route doesn't take a payload to begin with.
