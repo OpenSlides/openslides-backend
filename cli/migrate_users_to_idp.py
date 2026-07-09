cfd5afb9a222815adb2007820aea52942135d329
Jan Malte Behrens <janmtbehrens@gmail.com>
2026-07-09 15:34:49 +0200

    feat: Adapted migrate users script to zitadel

--- cli/migrate_users_to_idp.py
+++ cli/migrate_users_to_idp.py
@@ -13,16 +13,20 @@ def get_config(key, default=""):
 admin_username="admin"
 admin_password="admin"
 
+admin_token_path = "/zitadel/bootstrap/admin.pat"
+
 db_host = get_config("DATABASE_HOST")
 db_port = get_config("DATABASE_PORT")
 db_database = get_config("DATABASE_NAME")
 db_user = get_config("DATABASE_USER")
 db_password = get_config("DATABASE_PASSWORD")
 
+external_route = get_config("IDP_URL_EXTERNAL", "localhost:8080")
+
 idp_route = get_config("IDP_URL_INTERNAL", "http://zitadel-api:8080")
 idp_realm = get_config("IDP_OS_REALM", "openslides")
 
-idp_admin_route = f"{idp_route}/admin/realms/{idp_realm}/"
+idp_admin_route = f"{idp_route}/v2/"
 
 def create_connection():
     try:
@@ -38,36 +42,22 @@ def create_connection():
         pass
 
 # Returns access token of the REST API admin
-def get_admin_key():
+def get_admin_key() -> str:
+    # Fetch key from admin file
     try:
-        response = requests.post(f"{idp_route}/realms/master/protocol/openid-connect/token",
-            data={
-                'client_id': "admin-cli",
-                'username': admin_username,
-                'password': admin_password,
-                'grant_type': "password",
-            },
-            headers={
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        )
-
-        json_response = response.json()
-
-        if response.status_code != 200:
-            raise Exception(f"{response.status_code} {json_response}")
-
-        return json_response["access_token"]
+        with open(admin_token_path) as file:
+            idp_admin_access_token = file.read().replace("\n","")
+            return idp_admin_access_token
     except Exception as e:
-        logger.error(f"Error receiving keycloak admin token: {e}")
-    return None
+        logger.error(f"Error reading admin pat file: {e}")
+    return ""
 
-# Returns keycloak id of an existing keycloak user with matching username
-def get_idp_id_by_username(keycloak_admin_key, username):
+# Returns idp id of an existing idp user with matching username
+def get_idp_id_by_username(idp_admin_key, username):
     try:
         response = requests.get(idp_admin_route + "users?username=" + username,
             headers={
-                'Authorization': f'Bearer {keycloak_admin_key}'
+                'Authorization': f'Bearer {idp_admin_key}'
             }
         )
 
@@ -78,14 +68,14 @@ def get_idp_id_by_username(keycloak_admin_key, username):
 
         return json_response[0]['id']
     except Exception as e:
-        logger.error(f"Error getting keycloak user by username: {e}")
+        logger.error(f"Error getting idp user by username: {e}")
     return None
 
-def get_name_of_keycloak_user(keycloak_admin_key, idp_id):
+def get_name_of_idp_user(idp_admin_key, idp_id):
     try:
         response = requests.get(idp_admin_route + "users/" + idp_id,
             headers={
-                'Authorization': f'Bearer {keycloak_admin_key}'
+                'Authorization': f'Bearer {idp_admin_key}'
             }
         )
 
@@ -98,40 +88,74 @@ def get_name_of_keycloak_user(keycloak_admin_key, idp_id):
 
         return json_response['username']
     except Exception as e:
-        logger.error(f"Error getting keycloak user: {e}")
+        logger.error(f"Error getting idp user: {e}")
     return None
 
-# Creates a keycloak user for given OS user. Returns keycloak id of newly created user.
-# Returns existing keycloak users id, if a keycloak user of given username already exists
-def migrate_and_create_user(keycloak_admin_key, username, os_id):
+# Creates an IDP user for given OS user. Returns idp id of newly created user.
+# Returns existing idp users id, if a idp user of given username already exists
+def migrate_and_create_user(idp_admin_access_token, username, os_id, email, password):
     try:
-        response = requests.post(idp_admin_route + "users",
+        response = requests.post(
+        f"{idp_admin_route}organizations/_search",
+            headers={
+                'Authorization': f'Bearer {idp_admin_access_token}',
+                'Content-Type': 'application/json',
+                'Host': 'localhost:8080'
+            },
+            json={},
+            timeout=20,
+        )
+
+        organisationId = response.json()["result"][0]["id"]
+
+        ## Upload OS user to IDP
+        response = requests.post(idp_admin_route + "users/new",
             json={
                 'username': username,
-                'enabled': True,
-                "attributes": {
-                    "os_id": os_id
+                'organizationId': organisationId,
+                'human': {
+                    'hashedPassword': {
+                        'hash': password
+                    },
+                    'profile': {
+                        'givenName': username,
+                        'familyName': username,
+                    },
+                    'email': {
+                        'email': email,
+                        'isVerified': True
+                    }
                 },
+                'metadata': [
+                    {
+                        'key': 'os_id',
+                        'value': base64.b64encode(str(os_id).encode("utf-8")).decode("ascii")
+                    },
+                ]
             },
             headers={
-                'Authorization': f'Bearer {keycloak_admin_key}',
+                'Authorization': f'Bearer {idp_admin_access_token}',
+                'Host': f'{external_route}'
             }
         )
 
-        if response.status_code != 201 and response.status_code != 409:
-            raise Exception(f"{response.status_code}, {response}")
-
-        return get_idp_id_by_username(keycloak_admin_key, username)
+        if response.status_code == 200:
+            return response.json()["id"]
+        elif response.status_code == 409:
+            logger.error(f"A user named {username} already exists in IDP.")
+        else:
+            logger.error(f"Response: {response.status_code} {response.json()}")
     except Exception as e:
-        logger.error(f"Error migrating/creating keycloak user: {e}")
-    return None
+        logger.error(f"Error creating user: {e}")
+
+    return ""
 
 # This adds '=' for argon2 padding at the end of a password or salt. It needs to pad until the length of the string is divisible by 4
 def hash_padding(to_pad):
     return to_pad + '=' * (-len(to_pad) % 4)
 
-# Exports password to keycloak user
-def migrate_password(keycloak_admin_key, idp_id, password):
+# Exports password to idp user
+def migrate_password(idp_admin_key, idp_id, password):
     if len(password) == 152:
         # This password is likely SHA512 encoded. The user must therefore reset their password
         logger.warning(f"{idp_id} has a deprecated SHA512-encrypted password. They must reset their password on next visit")
@@ -142,7 +166,7 @@ def migrate_password(keycloak_admin_key, idp_id, password):
                     ]
                 ,
                 headers={
-                    'Authorization': f'Bearer {keycloak_admin_key}',
+                    'Authorization': f'Bearer {idp_admin_key}',
                 }
             )
 
@@ -175,31 +199,31 @@ def migrate_password(keycloak_admin_key, idp_id, password):
                     }]
                 },
                 headers={
-                    'Authorization': f'Bearer {keycloak_admin_key}',
+                    'Authorization': f'Bearer {idp_admin_key}',
                 }
             )
 
             if response.status_code != 204:
                 raise Exception(f"{response.status_code} {response.json()}")
         except Exception as e:
-            logger.error(f"Error migrating password for keycloak user {idp_id}: {e}")
+            logger.error(f"Error migrating password for idp user {idp_id}: {e}")
 
-# Exports email to keycloak user
-def migrate_email(keycloak_admin_key, idp_id, email):
+# Exports email to idp user
+def migrate_email(idp_admin_key, idp_id, email):
     try:
         response = requests.put(idp_admin_route + "users/" + idp_id,
             json={
                 'email': email,
             },
             headers={
-                'Authorization': f'Bearer {keycloak_admin_key}',
+                'Authorization': f'Bearer {idp_admin_key}',
             }
         )
 
         if response.status_code != 204:
             raise Exception(f"{response.status_code} {response.json()}")
     except Exception as e:
-        logger.error(f"Error migrating password for keycloak user {idp_id}: {e}")
+        logger.error(f"Error migrating password for idp user {idp_id}: {e}")
     return
 
 def user_stress_test(users_to_add) -> None:
@@ -210,18 +234,10 @@ def user_stress_test(users_to_add) -> None:
         existing_idp_id = ""
         os_id = i
 
-        keycloak_user_id = migrate_and_create_user(keycloak_admin_key, username, os_id)
+        idp_user_id = migrate_and_create_user(idp_admin_key, username, os_id, email, password)
 
-        if keycloak_user_id == None:
+        if idp_user_id == None:
             raise Exception(f"Error migrating or finding user {username}")
-
-        ## Migrate Data
-        migrate_email(keycloak_admin_key, keycloak_user_id, email)
-
-        migrate_password(keycloak_admin_key, keycloak_user_id, password)
-
-        ## Link username with keycloak id for later use
-        user_keycloak_map[username] = keycloak_user_id
 
 def main() -> None:
     conn = create_connection()
@@ -229,7 +245,7 @@ def main() -> None:
     ## Get Admin Key
     idp_admin_access_token = get_admin_key()
 
-    user_keycloak_map = {}
+    user_idp_map = {}
     ## Iterate all OS Users
     with conn.cursor() as cursor:
 
@@ -246,42 +262,40 @@ def main() -> None:
                 email = f"{username}@missing-email.com"
 
             if existing_idp_id is None or existing_idp_id == "":
-                # No Keycloak ID set. This OS User likely has no Keycloak Account yet
+                # No IDP ID set. This OS User likely has no IDP Account yet
+                logger.warning(f"Create new user {username}")
 
-                ## Upload OS user to Keycloak
-                keycloak_user_id = migrate_and_create_user(keycloak_admin_key, username, os_id)
+                ## Upload OS user to IDP
+                idp_user_id = migrate_and_create_user(idp_admin_access_token, username, os_id, email, password)
 
-                if keycloak_user_id == None:
-                    raise Exception(f"Error migrating or finding user {username}")
+                logger.warning(f"ID: {idp_user_id}")
+                if idp_user_id == None:
+                    raise Exception(f"Error migrating or finding user {username}. No ID")
             else:
-                # A Keycloak ID already exists. Check if it points to the correct OS User
-                keycloak_username = get_name_of_keycloak_user(keycloak_admin_key, existing_idp_id)
+                logger.warning(f"Update existing user {username} with id {existing_idp_id}")
+                # A IDP ID already exists. Check if it points to the correct OS User
+                idp_username = get_name_of_idp_user(idp_admin_access_token, existing_idp_id)
 
-                if keycloak_username is None:
+                if idp_username is None:
                     # No user with that id exists at all, create new one
-                    keycloak_user_id = migrate_and_create_user(keycloak_admin_key, username, os_id)
+                    idp_user_id = migrate_and_create_user(idp_admin_access_token, username, os_id, email)
 
-                    if keycloak_user_id == None:
+                    if idp_user_id == None:
                         raise Exception(f"Error migrating or finding user {username}")
-                elif keycloak_username != username:
-                    # Keycloak User exists, but is different from OS User
-                    raise Exception(f"Error: {username} already has a keycloak id in the database. However, that Keycloak ID points to {keycloak_username}")
+                elif idp_username != username:
+                    # IDP User exists, but is different from OS User
+                    raise Exception(f"Error: {username} already has a idp id in the database. However, that IDP ID points to {idp_username}")
                 else:
-                    # Keycloak User exists and is the same as OS User
-                    keycloak_user_id = existing_idp_id
+                    # IDP User exists and is the same as OS User
+                    idp_user_id = existing_idp_id
 
-            ## Migrate Data
-            migrate_email(keycloak_admin_key, keycloak_user_id, email)
+            ## Link username with idp id for later use
+            user_idp_map[username] = idp_user_id
 
-            migrate_password(keycloak_admin_key, keycloak_user_id, password)
-
-            ## Link username with keycloak id for later use
-            user_keycloak_map[username] = keycloak_user_id
-
-    ## Record Keycloak ID to OS User
-    for username, keycloak_user_id in user_keycloak_map.items():
+    ## Record IDP ID to OS User
+    for username, idp_user_id in user_idp_map.items():
         with conn.cursor() as cursor:
-            cursor.execute("UPDATE user_t SET idp_id = %s WHERE username = %s", (keycloak_user_id, username))
+            cursor.execute("UPDATE user_t SET idp_id = %s WHERE username = %s", (idp_user_id, username))
 
     ## Commit user changes
     conn.commit()
