8b3d2b9a2ad45e4115479fa86481c6027feb3278
Luisa <luisa.beerboom@intevation.de>
2026-07-29 16:07:46 +0200

    Rewrite organization_tag.delete

--- tests/system/action/organization_tag/test_delete.py
+++ tests/system/action/organization_tag/test_delete.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from openslides_backend.permissions.management_levels import OrganizationManagementLevel
 from tests.system.action.base import BaseActionTestCase
 
@@ -31,3 +33,43 @@ class OrganizationTagDeleteActionTest(BaseActionTestCase):
             OrganizationManagementLevel.CAN_MANAGE_ORGANIZATION,
         )
         self.assert_model_not_exists("organization_tag/1")
+
+    def test_delete_utilized_organization_tag(self) -> None:
+        self.create_meeting()
+        self.create_meeting(4)
+        self.create_meeting(7)
+        self.set_models(
+            {
+                "meeting/1": {"organization_tag_ids": [1, 2]},
+                "meeting/4": {"organization_tag_ids": [2]},
+                "meeting/7": {"organization_tag_ids": [1]},
+                "committee/60": {"organization_tag_ids": [1]},
+                "committee/63": {"organization_tag_ids": [1]},
+                "organization_tag/1": {
+                    "tagged_ids": [
+                        "meeting/1",
+                        "meeting/7",
+                        "committee/60",
+                        "committee/63",
+                    ]
+                },
+                "organization_tag/2": {
+                    "name": "test2",
+                    "color": "#ffffff",
+                    "tagged_ids": ["meeting/1", "meeting/4"],
+                },
+            }
+        )
+        response = self.request("organization_tag.delete", {"id": 1})
+        self.assert_status_code(response, 200)
+        self.assert_model_not_exists("organization_tag/1")
+        cases: dict[str, dict[str, Any]] = {
+            "meeting/1": {"organization_tag_ids": [2]},
+            "meeting/4": {"organization_tag_ids": [2]},
+            "meeting/7": {"organization_tag_ids": None},
+            "committee/60": {"organization_tag_ids": None},
+            "committee/63": {"organization_tag_ids": None},
+            "organization_tag/2": {"tagged_ids": ["meeting/1", "meeting/4"]},
+        }
+        for fqid, data in cases.items():
+            self.assert_model_exists(fqid, data)
