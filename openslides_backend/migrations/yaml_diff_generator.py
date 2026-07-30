f8b8730204e805b3a0dcb705c4bf03a8f0da7ce1
vkrasnovyd <viktoriia.krasnovyd@intevation.de>
2026-07-30 18:10:20 +0200

    Get changes from views related PR

--- openslides_backend/migrations/yaml_diff_generator.py
+++ openslides_backend/migrations/yaml_diff_generator.py
@@ -30,6 +30,15 @@ PREVIOUS_MODELS_DIR = os.path.join(
 )
 
 
+def load_models(mig_data_path: str) -> dict[str, Any]:
+    meta_file = os.path.join(mig_data_path, "collection-meta.yml")
+    collections_dir = os.path.join(mig_data_path, "collections")
+    return yaml.safe_load(build_models_yaml_content(meta_file, collections_dir))
+
+
+PREV_MODELS = load_models(PREVIOUS_MODELS_DIR)
+CURR_MODELS = load_models(CURR_MODELS_DIR)
+
 CollectionsRemoveList = list[list[str] | dict[str, Any]]
 EnumTypesRemoveDict = dict[str, list[str]]
 MetaAttributesRemoveList = list[list[str] | dict[str, Any]]
@@ -110,23 +119,21 @@ def dumpjson(diff: dict[str, Any]) -> None:
 
 
 def generate_diff() -> dict[str, Any]:
-    prev_models = load_models(PREVIOUS_MODELS_DIR)
-    curr_models = load_models(CURR_MODELS_DIR)
     directory = MigrationHelper.get_last_migration_directory()
     renames = MigrationHelper.get_migration_class(directory).renames
 
-    validate_renames(prev_models, curr_models, renames)
+    validate_renames(PREV_MODELS, CURR_MODELS, renames)
     # Edits caused by adding or removing field attributes
     secondary_edits: dict[str, Any] = {}
 
     return {
         "rename": renames,
         "remove": create_remove_recursive(
-            prev_models, curr_models, renames, prev_models, secondary_edits
+            PREV_MODELS, CURR_MODELS, renames, secondary_edits
         ),
-        "add": create_add_recursive(prev_models, curr_models, renames),
+        "add": create_add_recursive(PREV_MODELS, CURR_MODELS, renames),
         "edit": create_edit_recursive(
-            prev_models, curr_models, renames, secondary_edits
+            PREV_MODELS, CURR_MODELS, renames, secondary_edits
         ),
     }
 
@@ -169,7 +176,6 @@ def create_remove_recursive(
     prev_models: dict[str, Any],
     curr_models: dict[str, Any],
     renames_dict: dict[str, Any],
-    all_prev_models: dict[str, Any],
     secondary_edits: dict[str, Any],
     enum_tree: EnumTypesRemoveDict = {},
     path: tuple[str, ...] = (),
@@ -217,11 +223,11 @@ def create_remove_recursive(
                         field_def = prev_value
                     else:
                         field_name = path[2]
-                        field_def = all_prev_models[path[0]][path[1]][path[2]]
+                        field_def = PREV_MODELS[path[0]][path[1]][path[2]]
                     if not (
-                        "type" in field_def 
+                        "type" in field_def
                         and is_relational_field(field_def["type"])
-                        and is_view_field(path[0], field_name, field_def, all_prev_models)
+                        and is_view_field(path[0], field_name, field_def)
                     ):
                         missing_entries.append(key)
                 else:
@@ -231,7 +237,6 @@ def create_remove_recursive(
                 prev_value,
                 curr_models.get(key, {}),
                 renames_dict.get(key, {}),
-                all_prev_models,
                 secondary_edits,
                 enum_tree,
                 path + (key,),
@@ -324,12 +329,6 @@ def create_edit_recursive(
         return None
 
 
-def load_models(mig_data_path: str) -> dict[str, Any]:
-    meta_file = os.path.join(mig_data_path, "collection-meta.yml")
-    collections_dir = os.path.join(mig_data_path, "collections")
-    return yaml.safe_load(build_models_yaml_content(meta_file, collections_dir))
-
-
 def is_enum(key: str) -> bool:
     return key in FieldAttributes.enum_definitions
 
@@ -351,22 +350,18 @@ def is_relational_field(field_type: str) -> bool:
 
 
 def is_view_field(
-    collection_name: str,
-    field_name: str,
-    field_data: dict[str, Any],
-    all_prev_models: dict[str, dict[str, Any]],
+    collection_name: str, field_name: str, field_data: dict[str, Any]
 ) -> bool:
-    own = TableFieldType(collection_name, field_name, field_data)
+    own_field = TableFieldType(collection_name, field_name, field_data)
 
-    new_models = InternalHelper.MODELS
-    InternalHelper.MODELS = all_prev_models
+    InternalHelper.MODELS = PREV_MODELS
     foreign_fields = InternalHelper.get_definitions_from_foreign_list(
         field_data.get("to", None),
         field_data.get("reference", None),
     )
-    InternalHelper.MODELS = new_models
+    InternalHelper.MODELS = CURR_MODELS
 
-    state, *_ = InternalHelper.check_relation_definitions(own, foreign_fields)
+    state, *_ = InternalHelper.check_relation_definitions(own_field, foreign_fields)
     return state == FieldSqlErrorType.SQL
 
 
