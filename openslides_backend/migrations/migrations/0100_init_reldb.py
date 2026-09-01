6f3177d1a3202faed726371c694b0a993602293f
Luisa <luisa.beerboom@intevation.de>
2026-09-01 16:40:12 +0200

    Fix linting...again

--- openslides_backend/migrations/migrations/0100_init_reldb.py
+++ openslides_backend/migrations/migrations/0100_init_reldb.py
@@ -26,8 +26,9 @@ from openslides_backend.models.fields import (
     RelationListField,
     TimestampField,
 )
-from openslides_backend.models.models import \
-    *  # type: ignore # noqa # necessary to fill model_registry
+
+# necessary to fill model_registry
+from openslides_backend.models.models import *  # type: ignore # noqa
 from openslides_backend.shared.env import is_truthy
 
 RELATION_LIST_FIELD_CLASSES = [RelationListField, GenericRelationListField]
